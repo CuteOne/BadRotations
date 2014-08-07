7daba40bb53724d501bb37a5da649e78316b3c91
CodeMyGit <codemylife7@gmail.com>
2014-08-07 17:26:46 -0400

    Now really. works, Elemental

--- Rotations/Shaman/Elemental.lua
+++ Rotations/Shaman/Elemental.lua
@@ -199,9 +199,7 @@ function ShamanElemental()
 			---- # Keep Searing Totem up, unless Fire Elemental Totem is coming off cooldown in the next 20 seconds
 			-- searing_totem,if=cooldown.fire_elemental_totem.remains>20&!totem.fire.active
 			if isFireTotem(_FireElementalTotem) == false and (isFireTotem(_SearingTotem) == false or getTotemDistance("target") > 25) and getDistance("target") <= 25 and (isSelected("Fire Elemental") ~= true or GetSpellCD(_FireElementalTotem) > 20) then
-				if activeTotem == nil then
-					if castSpell("player",_SearingTotem,true) then return; end
-				end
+				if castSpell("player",_SearingTotem,true) then return; end
 			end	
 
 			-- spiritwalkers_grace,moving=1,if=((talent.elemental_blast.enabled&cooldown.elemental_blast.remains=0)|(cooldown.lava_burst.remains=0&!buff.lava_surge.react))|(buff.raid_movement.duration>=action.unleash_elements.gcd+action.earth_shock.gcd)
