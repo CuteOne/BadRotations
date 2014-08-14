259c42f6cd60d1183b293b3c5ca341e6bac43670
CodeMyGit <codemylife7@gmail.com>
2014-08-14 18:21:08 -0400

    Malkorok test

--- System/ObjectsManagers.lua
+++ System/ObjectsManagers.lua
@@ -213,7 +213,7 @@ if not metaTable1 then
 				for i = 1, #SpecificHPBuffs do -- start nomber of buff iteration
 					if UnitDebuffID(o.unit, SpecificHPBuffs[i].buff) ~= nil then -- if buff found
 						if SpecificHPBuffs[i].value ~= nil then
-							PercentWithIncoming = 100 + SpecificHPBuffs[i].value; -- we set its amount + 100 to make sure its within 50-100 range
+							PercentWithIncoming = 100 + (SpecificHPBuffs[i].value/1000000*100); -- we set its amount + 100 to make sure its within 50-100 range
 							break;
 						end
 					end
