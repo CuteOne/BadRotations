7d0399d94e9a51671798e8c1fda31236bf8085d6
ph34rt3hcute1 <ph34rt3hcute1@gmail.com>
2014-08-15 11:30:25 -0500

    Windwalker Update

--- Rotations/Monk/Windwalker.lua
+++ Rotations/Monk/Windwalker.lua
@@ -9,7 +9,6 @@ if select(3, UnitClass("player")) == 10 then
 	    if not canRun() then
 	    	return true
 	    end
-
 ---------------------------------------
 --- Ressurection/Dispelling/Healing ---
 ---------------------------------------
@@ -80,7 +79,7 @@ if select(3, UnitClass("player")) == 10 then
 		end
 	-- Touch of Karma
 		if getHP("player")<=50 and canAttack("target","player") and not UnitIsDeadOrGhost("target") then
-			if castSpell("target",_TouchofKarma,false) then return; end
+			if castSpell("target",_TouchOfKarma,false) then return; end
 		end
 	-- Fortifying Brew
 		if getHP("player")<=40 then
@@ -95,9 +94,15 @@ if select(3, UnitClass("player")) == 10 then
 --- Out of Combat ---
 ---------------------
 		if not isInCombat("player") then
-	--Expel Harm (Chi Builer)
+	-- Expel Harm (Chi Builer)
 			if BadBoy_data['Builder']==1 and (getChiMax("player")-getChi("player"))>=2 and getPower("player")>=40 and not isCasting("player") then
 				if castSpell("player",_ExpelHarm,true) then return; end
+			end
+	-- Provoke
+			if not IsMounted() and canAttack("target","player") and not UnitIsDeadOrGhost("target") then
+				if select(2,IsInInstance())=="none" and #members==1 then
+					if castSpell("target",_Provoke,true) then return; end
+				end
 			end
 		end
 
@@ -126,65 +131,94 @@ if select(3, UnitClass("player")) == 10 then
 					end
 				end
 			end
-
 	------------------------------
 	--- In Combat - Interrupts ---
 	------------------------------
 		-- Quaking Palm
-			if canInterrupt(_QuakingPalm,tonumber(getValue("Interrupts"))) then
+			if canInterrupt(_QuakingPalm,tonumber(getValue("Interrupts"))) and targetDistance<3.5 then
 				if castSpell("target",_QuakingPalm,false) then return; end
 			end
 		-- Spear Hand Strike
-			if canInterrupt(_SpearHandStrike,tonumber(getValue("Interrupts"))) and getSpellCD(_QuakingPalm)>0 and getSpellCD(_QuakingPalm)<119 then
+			if canInterrupt(_SpearHandStrike,tonumber(getValue("Interrupts"))) and getSpellCD(_QuakingPalm)>0 and getSpellCD(_QuakingPalm)<119 and targetDistance<3.5 then
 				if castSpell("target",_SpearHandStrike,false) then return; end
 			end
 	-----------------------------
 	--- In Combat - Cooldowns ---
 	-----------------------------
+		-- Energizing Brew
+			if getTimeToMax("player")>5 then
+				if castSpell("player",_EnergizingBrew,true) then return; end
+			end
+	--------------------------------
+	--- In Combat - All Rotation ---
+	--------------------------------
+		-- Touch of Death
+			if UnitBuffID("player",_DeathNote) and targetDistance<3.5 then
+				if castSpell("target",_TouchOfDeath,false) then return; end
+			end
+		-- Chi Brew
+			if getChi("player")<=2 and (getAgility() > AgiSnap or (getCharges(_ChiBrew)==1 and getRecharge(_ChiBrew)<=10) or getCharges(_ChiBrew)==2 or getTimeToDie("target")<(getCharges(_ChiBrew)*10)) then
+				if castSpell("player",_ChiBrew,true) then return; end
+			end
+		-- Tiger Palm
+			if  targetDistance<3.5 and getBuffRemain("player",_TigerPower)<=3 and getChi("player")>=1 then
+				if castSpell("target",_TigerPalm,false) then return; end
+			end
+		-- Raising Sun Kick
+
+		-- Tiger Palm
+			if  targetDistance<3.5 and getBuffRemain("player",_TigerPower)==0 and getRSR()>1 and getTimeToMax("player")>1 and getChi("player")>=1 then
+				if castSpell("target",_TigerPalm,false) then return; end
+			end
 
 	-----------------------------------------
 	--- In Combat - Multi-Target Rotation ---
 	-----------------------------------------
+			if useAoE() then
+		--	Spinning Crane Kick
 
+			end
 	------------------------------------------
 	--- In Combat - Single-Target Rotation ---
 	------------------------------------------
-		-- Touch of Death
-			if UnitBuffID("player",_DeathNote) then
-				if castSpell("target",_TouchOfDeath,false) then return; end
-			end
+			if not useAoE() then
+		-- Raising Sun Kick
+
 		-- Fists of Fury
-			if getSpellCD(_EnergizingBrew)==0 
-				and getTimeToMax("player")>4 
-				and getBuffRemain("player",_TigerPower)>4 
-				and getChi("player")>=3 
-				and not isStanding(0.5) 
-				and getFacing("player","target")==true 
-			then
-				if castSpell("player",_FistsOfFury,true) then return; end
-			end
+				if getSpellCD(_EnergizingBrew)==0 
+					and getTimeToMax("player")>4 
+					and getBuffRemain("player",_TigerPower)>4 
+					and getChi("player")>=3 
+					and not isStanding(0.5) 
+					and getFacing("player","target")==true 
+					and targetDistance < 3.5
+				then
+					if castSpell("player",_FistsOfFury,true) then return; end
+				end
 		-- Tiger Palm
-			if (getBuffRemain("player",_TigerPower)<=3 and getChi("player")>=1)
-				or (getBuffRemain("player",_TigerPower)==0 and getRSR()>1 and getTimeToMax("player")>1 and getChi("player")>=1)
-				or (getBuffRemain("player",_ComboBreakerTigerPalm)>0 and (getBuffRemain("player",_ComboBreakerTigerPalm)<=2 or getTimeToMax("player")>=2))
-			then
-				if castSpell("target",_TigerPalm,false) then return; end
-			end
+				if targetDistance<3.5	and getBuffRemain("player",_ComboBreakerTigerPalm)>0 and (getBuffRemain("player",_ComboBreakerTigerPalm)<=2 or getTimeToMax("player")>=2) then
+					if castSpell("target",_TigerPalm,false) then return; end
+				end
 		-- Chi Wave
-			if getTimeToMax("player")>2 then
-				if castSpell("player",_ChiWave,true) then return; end
-			end
+				if getTimeToMax("player")>2 and targetDistance<=40 then
+					if castSpell("player",_ChiWave,true) then return; end
+				end
 		-- Blackout Kick
-			if (getBuffRemain("player",_ComboBreakerBlackoutKick)>0	or (getPower("player")+getRegen("player")*getRSRCD()>=40 and getChi("player")>=2)) then
-				if castSpell("target",_BlackoutKick,false) then return; end
-			end
+				if getBuffRemain("player",_ComboBreakerBlackoutKick)>0	and targetDistance<3.5 then
+					if castSpell("target",_BlackoutKick,false) then return; end
+				end
 		-- Jab
-			if (getChiMax("player")-getChi("player"))>=2 and getPower("player")>=40 and (getHP("player")>=80 or getSpellCD(_ExpelHarm)>0) then
-				if castSpell("target",_Jab,false) then return; end
+				if (getChiMax("player")-getChi("player"))>=2 and getPower("player")>=40 and (getHP("player")>=80 or getSpellCD(_ExpelHarm)>0) and targetDistance<3.5 then
+					if castSpell("target",_Jab,false) then return; end
+				end
+		-- Blackout Kick
+				if getPower("player")+getRegen("player")*getRSRCD()>=40 and getChi("player")>=2 and targetDistance<3.5 then
+					if castSpell("target",_BlackoutKick,false) then return; end
+				end
 			end
 		end --In Combat End	
 	-- Start Attack
-		if targetDistance<5 and isEnnemy("target") and not UnitIsDeadOrGhost("target") then
+		if targetDistance<3.5 and isEnnemy("target") and not UnitIsDeadOrGhost("target") then
 			StartAttack()
 		end
 	end
