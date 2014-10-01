d2956cea596c2338c28a0f5f15ce0553c686a1d5
averykey <jackavoden@live.com>
2014-10-01 18:17:01 -0500

    Prot spell list

--- ClassLibs/Warrior/SpellList.lua
+++ ClassLibs/Warrior/SpellList.lua
@@ -1,6 +1,13 @@
 if select(3, UnitClass("player")) == 1 then
 
 --Prot--
+DemoralizingShout	= 1160;
+Devastate			= 20243;
+LastStand			= 12975;
+ShieldSlam			= 23922;
+Revenge				= 6572;
+ShieldBarrier		= 112048;
+ShieldBlock			= 2565;
 
 --Arms--
 
@@ -11,9 +18,9 @@ WildStrike 			= 100130;
 
 --Shared--
 Cleave 				= 845;
-HeroicStrike		= 78;
 ColossusSmash 		= 86346;
 Charge 				= 100;
+HeroicStrike		= 78;
 HeroicThrow			= 57755;
 Execute				= 5308;
 HeroicLeap			= 6544;
@@ -21,28 +28,29 @@ Recklessness		= 1719;
 ShatteringThrow		= 64382;
 Throw				= 122475;
 ThunderClap			= 6343;
+VictoryRush			= 34428;
 Whirlwind			= 1680;
 
 --Utility--
+DemoralizingBanner	= 114203;
 Disarm				= 676;
 DiebytheSword		= 118038;
 Intervene			= 3411;
 Hamstring			= 1715;
 IntimidatingShout 	= 5246;
+MockingBanner		= 114192;
 Pummel				= 6552;
 RallyingCry			= 97462;
 SunderArmor			= 7386;
 Taunt				= 355;
 ShieldWall			= 871;
-SpellReflection		= 23920;
-MockingBanner		= 114192;
-DemoralizingBanner	= 114203;
 SkullBanner			= 114207;
+SpellReflection		= 23920;
 
 --Buffs--
 BattleShout 		= 6673;
-CommandingShout 	= 469;
 BerserkerRage 		= 18499;
+CommandingShout 	= 469;
 
 --Stance--
 BattleStance 		= 2457;
@@ -67,9 +75,11 @@ StormBolt			= 107570;
 
 --Procs--
 Enrage				= 12880;
-RagingBlowProc		= 131116;
-WildStrikeProc		= 46916;
+Incite				= 122016;
 MeatCleaver			= 85739;
+RagingBlowProc		= 131116;
+Ultimatum			= 122510;
+WildStrikeProc		= 46916;
 
 --Glyphs--
 
