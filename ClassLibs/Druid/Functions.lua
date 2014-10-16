57ef235efda7e7c0cbff860030a6842d4e9e6c05
ph34rt3hcute1 <ph34rt3hcute1@gmail.com>
2014-10-16 00:03:31 -0400

    Additional Feral Fixes

--- ClassLibs/Druid/Functions.lua
+++ ClassLibs/Druid/Functions.lua
@@ -113,30 +113,30 @@ if select(3, UnitClass("player")) == 11 then
 -- end
 
 ------Member Check------
-function CalculateHP(unit)
-  incomingheals = UnitGetIncomingHeals(unit) or 0
-  return 100 * ( UnitHealth(unit) + incomingheals ) / UnitHealthMax(unit)
-end
+-- function CalculateHP(unit)
+--   incomingheals = UnitGetIncomingHeals(unit) or 0
+--   return 100 * ( UnitHealth(unit) + incomingheals ) / UnitHealthMax(unit)
+-- end
 
-function GroupInfo()
-    members, group = { { Unit = "player", HP = CalculateHP("player") } }, { low = 0, tanks = { } }      
-    group.type = IsInRaid() and "raid" or "party" 
-    group.number = GetNumGroupMembers()
-    if group.number > 0 then
-        for i=1,group.number do 
-            if canHeal(group.type..i) then 
-                local unit, hp = group.type..i, CalculateHP(group.type..i) 
-                table.insert( members,{ Unit = unit, HP = hp } ) 
-                if hp < 90 then group.low = group.low + 1 end 
-                if UnitGroupRolesAssigned(unit) == "TANK" then table.insert(group.tanks,unit) end 
-            end 
-        end 
-        if group.type == "raid" and #members > 1 then table.remove(members,1) end 
-        table.sort(members, function(x,y) return x.HP < y.HP end)
-        --local customtarget = canHeal("target") and "target" -- or CanHeal("mouseover") and GetMouseFocus() ~= WorldFrame and "mouseover" 
-        --if customtarget then table.sort(members, function(x) return UnitIsUnit(customtarget,x.Unit) end) end 
-    end
-end
+-- function GroupInfo()
+--     members, group = { { Unit = "player", HP = CalculateHP("player") } }, { low = 0, tanks = { } }      
+--     group.type = IsInRaid() and "raid" or "party" 
+--     group.number = GetNumGroupMembers()
+--     if group.number > 0 then
+--         for i=1,group.number do 
+--             if canHeal(group.type..i) then 
+--                 local unit, hp = group.type..i, CalculateHP(group.type..i) 
+--                 table.insert( members,{ Unit = unit, HP = hp } ) 
+--                 if hp < 90 then group.low = group.low + 1 end 
+--                 if UnitGroupRolesAssigned(unit) == "TANK" then table.insert(group.tanks,unit) end 
+--             end 
+--         end 
+--         if group.type == "raid" and #members > 1 then table.remove(members,1) end 
+--         table.sort(members, function(x,y) return x.HP < y.HP end)
+--         --local customtarget = canHeal("target") and "target" -- or CanHeal("mouseover") and GetMouseFocus() ~= WorldFrame and "mouseover" 
+--         --if customtarget then table.sort(members, function(x) return UnitIsUnit(customtarget,x.Unit) end) end 
+--     end
+-- end
 
 
 
@@ -350,8 +350,8 @@ function feralBuffs()
     then
         -- Mark of the Wild
         if isChecked("Mark of the Wild") and not UnitExists("mouseover") then
-            for i = 1, #members do
-                if not isBuffed(members[i].Unit,{115921,20217,1126,90363}) and (#members==select(5,GetInstanceInfo()) or select(2,IsInInstance())=="none") then
+            for i = 1, #nNova do --members
+                if not isBuffed(nNova[i].unit,{115921,20217,1126,90363,159988,160017,160077}) and (#nNova==select(5,GetInstanceInfo()) or select(2,IsInInstance())=="none") then
                     if UnitPower("player") ~= UnitPower("player",0) then
                         CancelShapeshiftForm()
                     else
