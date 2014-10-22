3fd47df27d49f7e2bf59fb8335c9998fd8584b2c
ragnarger <ingo@glasenhardt.com>
2014-10-23 01:08:50 +0200

    mage: going to build basement

--- ClassLibs/Mage/SpellList.lua
+++ ClassLibs/Mage/SpellList.lua
@@ -1,54 +1,72 @@
 if select(3, UnitClass("player")) == 8 then
- 
--- Spells
-_AlterTime				=	108979;
-_ArcaneBrilliance		=	1459;
-_ArcaneExplosion 		= 	1449;
-_Blink 					=	1953;
-_Blizzard 				=	10;
-_ConeOfCold 			=	120;
-_ConjureManaGem			=	759;
-_ConjureRefreshment		=	42955;
-_ConjureRefreshmentTable =	43987;
-_Counterspell			=	2139;
-_DeepFreeze				=	44572;
-_Evocation				=	12051;
-_FireBlast				=	2136;
-_FlameStrike			=	2120;
-_FrostArmor				=	7302;
-_FrostNova				=	122;
-_Frosbolt				=	116;
-_FrostfireBolt			=	44614;
-_FrozenOrb				=	84714;
-_GreaterInvisibility	=	110959;
-_IceBarrier				=	11426;
-_IceBlock				=	45438;
-_IceLance				=	30455;
-_IcyVeins				=	131078;
-_LivingBomb				=	44457;
-_MageArmor				=	6117;
-_MirrorImage			=	55342;
-_MoltenArmor			=	30482;
-_Polymorph				=	118;
-_PresenceOfMind			=	12043;
-_RemoveCurse			=	475;
-_RingOfFrost			=	113724;
-_SlowFall				=	130;
-_Spellsteal				=	30449;
-_SummonWaterElemental	=	30687;
-_TimeWarp				=	80353;
 
--- Passives
-_BrainFreeze			=	44549;
-_BurningSoul			=	84254;
-_FingersOfFrost			=	112965;
-_ImprovedCounterspell	=	12598;
-_Invocation				=	114003
-_MasteryIcicles			=	76613
+-- Frost
+	FrostBomb 		= 112948;
+	Frostbolt		= 116;
+	FrostfireBolt	= 44614;
+	FrozenOrb		= 84721;
+	IceLance		= 30455;
+
+-- Fire
+	Blast Wave		= 157981;
+	Combustion		= 11129;
+	Fireball		= 133;
+	Ignite			= 12654; --12846 (mastery: ignite)
+	InfernoBlast	= 108853;
+	Pyroblast		= 11366;
+
+-- Arcane
+	ArcaneBarrage	= 44425;
+	ArcaneBlast		= 30451;
+	ArcaneExplosion	= 1449;
+	ArcaneMissiles	= 5143;
+	UnstableExplo	= 157976;
+
+-- Shared
 
 
+-- Utility
 
 
+-- Buffs
+	-- fire
+	EnhancedPyro	= 157644
+	HeatingUp		= 48107;
+	Pyroblast!		= 48108;
+	Pyromaniac		= 166868;
+	-- frost
+	BrainFreeze		= 57761;
+	EnhancedFB		= 157646;
+	FingersOfFrost	= 44544;
+	IceShard		= 166869;
+	IcyVeins		= 12472;
+	-- arcane
+	ArcaneAffinity	= 166871;
+	ArcaneCharge	= 36032;
+	ArcaneInstability= 166872;
+	ArcaneMissiles!	= 79683;
+	ArcanePower		= 12042;
+
+
+	-- all
+	Archmage		= 177176;
+	Instability		= 177051;  -- trinket 113948
+	MoltenMetal		= 177081;  -- trinket 113984
+	RuneOfPower		= 116014;
+	HowlingSoul		= 177046;  -- trinket 119194
+	MarkOfTheT		= 159234;
+
+
+-- Debuffs
+
+
+-- Talents
+
+
+-- Trinkets
+
+
+-- Glyphs
 
 
 
---
+++ Rotations/Mage/Frost.lua
@@ -0,0 +1,8 @@
+if select(3, UnitClass("player")) == 8 then
+
+function FrostMage()
+
+
+
+end
+end
