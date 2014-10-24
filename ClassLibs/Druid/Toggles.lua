b396e47d95d42a0c43cae6cb8535f388b9474325
masoudshafiee <sshhaaffiiee@gmail.com>
2014-10-24 14:52:37 +0330

    Fix toggle

--- ClassLibs/Druid/Toggles.lua
+++ ClassLibs/Druid/Toggles.lua
@@ -263,7 +263,7 @@ if select(3, UnitClass("player")) == 11 then
 			elseif getValue(toggle) == 9 then
                 return IsMouseButtonDown(5);
 			elseif getValue(toggle) == 10 then
-                return 1
+                return false
             end
         end
     end
