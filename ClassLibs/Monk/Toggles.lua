8c23fe64bb38fce34f1011dca5adaee9f64d39f6
chumiiger <jonas.dreier@googlemail.com>
2014-11-30 05:41:47 +0100

    Brewmaster Level 100 first release
    Needs more testing, but looks fine so far in heroics

--- ClassLibs/Monk/Toggles.lua
+++ ClassLibs/Monk/Toggles.lua
@@ -11,44 +11,48 @@ if select(3,UnitClass("player")) == 10 then
    function MonkBrewToggles()
 
         -- Aoe Button
-        if  AoEModesLoaded ~= "Brew Monk AoE Modes" then
+        if AoEModesLoaded ~= "Brew Monk AoE Modes" then
             AoEModes = {
-                [1] = { mode = "Sin", value = 1 , overlay = "Single Target Enabled", tip = "|cffC0C0C0AoE \n|cffFFDD11Recommended for \n|cff00FF00Single Target (1-2)", highlight = 0, icon = 108557 },
-                [2] = { mode = "AoE", value = 2 , overlay = "AoE Enabled", tip = "|cffC0C0C0AoE \n|cffFFDD11Recommended for \n|cffFF0000AoE (3+)", highlight = 0, icon = 101546 },
-                [3] = { mode = "Auto", value = 3 , overlay = "Auto-AoE Enabled", tip = "|cffC0C0C0AoE \n|cffFFDD11Auto-AoE", highlight = 1, icon = 116812 }
+                [1] = { mode = "Auto", value = 1 , overlay = "Automatic Rotation", tip = "Swaps between Single and Multiple based on number of targets in range.", highlight = 1, icon = _SpinningCraneKick },
+                [2] = { mode = "Sing", value = 2 , overlay = "Single Target Rotation", tip = "Single target rotation used.", highlight = 0, icon = _Jab }
             };
-            CreateButton("AoE",0.5,1)
-            AoEModesLoaded = "Brew Monk AoE Modes";
+           CreateButton("AoE",1,0)
+           AoEModesLoaded = "Brew Monk AoE Modes";
         end
-        -- Interrupts Button
-        if  InterruptsModesLoaded ~= "Brew Monk Interrupts Modes" then
-            InterruptsModes = {
-                [1] = { mode = "None", value = 1 , overlay = "Interrupts Disabled", tip = "|cffC0C0C0Interrupts \n|cffFF0000No Interrupts will be used.", highlight = 0, icon = [[INTERFACE\ICONS\INV_Misc_AhnQirajTrinket_03]] },
-                [2] = { mode = "All", value = 2 , overlay = "Interrupts Enabled", tip = "|cffC0C0C0Interrupts \n|cffFF0000Spells Included: \n|cffFFDD11Spear Hand Strike.", highlight = 1, icon = 116705 }
+        -- Cooldowns Button
+        if CooldownsModesLoaded ~= "Cooldown Modes" then
+            CustomCooldownsModes = {
+                [1] = { mode = "Auto", value = 1 , overlay = "Cooldowns Automated", tip = "Automatic Cooldowns - Boss Detection.", highlight = 1, icon = _InvokeXuen },
+                [2] = { mode = "On", value = 2 , overlay = "Cooldowns Enabled", tip = "Cooldowns used regardless of target.", highlight = 1, icon = _InvokeXuen },
+                [3] = { mode = "Off", value = 3 , overlay = "Cooldowns Disabled", tip = "No Cooldowns will be used.", highlight = 0, icon = [[INTERFACE\ICONS\INV_Misc_AhnQirajTrinket_03]] }
             };
-            CreateButton("Interrupts",1,0)
-            InterruptsModesLoaded = "Brew Monk Interrupts Modes";
+           CooldownsModes = CustomCooldownsModes
+           CreateButton("Cooldowns",2,0)
+           CooldownsModesLoaded = "Cooldown Modes";
         end
 
         -- Defensive Button
-        if  DefensiveModesLoaded ~= "Brew Monk Defensive Modes" then
-            DefensiveModes = {
-                [1] = { mode = "None", value = 1 , overlay = "Defensive Disabled", tip = "|cffC0C0C0Defensive \n|cffFF0000No Defensive Cooldowns will be used.", highlight = 0, icon = [[INTERFACE\ICONS\INV_Misc_AhnQirajTrinket_03]] },
-                [2] = { mode = "All", value = 2 , overlay = "Defensive Enabled", tip = "|cffC0C0C0Defensive \n|cffFF0000Spells Included: \n|cffFFDD11Fortifying Brew, \nElusive Brew, \nGuard", highlight = 1, icon = 115203 }
+        if DefensiveModesLoaded ~= "Defensive Modes" then
+            CustomDefensiveModes = {
+                [1] = { mode = "On", value = 1 , overlay = "Defensive Enabled", tip = "Defensive Cooldowns - Adjust in config", highlight = 1, icon = _Guard },
+                [2] = { mode = "Off", value = 2 , overlay = "Defensive Disabled", tip = "No Defensives will be used.", highlight = 0, icon = [[INTERFACE\ICONS\INV_Misc_AhnQirajTrinket_03]] }
             };
-            CreateButton("Defensive",1.5,1)
-            DefensiveModesLoaded = "Brew Monk Defensive Modes";
+            DefensiveModes = CustomDefensiveModes
+            CreateButton("Defensive",3,0)
+            DefensiveModesLoaded = "Defensive Modes";
         end
-        -- Cooldowns Button
-        if  CooldownsModesLoaded ~= "Brew Monk Cooldowns Modes" then
-            CooldownsModes = {
-                [1] = { mode = "None", value = 1 , overlay = "Cooldowns Disabled", tip = "|cffC0C0C0Cooldowns \n|cffFF0000No cooldowns will be used.", highlight = 0, icon = [[INTERFACE\ICONS\INV_Misc_AhnQirajTrinket_03]] },
-                [2] = { mode = "User", value = 2 , overlay = "User Cooldowns Enabled", tip = "|cffC0C0C0Cooldowns \n|cffFF0000Spells Included: \n|cffFFDD11Config's selected spells.", highlight = 1, icon = [[INTERFACE\ICONS\inv_misc_blackironbomb]]},
-                [3] = { mode = "All", value = 3 , overlay = "Cooldowns Enabled", tip = "|cffC0C0C0Cooldowns \n|cffFF0000Spells Included: \n|cffFFDD11Avenging Wrath, \nHoly Avenger.", highlight = 1, icon = 115080 }
+
+     -- Interrupts Button
+        if InterruptsModesLoaded ~= "Interrupt Modes" then
+            CustomInterruptsModes = {
+                [1] = { mode = "On", value = 1 , overlay = "Interrupts Enabled", tip = "Includes Basic Interrupts.", highlight = 1, icon = _SpearHandStrike },
+                [2] = { mode = "Off", value = 2 , overlay = "Interrupts Disabled", tip = "No Interrupts will be used.", highlight = 0, icon = [[INTERFACE\ICONS\INV_Misc_AhnQirajTrinket_03]] }
             };
-            CreateButton("Cooldowns",2,0)
-            CooldownsModesLoaded = "Brew Monk Cooldowns Modes";
+            InterruptsModes = CustomInterruptsModes
+            CreateButton("Interrupts",4,0)
+            InterruptsModesLoaded = "Interrupt Modes";
         end
+
         function SpecificToggle(toggle)
             if getValue(toggle) == 1 then
                 return IsLeftControlKeyDown();
