a949b82f2a58b6b19e88be1fcd641b8201d8b054
Tocsin614 <paul.borrows@yahoo.com>
2014-12-26 16:22:50 -0500

    Combat Update for wod
    Moved all Combat to it's own folder.
    Feedback would be awesome..I just hit 100 on the rogue. 570 ilvl FTW..

---
+++ Rotations/Rogue/Combat/Combat.lua
@@ -0,0 +1,277 @@
+if select(3, UnitClass("player")) == 4 then
+	function CombatRogue()
+		if Currentconfig ~= "Combat Toxin" then
+			CombatOptions()
+			Currentconfig = "Combat Toxin"
+		end
+		if not canRun() then
+	    	return true
+	    end
+		CombatToggles()
+		poisonData()
+		makeEnemiesTable(40)
+		
+-- Need to add level checks for spells that changed from 90-100 ie Deadly poison changes to instant at 92
+
+-- Used some of Cuteones code to speed up changing this over to new format. 
+-- Just hit 100
+-- --------------
+-- --- Locals ---
+-- --------------
+
+		local kilRemain = getBuffRemain("player",_KillingSpree) --1 of 2 major cooldowns
+		local deeRemain = getBuffRemain("player",84747) --Deep Insight 30% buff
+     	local enemies = #getEnemies("player",8)
+     	local thisUnit = dynamicTarget(5,true)
+ 		local tarDist = getDistance("target","player")
+ 		local hasMouse = UnitExists("mouseover")
+ 		local level = UnitLevel("player")
+ 		local php = getHP("player")
+ 		local thp = getHP(thisUnit)
+ 		local combo = getCombo()
+ 		local power = getPower("player")
+ 		local stealth = getBuffRemain("player",_Stealth)~=0
+ 		local lethalRemain = getBuffRemain("player",157584)
+ 		local nonlethalRemain = getBuffRemain("player",_CripplingPoison)
+ 		local recRemain = getBuffRemain("player",_Recuperate)
+ 		local vanRemain = getBuffRemain("player",_VanishBuff)
+ 		local revRemain = getDebuffRemain(thisUnit,_RevealingStrike,"player")
+ 		local sndRemain = getBuffRemain("player",_SliceAndDice)
+ 		local ctRemain = getDebuffRemain(thisUnit,_CrimsonTempest,"player")
+ 		local dfaRemain = getBuffRemain("player",_DeathFromAbove)
+ 		--local adrRemain = getbuffRemain("player",13750)
+ 		local cdRush = getSpellCD(_AdrenalineRush)
+ 		local ddRemain = getDebuffRemain(thisUnit,113780,"player")
+
+
+----------------------------------
+--- Poisons/Healing/Dispelling ---
+----------------------------------
+
+		if (isCastingSpell(_InstantPoison) and lethalRemain>5) or ((isCastingSpell(_LeechingPoison) and nonlethalRemain>5)) then
+			RunMacroText("/stopcasting")
+		end
+	-- Lethal Poison
+		if lethalRemain<5 and not isMoving("player") and level>91 and not castingUnit("player") and not IsMounted() then
+			if castSpell("player",2823,true) then return end --The casting spell id is different(Just thought of it, below lvl 92 would be deadlypoison)
+		end
+	-- Non-Lethal Poison
+		if nonlethalRemain<5 and not isMoving("player") and not castingUnit("player") and not IsMounted() then
+			if castSpell("player",_CripplingPoison,true) then return end
+		end
+	-- Recuperate
+		if php < 80 and recRemain==0 and combo>0 then
+			if castSpell("player",_Recuperate,true,false,false) then return end
+		end
+	-- Cloak of Shadows
+		if canDispel("player") then
+			if castSpell("player",_CloakOfShadows,true,false,false) then return end
+		end
+
+	-- Pause
+		if pause() then
+			return true
+		else
+
+-------------
+--- Buffs ---
+-------------
+
+    -- Flask / Crystal
+	        if isChecked("Flask / Crystal") then
+	            if (select(2,IsInInstance())=="raid" or select(2,IsInInstance())=="none") and not UnitBuffID("player",105689) then
+	                if not UnitBuffID("player",127230) and canUse(86569) then
+	                    UseItemByName(tostring(select(1,GetItemInfo(86569))))
+	                end
+	            end
+	        end
+
+------------------
+--- Defensives ---
+------------------
+
+			if useDefensive() and not stealth then
+	-- Evasion
+				if php<50 then
+					if castSpell("player",_Evasion,true,false,false) then return end
+				end
+	-- Combat Readiness
+				if php<40 then
+					if castSpell("player",_CombatReadiness,true,false,false) then return end
+				end
+	-- Recuperate
+				if php<30 and combo>3 and recRemain==0 then
+					if castSpell("player",_Recuperate) then return end
+				end
+	-- Vanish
+				if php<15 then
+					if castSpell("player",_Vanish) then StopAttack(); ClearTarget(); return end
+				end
+    		end
+
+---------------------
+--- Out of Combat ---
+---------------------
+
+			if not (IsMounted() or IsFlying() or UnitIsFriend("target","player")) then
+	-- Stealth
+				if not isInCombat("player") and isChecked("Stealth") and (stealthTimer == nil or stealthTimer <= GetTime()-getValue("Stealth Timer")) and getCreatureType("target") == true and not stealth then
+					-- Always
+					if getValue("Stealth") == 1 then 
+						if castSpell("player",_Stealth,true,false,false) then stealthTimer=GetTime(); return end
+					end
+					-- Pre-Pot
+					if getValue("Stealth") == 2 and getBuffRemain("player",105697) > 0 and tarDist < 20 then
+						if castSpell("player",_Stealth,true,false,false) then stealthTimer=GetTime(); return end
+					end
+					-- 20 Yards
+					if getValue("Stealth") == 3 and tarDist < 20 then
+						if castSpell("player",_Stealth,true,false,false) then stealthTimer=GetTime(); return end
+					end
+				end
+				if not isInCombat("player") and stealth and tarDist < 25 and tarDist >= 8 and level>=60 and getTalent(4,2) then
+	-- Shadowstep
+					if castSpell("target",_Shadowstep,false,false,false) then return end
+				end
+				if stealth and getDistance("target") < 40 and getDistance("target") >= 8 and level>=60 and getTalent(4,1) then
+	-- Ambush
+				if not isInCombat("player") and not noattack() and (isPicked() or level<15) and UnitBuffID("player",_Stealth) and combo<5 and power>60 then
+					if castSpell("target",_Ambush,false,false,false) then return end
+				end
+	-- 5 Combo Opener
+				if not isInCombat("player") and UnitBuffID("player",_Stealth) and not noattack() and combo == 5 then
+					if power>25 and sndRemain<5 then
+						if castSpell("player",_SliceAndDice,true,false,false) then return end
+					end
+					if power>40 and revRemain<3 then
+						if castSpell("target",_Rupture,false,false,false) then return end
+					end
+					if power>50 and dfaRemain<2 then
+						if castSpell("target",_DeathFromAbove,false,false,false) then return end
+					end
+				end
+	-- Sinister Strike
+				if not isInCombat("player") and not noattack() and combo < 5 and power>50 then
+					if castSpell("target",_SinisterStrike,false,false,false) then return end
+				end
+			end
+
+-----------------
+--- In Combat ---
+-----------------
+
+			if isInCombat("player") then
+
+	------------------------------
+	--- In Combat - Dummy Test ---
+	------------------------------
+
+		-- Dummy Test
+				if isChecked("DPS Testing") then
+					if UnitExists("target") then
+						if getCombatTime() >= (tonumber(getValue("DPS Testing"))*60) and isDummy() then
+							StopAttack()
+							ClearTarget()
+							print(tonumber(getValue("DPS Testing")) .." Minute Dummy Test Concluded - Profile Stopped")
+						end
+					end
+				end
+
+	------------------------------
+	--- In Combat - Interrupts ---
+	------------------------------
+
+				if useInterrupts() and not stealth then
+	-- Kick
+					if level>=18 then
+						if castInterrupt(_Kick,getValue("Interrupts")) then return end
+					end
+	-- Gouge
+					if castInterrupt(_Gouge,getValue("Interrupts")) then return end
+	-- Blind
+					if castInterrupt(_Blind,getValue("Interrupts")) then return end
+			    end
+
+	-----------------------------
+	--- In Combat - Cooldowns ---
+	-----------------------------
+
+				if useCDs() and not stealth and tarDist<5 then
+	-- Preparation
+					if vanRemain==0 and getSpellCD(_Vanish)>60 then
+						if castSpell("player",_Preparation,true,false,false) then return end
+					end
+	-- Vanish
+					if combo<5 and power<60 and getCombatTime()>10 then
+						if castSpell("player",_Vanish,true,false,false) then return end
+					end
+	-- Adrenaline Rush
+					if getSpellCD(_KillingSpree)>15 then
+						if castSpell("player",_AdrenalineRush,false,false,false) then return end
+					end
+	-- Killing Spree
+					if cdRush >5 or (revRemain>3 and sndRemain>4) then
+						if castSpell(thisUnit,_KillingSpree,true,false,false) then return end
+		        	end
+		        end
+
+	------------------------------------------
+	--- In Combat Rotation ---
+	------------------------------------------
+	
+	-- Slice and Dice
+				if not isKnown(_ImprovedSliceAndDice) and sndRemain<8 and power>25 and tarDist<5 and combo>3 then
+					if castSpell("player",_SliceAndDice,true,false,false) then return end
+				end
+	--Revealing Strike
+				if revRemain<3 then
+					if castSpell("target",_RevealingStrike,false,false) then return; end
+				end
+	-- Marked for Death
+				if combo>=0 and getTalent(6,2) then
+					if castSpell(thisUnit,_MarkedForDeath,true,false,false) then return end
+				end
+	-- Crimson Tempest
+				if useAoE() and combo>4 and enemies>=5 and ctRemain<8 and power>35 then
+					if castSpell(thisUnit,_CrimsonTempest,true,false,false) then return end
+				end
+	-- Blade Flurry aoe on pita(not tested yet)
+				if useAoE() then
+	                for i = 1, #enemiesTable do
+	                	if enemiesTable[i].distance<5 then
+	                    	local thisUnitAoE = enemiesTable[i].unit
+	                    	if not UnitBuffID("player",13877) and getSpellCD(_BladeFlurry)==0 then
+	                    	    if castSpell(thisUnitAoE,_BladeFlurry,false,false,false) then return end
+	                    	end
+	                    end
+	                end
+	            end
+	-- Blade Flurry aoe off (hope this works right not tested yet)
+				if enemies <2 and UnitBuffID("player",13877) and getSpellCD(_BladeFlurry)==0 then 
+					if castSpell("player",_BladeFlurry) then return; end
+				end
+	-- Death From Above
+				if power>50 and getTalent(7,3) then
+					if castSpell("target",_DeathFromAbove,false,false,false) then return end
+				end						
+	--Deep Insight Early finisher w/ 30% dmg buff
+				if combo>=4 and UnitBuffID("player",84747) then --might need to put 2sec remain check dunno if a second left and this fires off if it will be in time to get the buff otherwise should wait till 5cp normal
+					if castSpell("target",_Eviscerate,false,false,false) then return end
+				end	
+	-- Eviscerate
+				if combo>4 and power>35 then
+					if castSpell("target",_Eviscerate,false,false,false) then return end
+				end
+	-- Combo point builder...doh I forgot this lol			
+				if combo<5 and power>=70 then
+					if castSpell("target",_SinisterStrike,true) then return; end
+				end
+			end --In Combat End
+	-- Start Attack
+			if tarDist<5 and not stealth then
+				StartAttack()
+			end
+		end -- Pause End
+	end --Rogue Function End
+end --Class Check End
+end --wtf didn't I close
---
+++ Rotations/Rogue/Combat/Options.lua
@@ -0,0 +1,144 @@
+if select(3, UnitClass("player")) == 4 then
+
+
+function titleOp(string)
+    return CreateNewTitle(thisConfig,string);
+end
+function checkOp(string,tip)
+    if tip == nil then
+        return CreateNewCheck(thisConfig,string);
+    else
+        return CreateNewCheck(thisConfig,string,tip);
+    end
+end
+function textOp(string)
+    return CreateNewText(thisConfig,string);
+end
+function wrapOp(string)
+    return CreateNewWrap(thisConfig,string);
+end
+function boxOp(string, minnum, maxnum, stepnum, defaultnum, tip)
+    if tip == nil then
+        return CreateNewBox(thisConfig,string, minnum, maxnum, stepnum, defaultnum);
+    else
+        return CreateNewBox(thisConfig,string, minnum, maxnum, stepnum, defaultnum, tip);
+    end
+end
+function dropOp(string, base, tip1, value1, value2, value3, value4, value5, value6, value7, value8, value9, value10)
+    return CreateNewDrop(thisConfig, string, base, tip1, value1, value2, value3, value4, value5, value6, value7, value8, value9, value10);
+end
+-- Config Panel
+function NewRogueConfig()
+    --if not doneConfig then
+        thisConfig = 0
+        -- Title
+        titleOp("CuteOne New Rogue");
+                -- Spacer
+        CreateGeneralsConfig();
+
+        WrapsManager();
+    --end
+end
+
+-- Config Panel
+function CombatOptions()
+    --if not doneConfig then
+        thisConfig = 0
+        -- Title
+        titleOp("Combat Toxin");
+                -- Spacer
+        textOp(" ");
+        wrapOp("--- General ---");
+
+    
+            -- Dummy DPS Test
+            checkOp("DPS Testing","|cff15FF00Enables|cffFFFFFF/|cffD60000Disable |cffFFFFFFtimed tests on Training Dummies. This mode stops the rotation after the specified time if the target is a Training Dummy.");
+            boxOp("DPS Testing", 5, 60, 5, 5, "|cffFFFFFFSet to desired time for test in minuts. Min: 5 / Max: 60 / Interval: 5")
+            textOp("DPS Testing");
+
+            -- Stealth Timer
+            checkOp("Stealth Timer");
+            boxOp("Stealth Timer", 0, 2, 0.25, 1, "|cffFFBB00How long to wait(seconds) before using \n|cffFFFFFFStealth.");
+            textOp("Stealth Timer");
+
+            -- Stealth
+            checkOp("Stealth");
+            dropOp("Stealth",1,"Stealthing method.","|cff00FF00Always","|cffFFDD00PrePot","|cffFF000020Yards");
+            textOp("Stealth");
+
+            -- Leathal Poison
+            checkOp("Lethal Poison");
+            dropOp("Lethal Poison",1,"Lethal Poison.","|cff13A300Instant","|cffFF8000Wound");
+            textOp("Lethal");
+
+            -- Non-Leathal Poison
+            checkOp("Non-Lethal Poison");
+            dropOp("Non-Lethal Poison",2,"Non-Lethal Poison.","|cff6600FFCrip","|cff00CF1CLeech");
+            textOp("Non-Lethal");
+
+        -- Spacer
+        textOp(" ");
+        wrapOp("--- Cooldowns ---");
+
+            -- Agi Pot
+            checkOp("Agi-Pot");
+            textOp("Agi-Pot");
+
+            -- Flask / Crystal
+            checkOp("Flask / Crystal");
+            textOp("Flask / Crystal");
+
+        -- Spacer
+        textOp(" ");
+        wrapOp("--- Defensive ---");
+
+            -- Healthstone
+            checkOp("Pot/Stoned");
+            boxOp("Pot/Stoned", 0, 100, 5, 60, "|cffFFBB00Health Percentage to use at.");
+            textOp("Pot/Stoned");
+
+            
+        -- Spacer --
+        textOp(" ");
+        wrapOp("--- Interrupts ---");
+
+
+            -- Interrupt Percentage
+            checkOp("Interrupts");
+            boxOp("Interrupts", 5, 95, 5, 0, "|cffFFBB00Cast Percentage to use at.");
+            textOp("Interrupt At");
+
+        -- Spacer
+        textOp(" ");
+        wrapOp("--- Toggle Keys ---");
+
+            -- Single/Multi Toggle
+            checkOp("Rotation Mode","|cff15FF00Enables|cffFFFFFF/|cffD60000Disable |cffFFFFFFRotation Mode Toggle Key|cffFFBB00.");
+            dropOp("Rotation Mode", 4, "Toggle")
+            textOp("Rotation");
+
+            --Cooldown Key Toggle
+            checkOp("Cooldown Mode","|cff15FF00Enables|cffFFFFFF/|cffD60000Disable |cffFFFFFFCooldown Mode Toggle Key|cffFFBB00.");
+            dropOp("Cooldown Mode", 3, "Toggle")
+            textOp("Cooldowns")
+
+            --Defensive Key Toggle
+            checkOp("Defensive Mode","|cff15FF00Enables|cffFFFFFF/|cffD60000Disable |cffFFFFFFDefensive Mode Toggle Key|cffFFBB00.");
+            dropOp("Defensive Mode", 6, "Toggle")
+            textOp("Defensive")
+
+            --Interrupts Key Toggle
+            checkOp("Interrupt Mode","|cff15FF00Enables|cffFFFFFF/|cffD60000Disable |cffFFFFFFInterrupt Mode Toggle Key|cffFFBB00.");
+            dropOp("Interrupt Mode", 6, "Toggle")
+            textOp("Interrupts")
+
+            
+
+        -- General Configs
+        CreateGeneralsConfig();
+
+        WrapsManager();
+    --end
+end
+
+end
