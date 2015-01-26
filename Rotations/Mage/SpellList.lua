a68f00c3a13b1b2365b93100bfe0c225171e4a84
GabbzGit <mikael.x.eneqvist@gmail.com>
2015-01-26 18:09:15 +0100

    Mage Updates

--- Rotations/Mage/SpellList.lua
+++ Rotations/Mage/SpellList.lua
@@ -21,12 +21,14 @@ if select(3, UnitClass("player")) == 8 then
 	ThermalVoid		= 155149;
 
 -- Fire
-	BlastWave		= 157981;
-	Combustion		= 11129;
-	Fireball		= 133;
-	Ignite			= 12654; --12846 (mastery: ignite)
-	InfernoBlast	= 108853;
-	Pyroblast		= 11366;
+	BlastWave		= 157981
+	Combustion		= 11129
+	Fireball		= 133
+	Ignite			= 12654 --12846 (mastery: ignite)
+	InfernoBlast	= 108853
+	Pyroblast		= 11366
+	Scorch          = 2948
+	LivingBomb		= 44457
 -- FiTalents
 
 -- Arcane
@@ -68,9 +70,9 @@ if select(3, UnitClass("player")) == 8 then
 
 	-- fire
 	EnhancedPyro	= 157644
-	HeatingUp		= 48107;
-	PyroblastP		= 48108;
-	Pyromaniac		= 166868;
+	HeatingUp		= 48107
+	PyroblastBuff	= 48108
+	Pyromaniac		= 166868
 
 	-- arcane
 	ArcaneAffinity	= 166871;
@@ -84,6 +86,7 @@ if select(3, UnitClass("player")) == 8 then
 	RuneOfPower		= 116011;
 	HowlingSoul		= 177046;  -- trinket 119194
 	MarkOfTheT		= 159234;
+	IncantersFlow 	= 1463
 
 
 -- Debuffs
@@ -124,6 +127,56 @@ if select(3, UnitClass("player")) == 8 then
 
 	-- Todo : Here we should look into what the best value for clipping, 50 atm, why 50? Is it related to latency or custom lag tolerance?
 	-- Further testing is needed.
+	function getIncantersFlowsDirection()
+		if not playerBuffIncantersFlowStacks then
+			playerBuffIncantersFlowDirection = "Unknown"
+			playerBuffIncantersFlowStacks = 0
+			return playerBuffIncantersFlowDirection
+		end
+		if not lastIncantersFlowStacks then
+			lastIncantersFlowStacks = playerBuffIncantersFlowStacks
+			playerBuffIncantersFlowDirection = "Unknown"
+		end
+		if lastIncantersFlowStacks > playerBuffIncantersFlowStacks then
+			playerBuffIncantersFlowDirection = "Up"
+			return playerBuffIncantersFlowDirection
+		end
+
+		if lastIncantersFlowStacks < playerBuffIncantersFlowStacks then
+			playerBuffIncantersFlowDirection = "Down"
+			return playerBuffIncantersFlowDirection
+		end
+
+		if lastIncantersFlowStacks == playerBuffIncantersFlowStacks then
+			return playerBuffIncantersFlowDirection
+		end
+
+	end
+
+	function getNumberOfTargetsWithOutLivingBomb(table)
+		local counter = 0
+		for i=1,#table do
+			local thisUnit = table[i]
+			-- increase counter for each occurences
+			if not (UnitDebuffID(thisUnit,LivingBomb,"player")) then
+				counter = counter + 1
+			end
+		end
+  		return counter
+	end
+
+	function getNumberOfTargetsWithOutCombustion(table)
+		local counter = 0
+		for i=1,#table do
+			local thisUnit = table[i]
+			-- increase counter for each occurences
+			if not (UnitDebuffID(thisUnit,Combustion,"player")) then
+				counter = counter + 1
+			end
+		end
+  		return counter
+	end
+
 	function isItOkToClipp()
 		local  name, _, _, _, _, endTime = UnitChannelInfo("player")
 		if name then
@@ -154,6 +207,76 @@ if select(3, UnitClass("player")) == 8 then
 
 	function castArcaneBlast(target)
 		if castSpell(target,ArcaneBlast,false,true) then
+			return true
+		end
+	end
+
+	function castArcaneOrb(target, maxArcaneCharges)
+		if target and (maxArcaneCharges <= arcaneCharge) then
+			if castSpell(target,ArcaneOrb,false,true) then
+				return true
+			end
+		end
+	end
+
+	function castArcaneBarrage(target, maxArcaneCharges)
+		if target and arcaneCharge >= maxArcaneCharges then
+			if castSpell("target",ArcaneBarrage,false,false) then
+				return true
+			end
+		end
+	end
+
+	function castIceFloes()
+		if not IceFloesTimer or IceFloesTimer < GetTime() - 2 then
+			if castSpell("player",IceFloes,true,false) then
+				IceFloesTimer = GetTime()
+				return true
+			end 
+		end
+	end
+	function castScorch(target)
+		if castSpell(target, Scorch,false,false) then
+			return true
+		end 
+	end
+
+	function castFireball(target)
+		if castSpell(target, Fireball,false,true) then
+			return true
+		end 
+	end
+	function castInfernoBlast(target)
+		if castSpell(target, InfernoBlast,false,false) then
+			return true
+		end 
+	end
+	function castLivingBomb(target)
+		-- # Living Bomb application
+		-- actions.living_bomb=inferno_blast,cycle_targets=1,if=dot.living_bomb.ticking&active_dot.living_bomb<active_enemies
+		if targetDebuffLivingBombRemain > 0 and targetNumberOfEnemiesinLBRange > 0 then 
+			if castSpell(target, InfernoBlast, false, false) then
+				return true
+			end
+		end 
+
+		-- actions.living_bomb+=/living_bomb,cycle_targets=1, if=target!=prismatic_crystal&(active_dot.living_bomb=0|(ticking&active_dot.living_bomb=1))&(((!talent.incanters_flow.enabled|incanters_flow_dir<0|buff.incanters_flow.stack=5)&remains<3.6)|((incanters_flow_dir>0|buff.incanters_flow.stack=1)&remains<gcd.max))&target.time_to_die>remains+12
+		if targetName ~= "Prismatic Crystal" and targetTimeToDie > (12 + targetDebuffLivingBombRemain) and targetDebuffLivingBombRemain < 3.6 then
+			if (not spellIncantersFlowIsKnown) or (playerBuffIncantersFlowDirection == "Down") or (playerBuffIncantersFlowStacks == 5)  then
+				if castSpell(target, LivingBomb, true, false) then
+					return true
+				end
+			end
+			if playerBuffIncantersFlowDirection == "Up" or playerBuffIncantersFlowStacks == 1 and targetDebuffLivingBombRemain < 1 then
+				if castSpell(target, LivingBomb, true, false) then
+					return true
+				end
+			end
+		end
+	end
+
+	function castPyroBlast(target)
+		if castSpell(target, PyroBlast, false, false, true, true) then
 			return true
 		end
 	end
