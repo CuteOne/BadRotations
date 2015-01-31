6b3e2d7147106eaa4b2642fcaf25d9ea138a9403
Mavmins <rom_king@hotmail.com>
2015-01-31 14:10:49 +0000

    Surv trap tweak

--- Rotations/Hunter/Survival/Survival.lua
+++ Rotations/Hunter/Survival/Survival.lua
@@ -311,7 +311,7 @@ if select(3, UnitClass("player")) == 3 then
 
 				-- actions.aoe+=/explosive_trap
 				if (isKnown(82939) and isChecked("Explosive Trap") and getSpellCD(82939) == 0) and UnitBuffID("player",77769) ~= nil and isMoving("target") == false then 
-					if castGround(dyn40,82939,40) then return end
+					if castGround(dyn40,82939,36) then return end
 				end
 
 				-- actions.aoe+=/a_murder_of_crows
@@ -424,7 +424,7 @@ if select(3, UnitClass("player")) == 3 then
 
 			-- actions+=/explosive_trap
 			if (isKnown(82939) and isChecked("Explosive Trap") and getSpellCD(82939) == 0) and isMoving("target") == false then 
-				if castGround(dyn40,82939,40) then return end
+				if castGround(dyn40,82939,36) then return end
 			end
 
 			-- actions+=/cobra_shot,if=buff.pre_steady_focus.up&buff.steady_focus.remains<5&(14+cast_regen)<=focus.deficit<80
