4e84adbedc9765e046e6b78526fb18102df7c010
Ingo <ingo@glasenhardt.com>
2015-02-26 16:42:42 +0100

    Oregorger LoS table entry
    not tested. should work

--- System/GeneralFunctions.lua
+++ System/GeneralFunctions.lua
@@ -1008,6 +1008,8 @@ function getLineOfSight(Unit1,Unit2)
 	local skipLoSTable = {
 		76585,-- Ragewing
 		77692, -- Kromog
+		77182, -- Oregorger
+		86644, -- Ore Crate from Oregorger boss
 	}
 	for i = 1,#skipLoSTable do
 		if getUnitID(Unit1) == skipLoSTable[i] or getUnitID(Unit2) == skipLoSTable[i] then
