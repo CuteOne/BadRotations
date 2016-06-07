edfe8a49b27eb11a9d66c6c7a1bd11fd12bff830
averykey <jackavoden@live.com>
2016-06-07 06:22:11 -0500

    getClass(), getClassType(), playerCanUseItem() return correct values now

--- Custom/foodflaskpot.lua
+++ Custom/foodflaskpot.lua
@@ -16,14 +16,14 @@ local intellectFlaskBuff = bb.player.flask.wod.buff.intellectBig
 local strengthFlaskBuff = bb.player.flask.wod.buff.strengthBig
 local staminaFlaskBuff = bb.player.flask.wod.buff.staminaBig
 
-local agilityFood = 
-local intellectFood =
-local strengthFood =
-local staminaFood =
-local agilityFoodBuff = 
-local intellectFoodBuff =
-local strengthFoodBuff =
-local staminaFoodBuff =
+local agilityFood = 0
+local intellectFood = 0
+local strengthFood = 0
+local staminaFood = 0
+local agilityFoodBuff = 0 
+local intellectFoodBuff = 0
+local strengthFoodBuff = 0
+local staminaFoodBuff = 0
 		
 --playerHasBuff(x) @param spellID # returns true if player has buff x
 function playerHasBuff(spellID)
@@ -40,69 +40,69 @@ function playerHasBuff(spellID)
     return false
 end
 
---getClass # returns class 
+--getClass # returns class as string
 function getClass()
-	local myClass = nil
+	local myClass = "nil"
 	local class = select(3, UnitClass("player"))
 	if class == 1 then -- Warrior
-		myClass = Warrior
+		myClass = "Warrior"
 	elseif class == 2 then -- Paladin
-		myClass = Paladin
+		myClass = "Paladin"
 	elseif class == 3 then -- Hunter
-		myClass = Hunter
+		myClass = "Hunter"
 	elseif class == 4 then -- Rogue
-		myClass = Rogue
+		myClass = "Rogue"
 	elseif class == 5 then -- Priest
-		myClass = Priest
+		myClass = "Priest"
 	elseif class == 6 then -- DeathKnight
-		myClass = DeathKnight
+		myClass = "DeathKnight"
 	elseif class == 7 then -- Shaman
-		myClass = Shaman
+		myClass = "Shaman"
 	elseif class == 8 then -- Mage
-		myClass = Mage
+		myClass = "Mage"
 	elseif class == 9 then -- Warlock
-		myClass = Warlock
+		myClass = "Warlock"
 	elseif class == 10 then -- Monk
-		myClass = Monk
+		myClass = "Monk"
 	elseif class == 11 then -- Druid
-		myClass = Druid
+		myClass = "Druid"
 	end
 	return myClass
 end
 
---getClassType # returns class type
+--getClassType # returns class type as string
 function getClassType()
-	local classType = nil
+	local classType = "nil"
 	local myClass = getClass()
-	local mySpec = GetSpecialization("player")
+	local mySpec = GetSpecialization()
 	--Agility
-	if myClass == Hunter 
-	or myClass == Rogue
-	or (myClass == Shaman and mySpec == 2)
-	or (myClass == Monk and mySpec == 3)
-	or (myClass == Druid and mySpec == 2) then
-		classType = agilityClass
+	if myClass == "Hunter" 
+	or myClass == "Rogue"
+	or (myClass == "Shaman" and mySpec == 2)
+	or (myClass == "Monk" and mySpec == 3)
+	or (myClass == "Druid" and mySpec == 2) then
+		classType = "agilityClass"
 	--Intellect
-	elseif (myClass == Paladin and mySpec == 1)
-	or myClass == Priest
-	or (myClass == Shaman and (mySpec == 1 or mySpec == 3))
-	or myClass == Mage
-	or myClass == Warlock
-	or (myClass == Monk and mySpec == 2)
-	or (myClass == Druid and (mySpec == 1 or mySpec == 4)) then
-		classType = intellectClass
+	elseif (myClass == "Paladin" and mySpec == 1)
+	or myClass == "Priest"
+	or (myClass == "Shaman" and (mySpec == 1 or mySpec == 3))
+	or myClass == "Mage"
+	or myClass == "Warlock"
+	or (myClass == "Monk" and mySpec == 2)
+	or (myClass == "Druid" and (mySpec == 1 or mySpec == 4)) then
+		classType = "intellectClass"
 	--Strength
-	elseif (myClass == Warrior and (mySpec == 1 or mySpec == 2))
-	or (myClass == Paladin and mySpec == 3) 
-	or (myClass == DeathKnight and (mySpec == 2 or mySpec == 3)) then
-		classType = strengthClass
+	elseif (myClass == "Warrior" and (mySpec == 1 or mySpec == 2))
+	or (myClass == "Paladin" and mySpec == 3) 
+	or (myClass == "DeathKnight" and (mySpec == 2 or mySpec == 3)) then
+		classType = "strengthClass"
 	--Stamina
-	elseif (myClass == Warrior and mySpec == 3)
-	or (myClass == Paladin and mySpec == 2)
-	or (myClass == DeathKnight and mySpec == 1)
-	or (myClass == Monk and mySpec == 1)
-	or (myClass == Druid and mySpec == 3) then
-		classType = staminaClass
+	elseif (myClass == "Warrior" and mySpec == 3)
+	or (myClass == "Paladin" and mySpec == 2)
+	or (myClass == "DeathKnight" and mySpec == 1)
+	or (myClass == "Monk" and mySpec == 1)
+	or (myClass == "Druid" and mySpec == 3) then
+		classType = "staminaClass"
 	end
 	return classType
 end
@@ -129,9 +129,7 @@ end
 function playerCanUseItem(itemID)
 	if itemID == nil then return false end
 	if playerHasItem(itemID) then
-		if GetItemSpell(GetInventoryItemID("player",itemID)) ~= nil then 
-		local slotItemID = GetInventoryItemID("player",itemID)
-		if GetItemCooldown(slotItemID) == 0 then
+		if GetItemCooldown(itemID) == 0 then
 			return true
 		end
 	end
@@ -143,7 +141,7 @@ function usePotion(skipSurvivability)
 	if skipSurvivability == nil then skipSurvivability = false end
 	local classType = getClassType()
 	--Agility
-	if classType == agilityClass then
+	if classType == "agilityClass" then
 		if playerCanUseItem(agilityPotion) then
 			if playerHasBuff(agilityPotionBuff) == false then
 				useItem(agilityPotion)
@@ -151,7 +149,7 @@ function usePotion(skipSurvivability)
 			end
 		end
 	--Intellect
-	elseif classType == intellectClass then
+	elseif classType == "intellectClass" then
 		if playerCanUseItem(intellectPotion) then
 			if playerHasBuff(intellectPotionBuff) == false then
 				useItem(intellectPotion)
@@ -159,7 +157,7 @@ function usePotion(skipSurvivability)
 			end
 		end
 	--Strength
-	elseif classType == strengthClass or (classType == staminaClass and skipSurvivability == true) then
+	elseif classType == "strengthClass" or (classType == "staminaClass" and skipSurvivability == true) then
 		if playerCanUseItem(strengthPotion) then
 			if playerHasBuff(strengthPotionBuff) == false then
 				useItem(strengthPotion)
@@ -167,7 +165,7 @@ function usePotion(skipSurvivability)
 			end
 		end
 	--Armor
-	elseif classType == staminaClass and skipSurvivability == false then
+	elseif classType == "staminaClass" and skipSurvivability == false then
 		if playerCanUseItem(armorPotion) then
 			if playerHasBuff(armorPotionBuff) == false then
 				useItem(armorPotion)
@@ -183,7 +181,7 @@ function useFlask(skipSurvivability)
 	if skipSurvivability == nil then skipSurvivability = false end
 	local classType = getClassType()
 	--Agility
-	if classType == agilityClass then
+	if classType == "agilityClass" then
 		if playerCanUseItem(agilityFlask) then
 			if playerHasBuff(agilityFlaskBuff) == false then
 				useItem(agilityFlask)
@@ -191,7 +189,7 @@ function useFlask(skipSurvivability)
 			end
 		end
 	--Intellect
-	elseif classType == intellectClass then
+	elseif classType == "intellectClass" then
 		if playerCanUseItem(intellectFlask) then
 			if playerHasBuff(intellectFlaskBuff) == false then
 				useItem(intellectFlask)
@@ -199,7 +197,7 @@ function useFlask(skipSurvivability)
 			end
 		end
 	--Strength
-	elseif classType == strengthClass or (classType == staminaClass and skipSurvivability == true) then
+	elseif classType == "strengthClass" or (classType == "staminaClass" and skipSurvivability == true) then
 		if playerCanUseItem(strengthFlask) then
 			if playerHasBuff(strengthFlaskBuff) == false then
 				useItem(strengthFlask)
@@ -207,7 +205,7 @@ function useFlask(skipSurvivability)
 			end
 		end
 	--Stamina
-	elseif classType == staminaClass and skipSurvivability == false then
+	elseif classType == "staminaClass" and skipSurvivability == false then
 		if playerCanUseItem(staminaFlask) then
 			if playerHasBuff(staminaFlaskBuff) == false then
 				useItem(staminaFlask)
@@ -223,7 +221,7 @@ function useFood(skipSurvivability)
 	if skipSurvivability == nil then skipSurvivability = false end
 	local classType = getClassType()
 	--Agility
-	if classType == agilityClass then
+	if classType == "agilityClass" then
 		if playerCanUseItem(agilityFood) then
 			if playerHasBuff(agilityFoodBuff) == false then
 				useItem(agilityFood)
@@ -231,7 +229,7 @@ function useFood(skipSurvivability)
 			end
 		end
 	--Intellect
-	elseif classType == intellectClass then
+	elseif classType == "intellectClass" then
 		if playerCanUseItem(intellectFood) then
 			if playerHasBuff(intellectFoodBuff) == false then
 				useItem(intellectFood)
@@ -239,7 +237,7 @@ function useFood(skipSurvivability)
 			end
 		end
 	--Strength
-	elseif classType == strengthClass or (classType == staminaClass and skipSurvivability == true) then
+	elseif classType == "strengthClass" or (classType == "staminaClass" and skipSurvivability == true) then
 		if playerCanUseItem(strengthFood) then
 			if playerHasBuff(strengthFoodBuff) == false then
 				useItem(strengthFood)
@@ -247,7 +245,7 @@ function useFood(skipSurvivability)
 			end
 		end
 	--Stamina
-	elseif classType == staminaClass and skipSurvivability == false then
+	elseif classType == "staminaClass" and skipSurvivability == false then
 		if playerCanUseItem(staminaFood) then
 			if playerHasBuff(staminaFoodBuff) == false then
 				useItem(staminaFood)
