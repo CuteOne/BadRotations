2e6ff059c78545360b293ab3096fa2919dbce822
ph34rt3hcute1 <ph34rt3hcute1@gmail.com>
2016-11-23 23:58:15 -0500

    Fix - Destruction function call erorr

--- Rotations/Warlock/Destruction/Destruction.lua
+++ Rotations/Warlock/Destruction/Destruction.lua
@@ -1,34 +1,12 @@
-if select(3, UnitClass("player")) == 9 then
-    function DestructionWarlock()
-        if Currentconfig ~= "Destruction Warlock" and destructionWarlock == nil then
-            destructionWarlock = cDestruction:new()
-            setmetatable(destructionWarlock, {__index = cDestruction})
-            destructionWarlock:update()
-            --KeyToggles()
-            if destructionWarlock.rotation == 1 then 
-              --  DestructionToggles()
-               -- DestructionConfig() 
-            end
-            Currentconfig = "Destruction Warlock"
-        end
+function WarlockDestruction()
+    if br.player == nil or br.player.profile ~= "Destruction" then
+        br.player = cDestruction:new("Destruction")
+        setmetatable(br.player, {__index = cDestruction})
 
-        if not canRun() then
-            return true
-        end
+        br.player:createOptions()
+        br.player:createToggles()
+        br.player:update()
+    end
 
-        -- ToDo add pause toggle
-        -- Manual Input
-        -- if IsLeftShiftKeyDown() then -- Pause the script, keybind in wow shift+1 etc for manual cast
-        -- return true
-        -- end
-        -- if IsLeftControlKeyDown() then -- Pause the script, keybind in wow ctrl+1 etc for manual cast
-        -- return true
-        -- end
-        -- if IsLeftAltKeyDown() then
-        --     return true
-        -- end
-
-        destructionWarlock:update()
-
-    end --Warlock Function End
-end --Class Check End
+    br.player:update()
+end
