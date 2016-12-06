7040dbcf3a2db510fd44c9080f7800652299f1d6
ph34rt3hcute1 <ph34rt3hcute1@gmail.com>
2016-12-06 14:23:28 -0500

    Outlaw - Stop Bribing non-enemies....they don't want your money!

--- Rotations/Rogue/Outlaw/OutlawCuteOne.lua
+++ Rotations/Rogue/Outlaw/OutlawCuteOne.lua
@@ -316,7 +316,7 @@ local function runRotation()
                 end
             end
     -- Bribe
-            if isChecked("Bribe") and UnitCreatureType(units.dyn30) == "Humanoid" and not isDummy(units.dyn30) then
+            if isChecked("Bribe") and UnitCreatureType(units.dyn30) == "Humanoid" and not isDummy(units.dyn30) and UnitIsEnemy(units.dyn30,"player") then
                 if cast.bribe(units.dyn30) then return end
             end
     -- Grappling Hook
