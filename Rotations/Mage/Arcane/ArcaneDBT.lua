fbb467769bc846aabba1cdc7fe336432b63a544a
ph34rt3hcute1 <ph34rt3hcute1@gmail.com>
2016-12-31 21:47:24 -0500

    Added ArcaneDBT Rotation

---
+++ Rotations/Mage/Arcane/ArcaneDBT.lua
@@ -0,0 +1,376 @@
+local rotationName = "CuteOne"
+
+---------------
+--- Toggles ---
+---------------
+local function createToggles()
+-- Rotation Button
+    RotationModes = {
+        [1] = { mode = "Auto", value = 1 , overlay = "Automatic Rotation", tip = "Swaps between Single and Multiple based on number of targets in range.", highlight = 1, icon = br.player.spell.flamestrike},
+        [2] = { mode = "Mult", value = 2 , overlay = "Multiple Target Rotation", tip = "Multiple target rotation used.", highlight = 0, icon = br.player.spell.flamestrike},
+        [3] = { mode = "Sing", value = 3 , overlay = "Single Target Rotation", tip = "Single target rotation used.", highlight = 0, icon = br.player.spell.pyroblast},
+        [4] = { mode = "Off", value = 4 , overlay = "DPS Rotation Disabled", tip = "Disable DPS Rotation", highlight = 0, icon = br.player.spell.iceBlock}
+    };
+    CreateButton("Rotation",1,0)
+-- Cooldown Button
+    CooldownModes = {
+        [1] = { mode = "Auto", value = 1 , overlay = "Cooldowns Automated", tip = "Automatic Cooldowns - Boss Detection.", highlight = 1, icon = br.player.spell.combustion},
+        [2] = { mode = "On", value = 1 , overlay = "Cooldowns Enabled", tip = "Cooldowns used regardless of target.", highlight = 0, icon = br.player.spell.combustion},
+        [3] = { mode = "Off", value = 3 , overlay = "Cooldowns Disabled", tip = "No Cooldowns will be used.", highlight = 0, icon = br.player.spell.combustion}
+    };
+    CreateButton("Cooldown",2,0)
+-- Defensive Button
+    DefensiveModes = {
+        [1] = { mode = "On", value = 1 , overlay = "Defensive Enabled", tip = "Includes Defensive Cooldowns.", highlight = 1, icon = br.player.spell.iceBarrier},
+        [2] = { mode = "Off", value = 2 , overlay = "Defensive Disabled", tip = "No Defensives will be used.", highlight = 0, icon = br.player.spell.iceBarrier}
+    };
+    CreateButton("Defensive",3,0)
+-- Interrupt Button
+    InterruptModes = {
+        [1] = { mode = "On", value = 1 , overlay = "Interrupts Enabled", tip = "Includes Basic Interrupts.", highlight = 1, icon = br.player.spell.counterspell},
+        [2] = { mode = "Off", value = 2 , overlay = "Interrupts Disabled", tip = "No Interrupts will be used.", highlight = 0, icon = br.player.spell.counterspell}
+    };
+    CreateButton("Interrupt",4,0)
+end
+
+---------------
+--- OPTIONS ---
+---------------
+local function createOptions()
+    local optionTable
+
+    local function rotationOptions()
+        local section
+    -- General Options
+        section = br.ui:createSection(br.ui.window.profile, "General")
+        -- APL
+            br.ui:createDropdownWithout(section, "APL Mode", {"|cffFFFFFFSimC","|cffFFFFFFAMR","|cff00ccffDBT","|cffff0000Testing"}, 1, "|cffFFFFFFSet APL Mode to use.")
+        -- Dummy DPS Test
+            br.ui:createSpinner(section, "DPS Testing",  5,  5,  60,  5,  "|cffFFFFFFSet to desired time for test in minuts. Min: 5 / Max: 60 / Interval: 5")
+        -- Pre-Pull Timer
+            br.ui:createSpinner(section, "Pre-Pull Timer",  5,  1,  10,  1,  "|cffFFFFFFSet to desired time to start Pre-Pull (DBM Required). Min: 1 / Max: 10 / Interval: 1")
+        -- Artifact
+            br.ui:createDropdownWithout(section,"Artifact", {"|cff00FF00Everything","|cffFFFF00Cooldowns","|cffFF0000Never"}, 1, "|cffFFFFFFWhen to use Artifact Ability.")
+        br.ui:checkSectionState(section)
+    -- Cooldown Options
+        section = br.ui:createSection(br.ui.window.profile, "Cooldowns")
+        -- Racial
+            br.ui:createCheckbox(section,"Racial")
+        -- Trinkets
+            br.ui:createCheckbox(section,"Trinkets")
+        -- Mirror Image
+            br.ui:createCheckbox(section,"Mirror Image")
+        br.ui:checkSectionState(section)
+    -- Defensive Options
+        section = br.ui:createSection(br.ui.window.profile, "Defensive")
+        -- Healthstone
+            br.ui:createSpinner(section, "Pot/Stoned",  60,  0,  100,  5,  "|cffFFFFFFHealth Percent to Cast At")
+        -- Heirloom Neck
+            br.ui:createSpinner(section, "Heirloom Neck",  60,  0,  100,  5,  "|cffFFBB00Health Percentage to use at.");
+        -- Gift of The Naaru
+            if br.player.race == "Draenei" then
+                br.ui:createSpinner(section, "Gift of the Naaru",  50,  0,  100,  5,  "|cffFFFFFFHealth Percent to Cast At")
+            end
+        -- Frost Nova
+            br.ui:createSpinner(section, "Frost Nova",  50,  0,  100,  5,  "|cffFFBB00Health Percentage to use at.");
+        br.ui:checkSectionState(section)
+    -- Interrupt Options
+        section = br.ui:createSection(br.ui.window.profile, "Interrupts")
+        -- Couterspell
+            br.ui:createCheckbox(section, "Counterspell")
+        -- Interrupt Percentage
+            br.ui:createSpinner(section, "Interrupt At",  0,  0,  95,  5,  "|cffFFFFFFCast Percent to Cast At")
+        br.ui:checkSectionState(section)
+    -- Toggle Key Options
+        section = br.ui:createSection(br.ui.window.profile, "Toggle Keys")
+        -- Single/Multi Toggle
+            br.ui:createDropdown(section, "Rotation Mode", br.dropOptions.Toggle,  4)
+        -- Cooldown Key Toggle
+            br.ui:createDropdown(section, "Cooldown Mode", br.dropOptions.Toggle,  3)
+        -- Defensive Key Toggle
+            br.ui:createDropdown(section, "Defensive Mode", br.dropOptions.Toggle,  6)
+        -- Interrupts Key Toggle
+            br.ui:createDropdown(section, "Interrupt Mode", br.dropOptions.Toggle,  6)
+        -- Pause Toggle
+            br.ui:createDropdown(section, "Pause Mode", br.dropOptions.Toggle,  6)
+        br.ui:checkSectionState(section)
+    end
+    optionTable = {{
+        [1] = "Rotation Options",
+        [2] = rotationOptions,
+    }}
+    return optionTable
+end
+
+----------------
+--- ROTATION ---
+----------------
+local function runRotation()
+    if br.timer:useTimer("debugArcane", math.random(0.15,0.3)) then
+        --Print("Running: "..rotationName)
+
+---------------
+--- Toggles ---
+---------------
+        UpdateToggle("Rotation",0.25)
+        UpdateToggle("Cooldown",0.25)
+        UpdateToggle("Defensive",0.25)
+        UpdateToggle("Interrupt",0.25)
+
+--------------
+--- Locals ---
+--------------
+        local addsExist                                     = false
+        local addsIn                                        = 999
+        local activePet                                     = br.player.pet
+        local activePetId                                   = br.player.petId
+        local artifact                                      = br.player.artifact
+        local buff                                          = br.player.buff
+        local cast                                          = br.player.cast
+        local castable                                      = br.player.cast.debug
+        local combatTime                                    = getCombatTime()
+        local cd                                            = br.player.cd
+        local charges                                       = br.player.charges
+        local deadMouse                                     = UnitIsDeadOrGhost("mouseover")
+        local deadtar, attacktar, hastar, playertar         = deadtar or UnitIsDeadOrGhost("target"), attacktar or UnitCanAttack("target", "player"), hastar or ObjectExists("target"), UnitIsPlayer("target")
+        local debuff                                        = br.player.debuff
+        local enemies                                       = br.player.enemies
+        local falling, swimming, flying, moving             = getFallTime(), IsSwimming(), IsFlying(), GetUnitSpeed("player")>0
+        local flaskBuff                                     = getBuffRemain("player",br.player.flask.wod.buff.agilityBig)
+        local friendly                                      = friendly or UnitIsFriend("target", "player")
+        local gcd                                           = br.player.gcd
+        local hasMouse                                      = ObjectExists("mouseover")
+        local hasPet                                        = IsPetActive()
+        local hasteAmount                                   = GetHaste()/100
+        local healPot                                       = getHealthPot()
+        local inCombat                                      = br.player.inCombat
+        local inInstance                                    = br.player.instance=="party"
+        local inRaid                                        = br.player.instance=="raid"
+        local lastSpell                                     = lastSpellCast
+        local level                                         = br.player.level
+        local lootDelay                                     = getOptionValue("LootDelay")
+        local lowestHP                                      = br.friend[1].unit
+        local mode                                          = br.player.mode
+        local moveIn                                        = 999
+        local moving                                        = isMoving("player")
+        local perk                                          = br.player.perk
+        local petInfo                                       = br.player.petInfo
+        local php                                           = br.player.health
+        local playerMouse                                   = UnitIsPlayer("mouseover")
+        local power, powmax, powgen, powerDeficit           = br.player.power.amount.mana, br.player.power.mana.max, br.player.power.regen, br.player.power.mana.deficit
+        local pullTimer                                     = br.DBM:getPulltimer()
+        local racial                                        = br.player.getRacial()
+        local recharge                                      = br.player.recharge
+        local solo                                          = br.player.instance=="none"
+        local spell                                         = br.player.spell
+        local talent                                        = br.player.talent
+        local ttd                                           = getTTD
+        local ttm                                           = br.player.power.ttm
+        local units                                         = br.player.units
+        local dt                                            = date("%H:%M:%S")
+        local debug                                         = false
+
+        if leftCombat == nil then leftCombat = GetTime() end
+        if profileStop == nil then profileStop = false end
+        -- if artifact.icyHand then iceHand= 1 else iceHand = 0 end
+       
+--------------------
+--- Action Lists ---
+--------------------
+    -- Action List - Extras
+        local function actionList_Extras()
+        -- Dummy Test
+            if isChecked("DPS Testing") then
+                if ObjectExists("target") then
+                    if getCombatTime() >= (tonumber(getOptionValue("DPS Testing"))*60) and isDummy() then
+                        StopAttack()
+                        ClearTarget()
+                        Print(tonumber(getOptionValue("DPS Testing")) .." Minute Dummy Test Concluded - Profile Stopped")
+                        profileStop = true
+                    end
+                end
+            end -- End Dummy Test
+
+        end -- End Action List - Extras
+    -- Action List - Defensive
+        local function actionList_Defensive()
+            if useDefensive() then
+        -- Pot/Stoned
+                if isChecked("Pot/Stoned") and php <= getOptionValue("Pot/Stoned")
+                    and inCombat and (hasHealthPot() or hasItem(5512))
+                then
+                    if canUse(5512) then
+                        useItem(5512)
+                    elseif canUse(healPot) then
+                        useItem(healPot)
+                    end
+                end
+        -- Heirloom Neck
+                if isChecked("Heirloom Neck") and php <= getOptionValue("Heirloom Neck") then
+                    if hasEquiped(122668) then
+                        if GetItemCooldown(122668)==0 then
+                            useItem(122668)
+                        end
+                    end
+                end
+        -- Gift of the Naaru
+                if isChecked("Gift of the Naaru") and php <= getOptionValue("Gift of the Naaru") and php > 0 and br.player.race == "Draenei" then
+                    if castSpell("player",racial,false,false,false) then return end
+                end
+        -- Frost Nova
+                if isChecked("Frost Nova") and php <= getOptionValue("Frost Nova") and #enemies.yards12 > 0 then
+                    if cast.frostNova() then return end
+                end
+            end -- End Defensive Toggle
+        end -- End Action List - Defensive
+    -- Action List - Interrupts
+        local function actionList_Interrupts()
+            if useInterrupts() then
+                for i=1, #enemies.yards30 do
+                    thisUnit = enemies.yards30[i]
+                    if canInterrupt(thisUnit,getOptionValue("Interrupt At")) then
+        -- Counterspell
+                        if isChecked("Counterspell") then
+                            if cast.counterspell(thisUnit) then return end
+                        end
+                    end
+                end
+            end -- End useInterrupts check
+        end -- End Action List - Interrupts
+    -- Action List - Cooldowns
+        local function actionList_Cooldowns()
+            if useCDs() and getDistance(units.dyn40) < 40 then
+        -- Rune of Power
+                -- rune_of_power,if=cooldown.icy_veins.remains<cast_time|charges_fractional>1.9&cooldown.icy_veins.remains>10|buff.icy_veins.up|target.time_to_die.remains+5<charges_fractional*10
+                -- TODO
+        -- Potion
+                -- potion,name=deadly_grace
+                -- TODO
+        -- Arcane Power
+                -- icy_veins,if=buff.icy_veins.down
+                -- TODO
+        -- Mirror Image
+                -- mirror_image
+                if isChecked("Mirror Image") then
+                    if cast.mirrorImage() then return end
+                end
+        -- Use Neck
+                -- use_item,slot=neck
+                -- TODO
+        -- Trinkets
+                if isChecked("Trinkets") then
+                    if canUse(13) then
+                        useItem(13)
+                    end
+                    if canUse(14) then
+                        useItem(14)
+                    end
+                end
+        -- Racial: Orc Blood Fury | Troll Berserking | Blood Elf Arcane Torrent
+                -- blood_fury | berserking | arcane_torrent
+                if isChecked("Racial") and (br.player.race == "Orc" or br.player.race == "Troll" or br.player.race == "Blood Elf") then
+                    if castSpell("player",racial,false,false,false) then return end
+                end
+            end -- End useCDs check
+        end -- End Action List - Cooldowns
+    -- Action List - AOE    
+        local function actionList_AOE()
+        -- AOE
+        end
+        local function actionList_Moving()
+        -- Moving  
+        end
+    -- Action List - PreCombat
+        local function actionList_PreCombat()
+            if not inCombat and not (IsFlying() or IsMounted()) then
+            -- Flask
+                -- flask,type=flask_of_the_whispered_pact
+                -- TODO
+            -- Food
+                -- food,type=azshari_salad
+            -- Augmentation
+                -- augmentation,type=defile
+                if isChecked("Pre-Pull Timer") and pullTimer <= getOptionValue("Pre-Pull Timer") then
+
+                end -- End Pre-Pull
+                if isValidUnit("target") and getDistance("target") < 40 then
+            -- Mirror Image
+                    -- mirror_image
+                    if isChecked("Mirror Image") then
+                        if cast.mirrorImage() then return end
+                    end
+            -- Potion
+                    -- potion,name=deadly_grace
+                    -- TODO
+            -- Frostbolt
+                    -- frostbolt
+                    if br.timer:useTimer("delayFB", getCastTime(spell.frostbolt)+0.5) then
+                        if cast.frostbolt("target") then return end
+                    end
+                end
+            end -- End No Combat
+        end -- End Action List - PreCombat
+---------------------
+--- Begin Profile ---
+---------------------
+    -- Profile Stop | Pause
+        if not inCombat and not hastar and profileStop==true then
+            profileStop = false
+        elseif (inCombat and profileStop==true) or pause() or mode.rotation==4 then
+            return true
+        else
+-----------------------
+--- Extras Rotation ---
+-----------------------
+            if actionList_Extras() then return end
+--------------------------
+--- Defensive Rotation ---
+--------------------------
+            if actionList_Defensive() then return end
+------------------------------
+--- Out of Combat Rotation ---
+------------------------------
+            if actionList_PreCombat() then
+            end
+--------------------------
+--- In Combat Rotation ---
+--------------------------
+            if inCombat and profileStop==false and isValidUnit(units.dyn40) and getDistance(units.dyn40) < 40 then
+    ------------------------------
+    --- In Combat - Interrupts ---
+    ------------------------------
+            if actionList_Interrupts() then return end
+
+    ---------------------------
+    --- SimulationCraft APL ---
+    ---------------------------
+            if getOptionValue("APL Mode") == 1 then
+            end -- End SimC APL
+	----------------------
+	--- Start AMR APL ---
+	----------------------
+        	if getOptionValue("APL Mode") == 2 then
+            end -- End AMR APL
+	----------------------
+    --- Start DBT APL ---
+    ----------------------
+            if getOptionValue("APL Mode") == 3 then
+            end -- End DBT APL
+	----------------------
+	--- Start Testing APL ---
+	----------------------
+            if getOptionValue("APL Mode") == 4 then
+            -- Test Ground for Casts
+            end -- End Testing APL
+         	end --End In Combat
+        end --End Rotation Logic
+    end -- End Timer
+end -- End runRotation
+local id = 62
+if br.rotations[id] == nil then br.rotations[id] = {} end
+tinsert(br.rotations[id],{
+    name = rotationName,
+    toggles = createToggles,
+    options = createOptions,
+    run = runRotation,
+})
