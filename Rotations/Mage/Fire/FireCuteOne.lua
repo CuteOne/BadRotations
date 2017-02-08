c163263e6d69784fd6fb2e6515ecac30c03c1485
SvsGit <svasya@ro.ru>
2017-02-08 11:39:13 +0300

    Fire Mage Update
    Blazing Barrier is back, don't you dare touch it again! Added Meteor TTD
    check and changed Flamestrike prio for kickass AoE.

--- Rotations/Mage/Fire/FireCuteOne.lua
+++ Rotations/Mage/Fire/FireCuteOne.lua
@@ -73,6 +73,8 @@ local function createOptions()
             end
         -- Frost Nova
             br.ui:createSpinner(section, "Frost Nova",  50,  0,  100,  5,  "|cffFFBB00Health Percentage to use at.");
+        -- Blazing Barrier
+            br.ui:createCheckbox(section,"Blazing Barrier")
         br.ui:checkSectionState(section)
     -- Interrupt Options
         section = br.ui:createSection(br.ui.window.profile, "Interrupts")
@@ -288,8 +290,8 @@ local function runRotation()
                 if cast.blastWave() then return end
             end
         -- Meteor
-            -- meteor,if=cooldown.combustion.remain()s>30|(cooldown.combustion.remain()s>target.time_to_die)|buff.rune_of_power.up
-            if cd.combustion > 30 or (cd.combustion > ttd("target")) or buff.runeOfPower.exists() then
+            -- meteor,if=cooldown.combustion.remains>30|(cooldown.combustion.remains>target.time_to_die)|buff.rune_of_power.up
+            if (cd.combustion > 30 or (cd.combustion > ttd("target")) or buff.runeOfPower.exists()) and ttd("target") > 8 then
                 if cast.meteor() then return end
             end
         -- Cinderstorm
@@ -497,6 +499,17 @@ local function runRotation()
     --- SimulationCraft APL ---
     ---------------------------
                 if getOptionValue("APL Mode") == 1 then
+        -- Blazing Barrier
+                    if isChecked("Blazing Barrier") and not buff.blazingBarrier.exists() then
+                        if cast.blazingBarrier() then return end
+                    end
+        -- Flamestrike
+                    -- flamestrike,if=talent.flame_patch.enabled&active_enemies>2&buff.hot_streak.react
+                    if ((#enemies.yards8t >= 3 and mode.rotation == 1) or mode.rotation == 2) and buff.hotStreak.exists() then
+                        if cast.flamestrike("best",nil,2,8) then return end
+                    elseif ((#enemies.yards8t >= 3 and mode.rotation == 1) or mode.rotation == 2) and buff.hotStreak.exists() and not talent.pyromaniac then
+                        if cast.flamestrike("best",nil,2,8) then return end
+                    end
         -- Mirror Image
                     -- mirror_image,if=buff.combustion.down
                     if isChecked("Mirror Image") and not buff.combustion.exists() then
