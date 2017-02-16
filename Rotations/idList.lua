57db4c4f9c691d69228c0b9961a886f38f080d6e
Hasanuddin Abu Bakar <hasan@openkod.com>
2017-02-17 00:13:18 +0800

    Fix Angelic Feather again. Only cast to yourself on CD

--- Rotations/idList.lua
+++ Rotations/idList.lua
@@ -1623,6 +1623,7 @@ br.idList = {
 
             },
             buffs                           = {
+                angelicFeather              = 121557,
                 atonement                   = 194384,
                 overloadedWithLight         = 223166,
                 powerInfusion               = 10060,
