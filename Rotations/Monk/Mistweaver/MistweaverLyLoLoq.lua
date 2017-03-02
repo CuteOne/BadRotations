b99c5a6dca7f60c3f20ba8dc350ca05aa8347c5b
kuukuukuatchu <echidnablade@gmail.com>
2017-03-02 03:59:52 -0500

    Fixed trinket errors

--- Rotations/Monk/Mistweaver/MistweaverLyLoLoq.lua
+++ Rotations/Monk/Mistweaver/MistweaverLyLoLoq.lua
@@ -309,13 +309,13 @@ local function runRotation()
 
     local function actionList_Cooldown()
         if useCDs() then
-            if isChecked("Trinket - 1") and getLowAllies(getValue("Trinket - 1")) >= getValue("Min Trinket 1 Targets") then
+            if isChecked("Trinket 1") and getLowAllies(getValue("Trinket 1")) >= getValue("Min Trinket 1 Targets") then
                 if canUse(13) then
                     useItem(13)
                     return true
                 end
             end
-            if isChecked("Trinket - 2") and getLowAllies(getValue("Trinket - 2")) >= getValue("Min Trinket 2 Targets") then
+            if isChecked("Trinket 2") and getLowAllies(getValue("Trinket 2")) >= getValue("Min Trinket 2 Targets") then
                 if canUse(14) then
                     useItem(14)
                     return true
