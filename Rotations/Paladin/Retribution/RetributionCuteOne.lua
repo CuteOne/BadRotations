8c08df7fc1c61d0dd545c7e43e749f25c36029e2
ph34rt3hcute1 <ph34rt3hcute1@gmail.com>
2017-03-05 17:11:17 -0500

    Fury - Added Whirlwind user specify unit use

--- Rotations/Paladin/Retribution/RetributionCuteOne.lua
+++ Rotations/Paladin/Retribution/RetributionCuteOne.lua
@@ -485,7 +485,7 @@ local function runRotation()
     --Profile Stop | Pause
         if not inCombat and not hastar and profileStop == true then
             profileStop = false
-        elseif (inCombat and profileStop == true) or ((IsMounted() or IsFlying()) and not UnitBuffID("player",220507)) or pause() or mode.rotation == 4 then
+        elseif (inCombat and profileStop == true) or ((IsMounted() or IsFlying()) and not UnitBuffID("player",220507) and not buff.divineSteed.exists()) or pause() or mode.rotation == 4 then
             return true
         else
 -----------------------
