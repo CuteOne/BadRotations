497017ff140298e9918040445d873d9e8fa5a157
qj739 <qingjie@huawei.com>
2017-03-18 01:44:58 +0800

    t
    t

--- Rotations/Druid/Restoration/RestorationSvs.lua
+++ Rotations/Druid/Restoration/RestorationSvs.lua
@@ -272,6 +272,7 @@ local function runRotation()
         end -- End Action List - Extras
         -- Action List - Pre-Combat
         function actionList_PreCombat()
+			Print("test git")
             -- Rejuvenation
             if isChecked("Rejuvenation") then
                 rejuvCount = 0
