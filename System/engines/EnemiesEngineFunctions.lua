dec12c28b3829adff9d880ab4d83e4588b5c4613
kuukuukuatchu <echidnablade@gmail.com>
2017-03-24 01:36:31 -0400

    System - Added cone logic for enemies

--- System/engines/EnemiesEngineFunctions.lua
+++ System/engines/EnemiesEngineFunctions.lua
@@ -135,3 +135,26 @@ function castDispelOffensiveBuffs(spell)
 		end
 	end
 end
+-- Cone Logic for Enemies
+function getEnemiesInCone(length,angle)
+    local playerX, playerY, playerZ = GetObjectPosition("player")
+    local facing = ObjectFacing("player")
+    local units = {};
+
+    for i = 1, #br.enemy do
+        local thisUnit = br.enemy[i].unit
+        if not UnitIsUnit(thisUnit,"player") and (isDummy(thisUnit) or UnitIsEnemy(thisUnit,"player")) then
+            local unitX, unitY, unitZ = GetObjectPosition(thisUnit)
+            if playerX and unitX then
+                local angleToUnit = getAngles(playerX,playerY,playerZ,unitX,unitY,unitZ)
+                local angleDifference = facing > angleToUnit and facing - angleToUnit or angleToUnit - facing
+                local shortestAngle = angleDifference < math.pi and angleDifference or math.pi*2 - angleDifference
+                local finalAngle = shortestAngle/math.pi*180
+                if finalAngle < angle then
+                    table.insert(units, thisUnit)
+                end
+            end
+        end
+    end
+    return units
+end
