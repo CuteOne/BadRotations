d5526884e6fe2cc3e09fe5f688e1603ac35c9f00
ph34rt3hcute1 <ph34rt3hcute1@gmail.com>
2017-03-26 12:08:09 -0400

    System - Add Fel Soul to Do Not Touch unit list

--- System/engines/EnemiesEngineCollections.lua
+++ System/engines/EnemiesEngineCollections.lua
@@ -85,7 +85,9 @@ doNotTouchUnitCandidates = {
     { unitID = 95888, buff = 205004 }, -- Don't attack Cordana Felsong when she casts Vengeance
     { unitID = 95888, buff = 197422 }, -- Don't attack Cordana Felsong when she casts Creeping Doom
     { unitID = 112956, buff = 225840 }, -- Don't attack Shimmering Manaspine
-    { unitID = 104154, buff = 206516 }, -- Don't attack Gul'dan when he is in The Eye of Aman'Thul cage 
+    { unitID = 104154, buff = 206516 }, -- Don't attack Gul'dan when he is in The Eye of Aman'Thul cage
+    -- Nighthold: Mythic Spellblade - Fel Soul
+    { unitID = 115905}, 
 }
 -- list of units to stun, either always or udner certain condition such as having a buff or wirldwinding etc
 crowdControlCandidates = {
