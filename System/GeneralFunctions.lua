b06749f4b7ff1dfb2b680622e1ce6ddf32ce5b9e
Vilt-Coding <slayerteino@hotmail.com>
2017-04-24 00:52:54 +0200

    GeneralFunctions - Add support for checking slot

--- System/GeneralFunctions.lua
+++ System/GeneralFunctions.lua
@@ -3274,18 +3274,21 @@ function TierScan(thisTier)
 	return equippedItems;
 end
 
-function hasEquiped(itemID)
+function hasEquiped(ItemID, Slot)
 	--Scan Armor Slots to see if specified item was equiped
 	local foundItem = false
 	for i=1, 19 do
 		-- if there is an item in that slot
-		if GetInventoryItemID("player", i) ~= nil then
-			-- check if it matches
-			if GetInventoryItemID("player", i) == itemID then
-				foundItem = true
-			end
-		end
-	end
+        if GetInventoryItemID("player", i) ~= nil then
+        	-- check if it matches
+            if GetInventoryItemID("player", i) == ItemID then
+                if i == Slot or Slot == nil then
+                    foundItem = true
+                    break
+                end
+            end
+        end
+    end
 	return foundItem;
 end
 
