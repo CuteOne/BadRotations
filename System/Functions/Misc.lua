e100cf7146e47624a5688ffe6ac497ea5ee5ff73
ph34rt3hcute1 <ph34rt3hcute1@gmail.com>
2017-06-22 15:39:44 -0400

    System - Added Fallen Avatar to Skip LoS Check

--- System/Functions/Misc.lua
+++ System/Functions/Misc.lua
@@ -61,6 +61,7 @@ function getLineOfSight(Unit1,Unit2)
 		114901, -- Gripping Tentacle (Trials of Valor)
 		116195, -- Bilewater Slime (Trials of Valor)
 		120436, -- Fallen Avatar (Tomb of Sargeras)
+		116939. -- Fallen Avatar (Tomb of Sargeras)
 		--86644, -- Ore Crate from Oregorger boss
 	}
 	for i = 1,#skipLoSTable do
