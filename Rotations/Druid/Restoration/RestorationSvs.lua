c64e2d7c42afe147375eef6fb576adfe7fde9618
Fengshen <513292111@qq.com>
2017-06-28 10:44:15 +0800

    Fix dispell

--- Rotations/Druid/Restoration/RestorationSvs.lua
+++ Rotations/Druid/Restoration/RestorationSvs.lua
@@ -524,7 +524,7 @@ local function runRotation()
                     for n = 1,40 do
                         local buff,_,_,count,bufftype,duration = UnitDebuff(br.friend[i].unit, n)
                         if buff then
-                            if (bufftype == "Curse" or bufftype == "Magic" or bufftype == "Poison") and UnitInRange(br.friend[i].unit) and br.friend[i].dispel then
+                            if (bufftype == "Curse" or bufftype == "Magic" or bufftype == "Poison") and UnitInRange(br.friend[i].unit) then
                                 if cast.naturesCure(br.friend[i].unit) then return end
                             end
                         end
