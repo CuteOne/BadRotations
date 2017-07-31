c3ca2c5d694f62b61604146d8c73b444d11bfe1e
LSharpAura <LSharpAura@users.noreply.github.com>
2017-07-31 12:07:55 -0400

    MM - Changed T20 buff to match idList

--- Rotations/Hunter/Marksmanship/MarksmanshipAura.lua
+++ Rotations/Hunter/Marksmanship/MarksmanshipAura.lua
@@ -803,7 +803,7 @@ local function runRotation()
 			end
 		-- Arcane Shot
 			-- arcane_shot,if=spell_targets.multishot=1&(!set_bonus.tier20_2pc|!action.aimed_shot.in_flight|buff.t20_2p_critical_aimed_damage.remains>action.aimed_shot.execute_time+gcd.max)&variable.vuln_aim_casts>0&variable.can_gcd&focus+cast_regen+action.aimed_shot.cast_regen<focus.max&(!variable.pooling_for_piercing|lowest_vuln_within.5>gcd.max)
-			if #enemies.yards8t == 1 and (not t20_2pc or buff.precision.remain() > getCastTime(spell.aimedShot) + gcd) and vulnAimCast > 0 and canGCD and power + (powerRegen*getCastTime(spell.arcaneShot)) + (powerRegen*getCastTime(spell.aimedShot)) + 20 < powerMax and (not poolForPiercing or lowestVuln > gcd) then
+			if #enemies.yards8t == 1 and (not t20_2pc or buff.t20_2pc_critical_aimed.remain() > getCastTime(spell.aimedShot) + gcd) and vulnAimCast > 0 and canGCD and power + (powerRegen*getCastTime(spell.arcaneShot)) + (powerRegen*getCastTime(spell.aimedShot)) + 20 < powerMax and (not poolForPiercing or lowestVuln > gcd) then
 				if cast.arcaneShot() then return end
 			end
 		-- Aimed Shot
