ad38dcfa49b2eac3e00c7914e9c9aabace484d3c
Fiskee <fisker@gmail.com>
2018-09-21 12:25:29 +0200

    System - Add hunter dispels

--- System/Functions/Aura.lua
+++ System/Functions/Aura.lua
@@ -73,7 +73,13 @@ function canDispel(Unit,spellID)
 		if spellID == 213644 then typesList = {"Poison", "Disease"} end
 	end
 	if ClassNum == 3 then --Hunter
-		typesList = { }
+		if spellID == 264265 then typesList = { "Magic", "" } end --spiritShock
+		if spellID == 264028 then typesList = { "Magic", "" } end --chiJiTranq
+		if spellID == 264266 then typesList = { "Magic", "" } end --naturesGrace
+		if spellID == 264264 then typesList = { "Magic", "" } end --netherShock
+		if spellID == 264263 then typesList = { "Magic", "" } end --sonicBlast
+		if spellID == 254262 then typesList = { "Magic", "" } end --soothingWater
+		if spellID == 254056 then typesList = { "Magic", "" } end --sporeCloud
 	end
 	if ClassNum == 4 then --Rogue
 		-- Cloak of Shadows
