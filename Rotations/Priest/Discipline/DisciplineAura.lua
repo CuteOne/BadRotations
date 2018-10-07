36ae3bcf634a926b1aedbe173505e19c0013d807
LSharpAura <LSharpAura@users.noreply.github.com>
2018-10-06 20:03:35 -0400

    Disc - Disabled old Bizkut/Aura profile
    Dropping support due to new profile being supported instead

--- Rotations/Priest/Discipline/DisciplineAura.lua
+++ Rotations/Priest/Discipline/DisciplineAura.lua
@@ -1444,7 +1444,8 @@ local function runRotation()
         end -- Pause
     end -- End Timer
 end -- End runRotation
-local id = 256
+--local id = 256
+local id = 0
 if br.rotations[id] == nil then br.rotations[id] = {} end
 tinsert(br.rotations[id],{
     name = rotationName,
