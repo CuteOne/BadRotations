e3529fbdfffa34fc2c599eef45abf9b854597c67
kuukuukuatchu <echidnablade@gmail.com>
2018-11-12 07:21:58 -0500

    System - Fix bossHelper typo

--- System/Misc/Zu/BossManager.lua
+++ System/Misc/Zu/BossManager.lua
@@ -1,34 +1,34 @@
---+----------------+--
---|Get Current Zone|--
---+----------------+--
-myCurrentZone = GetRealZoneText();
---+---------------------+--
---|Am I In Maw of Souls?|--
---+---------------------+--
-function inMawOfSouls()
-  if myCurrentZone == "Helmouth Cliffs" then
-    isBadRotationsHelya()
-  end
-end
+-- --+----------------+--
+-- --|Get Current Zone|--
+-- --+----------------+--
+-- myCurrentZone = GetRealZoneText();
+-- --+---------------------+--
+-- --|Am I In Maw of Souls?|--
+-- --+---------------------+--
+-- function inMawOfSouls()
+--   if myCurrentZone == "Helmouth Cliffs" then
+--     isBadRotationsHelya()
+--   end
+-- end
 
-function bossManager()
-	--+------------+--
---|Where Am I? |--
---|Check Please|--
---+------------+--
-AddEventCallback("ZONE_CHANGED_NEW_AREA", function()
-Print("We are in", GetRealZoneText())
-inMawOfSouls()
+-- function bossManager()
+-- 	--+------------+--
+-- --|Where Am I? |--
+-- --|Check Please|--
+-- --+------------+--
+-- AddEventCallback("ZONE_CHANGED_NEW_AREA", function()
+-- Print("We are in", GetRealZoneText())
+-- inMawOfSouls()
 
-end)
+-- end)
 
-  inMawOfSouls()
-end
+--   inMawOfSouls()
+-- end
 function bossHelper()
 	-- Automatic catch the pig
 	if EWT~=nil then
 		if GetMinimapZoneText() == "Ring of Booty" then
-			for i = 1, ObjectCount() do
+			for i = 1, GetObjectCount() do
 				local ID = ObjectID(ObjectWithIndex(i))
 				local object = ObjectWithIndex(i)
 				if ID == 130099 and ObjectExists(object) and getDistance(object) < 10 then
