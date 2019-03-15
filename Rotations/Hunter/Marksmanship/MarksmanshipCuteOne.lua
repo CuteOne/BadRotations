e1d097b9b3771d6b1d5959a1e3daf0c1e93f56b2
Kuukuukatchu <echidnablade@gmail.com>
2019-03-15 17:41:59 -0400

    MM Hunter - Activated profile

--- Rotations/Hunter/Marksmanship/MarksmanshipCuteOne.lua
+++ Rotations/Hunter/Marksmanship/MarksmanshipCuteOne.lua
@@ -726,8 +726,7 @@ local function runRotation()
         end --End Rotation Logic
     end -- End Timer
 end -- End runRotation
---local id = 254
-local id = 0
+local id = 254
 if br.rotations[id] == nil then br.rotations[id] = {} end
 tinsert(br.rotations[id],{
     name = rotationName,
