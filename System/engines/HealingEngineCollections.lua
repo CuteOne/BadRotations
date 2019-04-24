928b1b7f5bcfdfbe2792bf7c7cdc4aec42a18432
Laksmack <junk1@kanon.net>
2019-04-24 11:51:48 -0500

    HealEngine - fixed typo - woops!

--- System/engines/HealingEngineCollections.lua
+++ System/engines/HealingEngineCollections.lua
@@ -61,7 +61,7 @@ novaEngineTables.BadDebuffList= {
 	[274148] = "Taint", --Avatar of Sethraliss Debuff
 	[285213] = "Caress of Death", -- Caress of Death (Rastakhan)
 	--Crucible of storms
-	[282733] = "Embrace of the Void", -- Embrace of the Void (The Restless Cabal)
+	[284733] = "Embrace of the Void", -- Embrace of the Void (The Restless Cabal)
 	[285652] = "Insatiable Torment", --insatiable-torment (Uu'nat)
 }
 -- list of special units we want to heal, these npc will go directly into healing engine(Special Heal must be checked)
