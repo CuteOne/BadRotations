fa6d122ec4c175b193f033225ad7132403b1dc41
Ashley (CuteOne) <ph34rt3hcute1@gmail.com>
2019-07-11 18:22:06 -0400

    System - Restored validCheck for units within 8yrds of target non-instance

--- System/Functions/Misc.lua
+++ System/Functions/Misc.lua
@@ -419,7 +419,7 @@ function isValidUnit(Unit)
 		local distanceToPlayer = getDistance("player",Unit)
 		local inCombat = UnitAffectingCombat("player") or (GetObjectExists("pet") and UnitAffectingCombat("pet"))
 		local unitThreat = hasThreat(Unit) or targeting or isInProvingGround() or burnUnit or threatBypassUnit
-		return unitThreat or ((not instance and playerTarget) or (instance and #br.friend == 1)) -- (not hasThreat and (
+		return unitThreat or ((not instance and (playerTarget or distanceToTarget < 8)) or (instance and #br.friend == 1)) -- (not hasThreat and (
 			-- Not In Instance
 			-- (not instance and (playerTarget or distanceToTarget < 8)) or
 			-- In Instance 
