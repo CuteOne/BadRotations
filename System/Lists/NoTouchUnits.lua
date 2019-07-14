d00647fb72f2e8c535eaaf2fb44922d6bd413d2b
Kuukuu <echidnablade@gmail.com>
2019-07-14 11:57:12 -0400

    System - Added Viq-Goth to No Touch Units

--- System/Lists/NoTouchUnits.lua
+++ System/Lists/NoTouchUnits.lua
@@ -3,6 +3,10 @@ if br.lists == nil then
 end
 -- No Touch Units = List of units that we should not attack for any reason
 br.lists.noTouchUnits = {
+    -- Jade Temple
+    {unitID = 56448, buff = 106062}, -- Wise Mari with Bubble
+    --Shadopan Monastery
+    {unitID = 56747, buff = 110945}, -- Gu Cloudstrike with Charging Soul
     -- Iron Docks
     {unitID = 87451, buff = 164504, spell = 164426}, --Fleshrender Nok'gar, do not attack during defensive stance buff, Todo: Should stop when he cast 164504
     {unitID = 1, buff = 163689}, -- Never attack Sanguine Sphere
@@ -38,4 +42,6 @@ br.lists.noTouchUnits = {
     
     -- Underrot
     {unitID = 137458}, -- Rotting Spore
+    -- Siege of Boralus
+    {unitID = 128652}, -- Viq'Goth
 }
