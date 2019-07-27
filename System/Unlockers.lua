79fe0add47e08e5d49ebd93b48f180033d6da38e
Kuukuu <echidnablade@gmail.com>
2019-07-27 14:18:51 -0400

    System - Removed FH references as it no longer exists

--- System/Unlockers.lua
+++ System/Unlockers.lua
@@ -1,9 +1,4 @@
-if FH and not EWT then
-	IsHackEnabled = HackEnabled
-	SetHackEnabled = HackEnabled
-	ObjectTypes = ObjectType
-	ObjectExists = ObjectIsVisible
-elseif EWT and EasyWoWToolbox ~= nil then
+if EWT and EasyWoWToolbox ~= nil then
 	IsHackEnabled = IsHackEnabled
 	SetHackEnabled = SetHackEnabled
 	ObjectTypes = ObjectTypes
