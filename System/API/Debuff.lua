2303f8458c82fc543765891c6ea7442ab9998592
Kuukuu <echidnablade@gmail.com>
2019-07-29 17:21:21 -0400

    System - more debuff fixes!

--- System/API/Debuff.lua
+++ System/API/Debuff.lua
@@ -2,6 +2,55 @@ if br.api == nil then br.api = {} end
 -- debuff is the table located at br.player.debuff
 -- v is the spellID passed from the builder which cycles all the collected debuff spells from the spell list for the spec
 -- spell in the examples represent the name in the debuffs list (Spec, Shared Class, Shared Global Lists) defined in System/List/Spells.lua
+--Local function needed to facilitate debuff.calc
+local function getSnapshotValue(dot)
+    local self = br.player
+    -- Feral Bleeds
+    if GetSpecializationInfo(GetSpecialization()) == 103 then
+        local multiplier        = 1.00
+        local Bloodtalons       = 1.30
+        -- local SavageRoar        = 1.40
+        local TigersFury        = 1.15
+        local RakeMultiplier    = 1
+        -- Bloodtalons
+        if self.buff.bloodtalons.exists() then multiplier = multiplier*Bloodtalons end
+        -- Savage Roar
+        -- if self.buff.savageRoar.exists() then multiplier = multiplier*SavageRoar end
+        -- Tigers Fury
+        if self.buff.tigersFury.exists() then multiplier = multiplier*TigersFury end
+        -- rip
+        if dot == self.spell.debuffs.rip then
+            -- -- Versatility
+            -- multiplier = multiplier*(1+Versatility*0.1)
+            -- return rip
+            return 5*multiplier
+        end
+        -- rake
+        if dot == self.spell.debuffs.rake then
+            -- Incarnation/Prowl
+            if self.buff.incarnationKingOfTheJungle.exists() or self.buff.prowl.exists() then
+                RakeMultiplier = 2
+            end
+            -- return rake
+            return multiplier*RakeMultiplier
+        end
+
+    end
+    -- Assassination Bleeds
+    if GetSpecializationInfo(GetSpecialization()) == 259 then
+        local multiplier = 1
+        if self.buff.stealth.exists() and self.talent.nightstalker and (dot == self.spell.debuffs.rupture or dot == self.spell.debuffs.garrote) then multiplier = 1.5 end
+        if (self.buff.stealth.exists() or self.buff.vanish.exists() 
+            or (self.buff.subterfuge.exists() and self.buff.subterfuge.remain() >= 0.1 and self.buff.subterfuge.remain() >= getSpellCD(61304))) 
+            and dot == self.spell.debuffs.garrote and self.talent.subterfuge 
+        then 
+            multiplier = 1.8 
+        end
+        return multiplier
+    end
+    return 0
+end
+
 br.api.debuffs = function(debuff,k,v)
     local spec = GetSpecializationInfo(GetSpecialization()) 
     debuff.exists = function(thisUnit,sourceUnit)
@@ -73,53 +122,4 @@ br.api.debuffs = function(debuff,k,v)
     debuff.applied = function(thisUnit)
         return (spec == 103 or spec == 259) and debuff.bleed[thisUnit] or 0
     end
-end
-
---Local function needed to facilitate debuff.calc
-local function getSnapshotValue(dot)
-    local self = br.player
-    -- Feral Bleeds
-    if GetSpecializationInfo(GetSpecialization()) == 103 then
-        local multiplier        = 1.00
-        local Bloodtalons       = 1.30
-        -- local SavageRoar        = 1.40
-        local TigersFury        = 1.15
-        local RakeMultiplier    = 1
-        -- Bloodtalons
-        if self.buff.bloodtalons.exists() then multiplier = multiplier*Bloodtalons end
-        -- Savage Roar
-        -- if self.buff.savageRoar.exists() then multiplier = multiplier*SavageRoar end
-        -- Tigers Fury
-        if self.buff.tigersFury.exists() then multiplier = multiplier*TigersFury end
-        -- rip
-        if dot == self.spell.debuffs.rip then
-            -- -- Versatility
-            -- multiplier = multiplier*(1+Versatility*0.1)
-            -- return rip
-            return 5*multiplier
-        end
-        -- rake
-        if dot == self.spell.debuffs.rake then
-            -- Incarnation/Prowl
-            if self.buff.incarnationKingOfTheJungle.exists() or self.buff.prowl.exists() then
-                RakeMultiplier = 2
-            end
-            -- return rake
-            return multiplier*RakeMultiplier
-        end
-
-    end
-    -- Assassination Bleeds
-    if GetSpecializationInfo(GetSpecialization()) == 259 then
-        local multiplier = 1
-        if self.buff.stealth.exists() and self.talent.nightstalker and (dot == self.spell.debuffs.rupture or dot == self.spell.debuffs.garrote) then multiplier = 1.5 end
-        if (self.buff.stealth.exists() or self.buff.vanish.exists() 
-            or (self.buff.subterfuge.exists() and self.buff.subterfuge.remain() >= 0.1 and self.buff.subterfuge.remain() >= getSpellCD(61304))) 
-            and dot == self.spell.debuffs.garrote and self.talent.subterfuge 
-        then 
-            multiplier = 1.8 
-        end
-        return multiplier
-    end
-    return 0
 end
