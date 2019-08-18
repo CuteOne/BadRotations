d74fb2339eeddc57c2d861e86dc06f0207fa432a
Kuukuu <echidnablade@gmail.com>
2019-08-18 19:31:23 -0400

    Veng DH - Interrupt tweaks/font of azshara support/active mitigation tweak

--- Rotations/Demon Hunter/Vengeance/VengeanceKuu.lua
+++ Rotations/Demon Hunter/Vengeance/VengeanceKuu.lua
@@ -69,6 +69,8 @@ local function createOptions()
             br.ui:createCheckbox(section,"Throw Glaive")
         -- Use Cyclotronic on Pull
             br.ui:createCheckbox(section,"Cyclotronic on Pull")
+        -- Font of Azshara
+            br.ui:createSpinner(section,"Font of Azshara", 4,1, 10, 1, "|cffFFFFFFSet to desired time to use Font of Azshara (DBM Required). Min: 1 / Max: 10 / Interval: 1")
         -- Infernal Strike Key
             br.ui:createDropdown(section,"Infernal Strike Key", br.dropOptions.Toggle, 6, "|cffFFFFFFSet key to manually infernal strike")
         -- Sigil of Chains Key
@@ -239,6 +241,7 @@ local function runRotation()
                 end
                 if getDistance("player",unit) <= 6 or br.vengeance.groundOverride[getUnitID(unit)] ~= nil then
                     if cast.infernalStrike("player","ground") then
+                        br.addonDebug("Casting Inferal Strike (Player)")
                         iStrikeDelay = GetTime()
                         if wasMouseLooking then
                             MouselookStart()
@@ -246,33 +249,37 @@ local function runRotation()
                         return 
                     end
                 end
-                local combatRange = max(5, UnitCombatReach("player") + UnitCombatReach(unit))
-                local px,py,pz = ObjectPosition("player")
-                local x,y,z = GetPositionBetweenObjects(unit, "player", combatRange)
-                z = select(3,TraceLine(x, y, z+5, x, y, z-5, 0x110)) -- Raytrace correct z, Terrain and WMO hit
-                if z ~= nil and TraceLine(px, py, pz+2, x, y, z+1, 0x100010) == nil and TraceLine(x, y, z+4, x, y, z, 0x1) == nil then -- Check z and LoS, ignore terrain and m2 colissions and check no m2 on hook location
-                    CastSpellByName(GetSpellInfo(spell.infernalStrike))
-                    br.addonDebug("Casting Inferal Strike")
-                    ClickPosition(x,y,z)
-                    iStrikeDelay = GetTime()
-                    if wasMouseLooking then
-                        MouselookStart()
-                    end
-                end
                 return true
           --  end
           --  return false
         end
 
+        local function ShouldMitigate()
+            if UnitThreatSituation("player", "target") == 3 and UnitCastingInfo("target") then
+                if activeMitigationList[select(9,UnitCastingInfo("target"))] ~= nil then
+                    return true
+                end
+                -- for i = 1, #activeMitigationList do
+                --     local spell_id = activeMitigationList[i]
+                --     if UnitCastingInfo("target") == GetSpellInfo(spell_id) then
+                --         print(activeMitigationList[i])
+                --         br.addonDebug(GetSpellInfo(spell_id))
+                --         return true
+                --     end
+                -- end
+            end
+            return false
+        end
+
         local function trinketLogic(slot)
-            if not hasEquiped(165572,slot) and not hasEquiped(167868,slot) and not hasEquiped(169311,slot) and not hasEquiped(167555,slot) and ((slot == 13 and php <= getOptionValue("Trinket 1")) or slot == 14 and php <= getOptionValue("Trinket 2")) then
+            if not hasEquiped(169314,slot) and not hasEquiped(165572,slot) and not hasEquiped(167868,slot) and not hasEquiped(169311,slot) and not hasEquiped(167555,slot) and ((slot == 13 and php <= getOptionValue("Trinket 1")) or slot == 14 and php <= getOptionValue("Trinket 2")) then
                 if slot == 13 then
                     br.addonDebug("Using Trinket 1")
                 elseif slot == 14 then
                     br.addonDebug("Using Trinket 2")
                 end
                 useItem(slot)
-            elseif hasEquiped(167555,slot) and br.timer:useTimer("Cyclo Delay",3) then
+            elseif hasEquiped(167555,slot) and br.timer:useTimer("Cyclo Delay",3) and php >= 70 and not ShouldMitigate() then
                 br.addonDebug("Using Cyclotronic Blast")
                 useItem(slot)
             elseif hasEquiped(165572,slot) then
@@ -284,6 +291,13 @@ local function runRotation()
                 if (#getAllies("player",15) + getNumEnemies("player",15)) >= 4 and php <= 50 then
                     br.addonDebug("Using Idol of Indiscriminate Consumption")
                     useItem(slot)
+                end
+            elseif hasEquiped(169314,slot) then
+                local tank = getFocusedTank()
+                if not UnitIsUnit("player",tank) and php >= 75 and not UnitBuffID("player",296962) and br.timer:useTimer("Font Delay", 4) then
+                    br.addonDebug("Using Font Of Azshara")
+                    useItem(169314)
+                    return true
                 end
             elseif hasEquiped(169311,slot) then
                 if ((debuff.razorCoral.stack("target") >= 10 and debuff.razorCoral.remain("target") < 5) or (ttd("target") < 20 and debuff.razorCoral.stack("target") >= 5)) and br.timer:useTimer("Razor Coral Delay", 3) then
@@ -301,18 +315,6 @@ local function runRotation()
             "Beast","Wildtier","Bête","야수","Bestia","Fera","Животное","野兽","野獸",
             "Humanoid","Humanoid","Humanoide", "Humanoïde", "Umanoide", "Humanoide", "Гуманоид", "인간형",  "人型生物", "人型生物"
         }
-
-        local function ShouldMitigate()
-            if UnitThreatSituation("player", "target") == 3 then
-                for i = 1, #br.activeMitigationList do
-                    local spell_id = Casting[i]
-                    if UnitCastingInfo("target") == GetSpellInfo(spell_id) then
-                        return true
-                    end
-                end
-            end
-            return false
-        end
 
 --------------------
 --- Action Lists ---
@@ -334,7 +336,8 @@ local function runRotation()
             if isChecked("Torment") then
                 for i = 1, #enemies.yards30 do
                     local thisUnit = enemies.yards30[i]
-                    if not isTanking(thisUnit) and hasThreat(thisUnit) then
+                    local target = UnitTarget(thisUnit)
+                    if target and select(2,IsInInstance())~= "none" and UnitGroupRolesAssigned(target) ~= "TANK" then
                         if cast.torment(thisUnit) then br.addonDebug("Casting Torment") return end
                     end
                 end
@@ -420,7 +423,7 @@ local function runRotation()
                     thisUnit = enemies.yards30[i]
                      -- Disrupt
                     if canInterrupt(thisUnit,getOptionValue("Interrupt At")) and (sigilDelay == nil or GetTime() - sigilDelay > 2) then
-                        if isChecked("Disrupt") and getDistance(thisUnit) < 20 and cd.disrupt.remain() <= gcd then
+                        if isChecked("Disrupt") and getDistance(thisUnit) < 20 and getFacing("player",thisUnit) and cd.disrupt.remain() <= gcd then
                             if cast.disrupt(thisUnit) then br.addonDebug("Casting Disrupt") return end
                         -- Sigil of Silence
                         elseif isChecked("Sigil of Silence") and cd.sigilOfSilence.remain() <= gcd then
@@ -436,7 +439,7 @@ local function runRotation()
                             elseif talent.concentratedSigils and getDistance(thisUnit) <= 8 then
                                 if cast.sigilOfMisery() then sigilDelay = GetTime() br.addonDebug("Casting Sigil of Misery") return end
                             end
-                        elseif isChecked("Imprison") and cd.imprison.remain() <= gcd then
+                        elseif isChecked("Imprison") and cd.imprison.remain() <= gcd and getFacing("player",thisUnit) then
                             local type = UnitCreatureType(thisUnit)
                             for i = 1, #typesLoc do
                                 if type == typesLoc[i] then
@@ -487,6 +490,12 @@ local function runRotation()
                         end
                     end
                 end
+                if isChecked("Font of Azshara") and pullTimer <= getOptionValue("Font of Azshara") then
+                    if hasEquiped(169314) and canUseItem(169314) and br.timer:useTimer("Font Delay", 4) then
+                        br.addonDebug("Using Font Of Azshara")
+                        useItem(169314)
+                    end
+                end
                 if isChecked("Cyclotronic on Pull") and pullTimer <= 2 then
                     if hasEquiped(167555) and canUseItem(167555) and br.timer:useTimer("Cyclo Delay",3) then
                         br.addonDebug("Using Cyclotronic Blast")
@@ -494,7 +503,6 @@ local function runRotation()
                     end
                 end
                 if isChecked("Pre-Pull Timer") and pullTimer <= getOptionValue("Pre-Pull Timer") then
-                    
                     if isChecked("Worldvein Resonance") and essence.worldveinResonance.active and cd.worldveinResonance.remain() <= gcd then
                         if cast.worldveinResonance() then br.addonDebug("Casting Worldvein Resonance") return end
                     end
@@ -521,7 +529,7 @@ local function runRotation()
                 if cast.sigilOfFlame("best",false,1,8) then br.addonDebug("Casting Sigil Of Flame") return end
 			end
 			-- actions.brand+=/infernal_strike,if=cooldown.fiery_brand.remains=0
-			if mode.mover == 1 and not cast.last.infernalStrike(1) and charges.infernalStrike.count() == 2 and not cd.fieryBrand.exists() and getDistance("target") <= 10 and C_LossOfControl.GetNumEvents() == 0 and GetTime() - iStrikeDelay > 2 then
+			if mode.mover == 1 and not cast.last.infernalStrike(1) and charges.infernalStrike.count() == 2 and not cd.fieryBrand.exists() and getDistance("target") <= 6 and not isMoving("target") and C_LossOfControl.GetNumEvents() == 0 and GetTime() - iStrikeDelay > 2 then
                 --if cast.infernalStrike("targetGround","ground",1,6) then return end
                 if iStrike("target") then return end
             end
@@ -539,7 +547,7 @@ local function runRotation()
 					if cast.felDevastation() then br.addonDebug("Casting Fel Devastation") return end
 				end
 				-- actions.brand+=/infernal_strike,if=dot.fiery_brand.ticking
-				if mode.mover == 1 and not cast.last.infernalStrike(1) and charges.infernalStrike.count() == 2 and getDistance("target") <= 10 and C_LossOfControl.GetNumEvents() == 0 and GetTime() - iStrikeDelay > 2 then
+				if mode.mover == 1 and not cast.last.infernalStrike(1) and charges.infernalStrike.count() == 2 and getDistance("target") <= 6 and not isMoving("target") and C_LossOfControl.GetNumEvents() == 0 and GetTime() - iStrikeDelay > 2 then
                     --if cast.infernalStrike("player","ground",1,6) then return end
                     if iStrike("target") then return end
 				end
@@ -555,10 +563,10 @@ local function runRotation()
 
         local function actionList_ActiveMitigation()
             if ShouldMitigate() and #enemies.yards8 > 0 then
-                if isChecked("Trinket 1") and canTrinket(13) and not hasEquiped(169311,13) then  
+                if isChecked("Trinket 1") and canTrinket(13) and not hasEquiped(169311,13) and not hasEquiped(167555,13) then  
                     trinketLogic(13)
                     return          
-                elseif isChecked("Trinket 2") and canTrinket(14) and not hasEquiped(169311,14) then            
+                elseif isChecked("Trinket 2") and canTrinket(14) and not hasEquiped(169311,14) and not hasEquiped(167555,14) then            
                     trinketLogic(14)
                     return
                     -- Metamorphosis
@@ -589,6 +597,9 @@ local function runRotation()
         elseif (inCombat and profileStop==true) or IsMounted() or IsFlying() or pause() or mode.rotation==4 then
             return true
         else
+            if isCastingSpell(296962) and php < 75 then
+                RunMacroText("/stopcasting")
+            end
             -- Infernal Strike
             if SpecificToggle("Infernal Strike Key") and not GetCurrentKeyBoardFocus() and isChecked("Infernal Strike Key") and GetTime() - iStrikeDelay > 2 then
                 CastSpellByName(GetSpellInfo(spell.infernalStrike),"cursor") iStrikeDelay = GetTime() return  
@@ -615,10 +626,11 @@ local function runRotation()
                 --     end
                 -- end
                 ChatOverlay("In-Combat!")
+                if actionList_ActiveMitigation() then return end
     --------------------------
     --- Defensive Rotation ---
     --------------------------
-            if actionList_Defensive() then return end
+                if actionList_Defensive() then return end
     ------------------------------
     --- In Combat - Interrupts ---
     ------------------------------
@@ -666,15 +678,15 @@ local function runRotation()
                     if cast.concentratedFlame("player") then br.addonDebug("Casting Concentrated Flame Heal") return end
                 end
                 -- soul_cleave
-                if talent.spiritBomb and buff.soulFragments.stack() >= 1 and #enemies.yards5t < 2 and php <= getOptionValue("Soul Cleave") then
+                if talent.spiritBomb and buff.soulFragments.stack() >= 1 and (#enemies.yards5t < 2 and #enemies.yards5t > 0) and php <= getOptionValue("Soul Cleave") then
                     if cast.soulCleave() then br.addonDebug("Casting Soul Cleave") return end
                 end
 				-- spirit_bomb
-				if talent.spiritBomb and buff.soulFragments.stack() >= 4 or (#enemies.yards5t >= 3 and php <= getOptionValue("Spirit Bomb")) then
+				if talent.spiritBomb and (buff.soulFragments.stack() >= 4 or (#enemies.yards5t >= 3 and php <= getOptionValue("Spirit Bomb"))) then
                     if cast.spiritBomb() then br.addonDebug("Casting Spirit Bomb") return end
                 end
                 -- soul_cleave
-                if painDeficit <= 30 and (not buff.soulFragments.exists() or #enemies.yards5t < 2) and talent.spiritBomb then
+                if painDeficit <= 30 and getDistance("target","player") <= 5 and (not buff.soulFragments.exists() or #enemies.yards5t < 2) and talent.spiritBomb then
                     if cast.soulCleave() then br.addonDebug("Casting Soul Cleave") return end
                 end
                 -- fracture
@@ -698,9 +710,19 @@ local function runRotation()
                     if cast.felblade() then br.addonDebug("Casting Felblade") return end
                 end
                 -- infernal_strike
-				if mode.mover == 1 and not cast.last.infernalStrike(1) and charges.infernalStrike.count() == 2 and #enemies.yards5 > 0 and C_LossOfControl.GetNumEvents() == 0 and GetTime() - iStrikeDelay > 2  then
+				if mode.mover == 1 and not cast.last.infernalStrike(1) and charges.infernalStrike.count() == 2 and getDistance("target") <= 6 and not isMoving("target") and C_LossOfControl.GetNumEvents() == 0 and GetTime() - iStrikeDelay > 2  then
                     --if cast.infernalStrike("player","ground",1,6) then return end
                     if iStrike("target") then return true end
+                end
+                -- Font of Azshara
+                if isChecked("Font of Azshara") then
+                    local tank = getFocusedTank()
+                    if not UnitIsUnit(getFocusedTank(),"player") and php >= 75 and not UnitBuffID("player",296962) then
+                        if hasEquiped(169314) and canUseItem(169314) and br.timer:useTimer("Font Delay", 4) then
+                            br.addonDebug("Using Font Of Azshara")
+                            useItem(169314)
+                        end
+                    end
                 end
                 -- Worldvein Resonance
                 if isChecked("Worldvein Resonance") and essence.worldveinResonance.active and getSpellCD(295186) <= gcd and #enemies.yards5 > 0 then
