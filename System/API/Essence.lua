6edfabebadfbcee96ea6a4488d0b2bab3965ed84
Kuukuu <echidnablade@gmail.com>
2019-08-24 23:53:52 -0400

    System - Essence fix for low levels

--- System/API/Essence.lua
+++ System/API/Essence.lua
@@ -1,35 +1,33 @@
 local br = _G["br"]
 if br.api == nil then br.api = {} end
-br.api.essences = function(essence,k,v)
-    local GetSpellInfo = _G["GetSpellInfo"]
-    local C_AzeriteEssence = _G["C_AzeriteEssence"]
-    local essenceTable = C_AzeriteEssence.GetEssences()
-    local essenceIdName = GetSpellInfo(GetSpellInfo(v)) or "None"
-    local essenceIcon = select(3,GetSpellInfo(v)) or 0
-    local milestoneTable = C_AzeriteEssence.GetMilestones()
-    -- Find Essence Ranks
-    for i = 1, #essenceTable do
-        local thisEssence = essenceTable[i]
-        if thisEssence.icon == essenceIcon then
-            essence.essenceID = thisEssence.ID
-            essence.rank = thisEssence.rank or 0
-            break
-        end
-    end
-    -- Find Major/Minor/Active
+br.api.essences = function(essence,k,v)    
+    -- Find Major/Minor/Active/Rank
     essence.active = false
     essence.major = false
     essence.minor = false
-    for i = 1, #milestoneTable do
-        local thisMilestone = milestoneTable[i]
-        if not thisMilestone.unlocked then break end
-        if thisMilestone.slot ~= nil then
-            local milestoneEssence = C_AzeriteEssence.GetMilestoneEssence(thisMilestone.ID)
-            essence.active = essence.essenceID == milestoneEssence
-            if essence.active then
-                if thisMilestone.slot == 0 then essence.major = true end
-                if thisMilestone.slot == 1 or thisMilestone.slot == 2 then essence.minor = true end
-                break
+    essence.rank = 0
+    local milestoneTable = C_AzeriteEssence.GetMilestones()
+    local GetSpellInfo = _G["GetSpellInfo"]
+    local C_AzeriteEssence = _G["C_AzeriteEssence"]
+    local essenceIdName = GetSpellInfo(GetSpellInfo(v)) or "None"
+    local essenceIcon = select(3,GetSpellInfo(v)) or 0
+    if milestoneTable then
+            for i = 1, #milestoneTable do
+            local thisMilestone = milestoneTable[i]
+            if not thisMilestone.unlocked then break end
+            if thisMilestone.slot ~= nil then
+                local milestoneEssence = C_AzeriteEssence.GetMilestoneEssence(thisMilestone.ID)
+                local rank = C_AzeriteEssence.GetEssenceInfo(milestoneEssence).rank
+                local icon = C_AzeriteEssence.GetEssenceInfo(milestoneEssence).icon
+                if essenceIcon == icon then
+                    essence.rank = rank
+                end
+                essence.active = essence.essenceID == milestoneEssence
+                if essence.active then
+                    if thisMilestone.slot == 0 then essence.major = true end
+                    if thisMilestone.slot == 1 or thisMilestone.slot == 2 then essence.minor = true end
+                    break
+                end
             end
         end
     end
