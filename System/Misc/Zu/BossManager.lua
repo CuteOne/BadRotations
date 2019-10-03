fac3db1e73b5c12e782ab881a3ae956fd6f75c51
Ashley (CuteOne) <ph34rt3hcute1@gmail.com>
2019-10-03 11:19:44 -0400

    System - Distance check change for Pig Catcher - Laksmackt

--- System/Misc/Zu/BossManager.lua
+++ System/Misc/Zu/BossManager.lua
@@ -1,14 +1,17 @@
 function bossHelper()
-	-- Automatic catch the pig
-	if EWT~=nil then
-		if select(8,GetInstanceInfo()) == 1754 then
-			for i = 1, GetObjectCount() do
-				local ID = ObjectID(GetObjectWithIndex(i))
-				local object = GetObjectWithIndex(i)
-				if ID == 130099 and getDistance(object) < 10 then
-					InteractUnit(object)
-				end
-			end
-		end
-	end	
+    -- Automatic catch the pig
+    if EWT ~= nil then
+        if select(8, GetInstanceInfo()) == 1754 then
+            for i = 1, GetObjectCount() do
+                local ID = ObjectID(GetObjectWithIndex(i))
+                local object = GetObjectWithIndex(i)
+                local x1, y1, z1 = ObjectPosition("player")
+                local x2, y2, z2 = ObjectPosition(object)
+                local distance = math.sqrt(((x2 - x1) ^ 2) + ((y2 - y1) ^ 2) + ((z2 - z1) ^ 2))
+                if ID == 130099 and distance < 10 then
+                    InteractUnit(object)
+                end
+            end
+        end
+    end
 end
