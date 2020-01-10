68759ea53a21af507fc39c91d5dc5daf26c52f14
Ashley (CuteOne) <ph34rt3hcute1@gmail.com>
2020-01-10 13:36:59 -0500

    Updated Sample Rotation file to current design practice

--- Rotations/sample rotation.lua
+++ Rotations/sample rotation.lua
@@ -4,7 +4,7 @@ local rotationName = "CuteOne" -- Change to name of profile listed in options dr
 --- Toggles ---
 ---------------
 local function createToggles() -- Define custom toggles
--- Rotation Button
+    -- Rotation Button
     RotationModes = {
         [1] = { mode = "Auto", value = 1 , overlay = "Automatic Rotation", tip = "Swaps between Single and Multiple based on number of #enemies.yards8 in range.", highlight = 0, icon = br.player.spell.whirlwind },
         [2] = { mode = "Mult", value = 2 , overlay = "Multiple Target Rotation", tip = "Multiple target rotation used.", highlight = 0, icon = br.player.spell.bladestorm },
@@ -12,20 +12,20 @@ local function createToggles() -- Define custom toggles
         [4] = { mode = "Off", value = 4 , overlay = "DPS Rotation Disabled", tip = "Disable DPS Rotation", highlight = 0, icon = br.player.spell.enragedRegeneration}
     };
     CreateButton("Rotation",1,0)
--- Cooldown Button
+    -- Cooldown Button
     CooldownModes = {
         [1] = { mode = "Auto", value = 1 , overlay = "Cooldowns Automated", tip = "Automatic Cooldowns - Boss Detection.", highlight = 1, icon = br.player.spell.battleCry },
         [2] = { mode = "On", value = 2 , overlay = "Cooldowns Enabled", tip = "Cooldowns used regardless of target.", highlight = 0, icon = br.player.spell.battleCry },
         [3] = { mode = "Off", value = 3 , overlay = "Cooldowns Disabled", tip = "No Cooldowns will be used.", highlight = 0, icon = br.player.spell.battleCry }
     };
     CreateButton("Cooldown",2,0)
--- Defensive Button
+    -- Defensive Button
     DefensiveModes = {
         [1] = { mode = "On", value = 1 , overlay = "Defensive Enabled", tip = "Includes Defensive Cooldowns.", highlight = 1, icon = br.player.spell.enragedRegeneration },
         [2] = { mode = "Off", value = 2 , overlay = "Defensive Disabled", tip = "No Defensives will be used.", highlight = 0, icon = br.player.spell.enragedRegeneration }
     };
     CreateButton("Defensive",3,0)
--- Interrupt Button
+    -- Interrupt Button
     InterruptModes = {
         [1] = { mode = "On", value = 1 , overlay = "Interrupts Enabled", tip = "Includes Basic Interrupts.", highlight = 1, icon = br.player.spell.pummel },
         [2] = { mode = "Off", value = 2 , overlay = "Interrupts Disabled", tip = "No Interrupts will be used.", highlight = 0, icon = br.player.spell.pummel }
@@ -88,80 +88,168 @@ local function createOptions()
     return optionTable
 end
 
+--------------
+--- Locals ---
+--------------
+-- BR API Locals - Many of these are located from System/API, this is a sample of commonly used ones but no all inclusive
+local buff
+local cast
+local cd
+local debuff
+local enemies
+local equiped
+local gcd
+local gcdMax
+local has
+local inCombat
+local item
+local level
+local mode
+local php
+local spell
+local talent
+local units
+local use
+-- General Locals - Common Non-BR API Locals used in profiles
+local haltProfile
+local hastar
+local healPot
+local profileStop
+local ttd
+-- Profile Specific Locals - Any custom to profile locals
+local actionList = {}
+
+-----------------
+--- Functions --- -- List all profile specific custom functions here
+-----------------
+
+--------------------
+--- Action Lists --- -- All Action List functions from SimC (or other rotation logic) here, some common ones provided
+--------------------
+-- Action List - Extra
+actionList.Extra = function()
+
+end -- End Action List - Extra
+
+-- Action List - Defensive
+actionList.Defensive = function()
+
+end -- End Action List - Defensive
+
+-- Action List - Interrrupt
+actionList.Interrupt = function()
+
+end -- End Action List - Interrupt
+
+-- Action List - Cooldowns
+actionList.Cooldown = function()
+
+end -- End Action List - Cooldowns
+
+-- Action List - Pre-Combat
+actionList.PreCombat = function()
+
+end -- End Action List - PreCombat
+
 ----------------
 --- ROTATION ---
 ----------------
 local function runRotation()
-    if br.timer:useTimer("debugFury", 0.1) then --change "debugFury" to "debugSpec" (IE: debugFire)
-        --Print("Running: "..rotationName)
+    ---------------------
+    --- Define Locals ---
+    ---------------------
+    -- BR API Locals
+    buff                                          = br.player.buff
+    cast                                          = br.player.cast
+    cd                                            = br.player.cd
+    debuff                                        = br.player.debuff
+    enemies                                       = br.player.enemies
+    equiped                                       = br.player.equiped
+    gcd                                           = br.player.gcd
+    gcdMax                                        = br.player.gcdMax
+    has                                           = br.player.has
+    inCombat                                      = br.player.inCombat
+    item                                          = br.player.items
+    level                                         = br.player.level
+    mode                                          = br.player.mode
+    php                                           = br.player.health
+    spell                                         = br.player.spell
+    talent                                        = br.player.talent
+    units                                         = br.player.units
+    use                                           = br.player.use
+    -- General Locals
+    hastar                                        = GetObjectExists("target")
+    healPot                                       = getHealthPot()
+    profileStop                                   = profileStop or false
+    ttd                                           = getTTD
+    haltProfile                                   = (inCombat and profileStop) or (IsMounted() or IsFlying()) or pause() or mode.rotation==4
+    -- Units
+    units.get(5) -- Makes a variable called, units.dyn5
+    units.get(40) -- Makes a variable called, units.dyn40
+    -- Enemies
+    enemies.get(5) -- Makes a varaible called, enemies.yards5
+    enemies.get(40) -- Makes a varaible called, enemies.yards40
 
----------------
---- Toggles --- -- List toggles here in order to update when pressed
----------------
-        UpdateToggle("Rotation",0.25)
-        UpdateToggle("Cooldown",0.25)
-        UpdateToggle("Defensive",0.25)
-        UpdateToggle("Interrupt",0.25)
---------------
---- Locals ---
---------------
-        local artifact                                      = br.player.artifact
-        local buff                                          = br.player.buff
-        local cast                                          = br.player.cast
-        local combatTime                                    = getCombatTime()
-        local cd                                            = br.player.cd
-        local charges                                       = br.player.charges
-        local debuff                                        = br.player.debuff
-        local enemies                                       = br.player.enemies
-        local falling, swimming, flying, moving             = getFallTime(), IsSwimming(), IsFlying(), GetUnitSpeed("player")>0
-        local gcd                                           = br.player.gcd
-        local healPot                                       = getHealthPot()
-        local inCombat                                      = br.player.inCombat
-        local inInstance                                    = br.player.instance=="party"
-        local inRaid                                        = br.player.instance=="raid"
-        local level                                         = br.player.level
-        local lowestHP                                      = br.friend[1].unit
-        local mode                                          = br.player.mode
-        local perk                                          = br.player.perk        
-        local php                                           = br.player.health
-        local power, powmax, powgen                         = br.player.power, br.player.powerMax, br.player.powerRegen
-        local pullTimer                                     = br.DBM:getPulltimer()
-        local race                                          = br.player.race
-        local racial                                        = br.player.getRacial()
-        local spell                                         = br.player.spell
-        local talent                                        = br.player.talent
-        local ttm                                           = br.player.timeToMax
-        local units                                         = br.player.units
-        
-        if leftCombat == nil then leftCombat = GetTime() end
-        if profileStop == nil then profileStop = false end
+    -- Profile Specific Locals
 
---------------------
---- Action Lists ---
---------------------
+    -- SimC specific variables
+    
 
------------------
---- Rotations ---
------------------
-        -- Pause
-        if pause() or (UnitExists("target") and (UnitIsDeadOrGhost("target") or not UnitCanAttack("target", "player"))) or mode.rotation == 4 then
-            return true
-        else
----------------------------------
---- Out Of Combat - Rotations ---
----------------------------------
-            if not inCombat and GetObjectExists("target") and not UnitIsDeadOrGhost("target") and UnitCanAttack("target", "player") then
+    ---------------------
+    --- Begin Profile ---
+    ---------------------
+    -- Profile Stop | Pause
+    if not inCombat and not hastar and profileStop then
+        profileStop = false
+    elseif haltProfile then
+        return true
+    else
+        ---------------------------------
+        --- Out Of Combat - Rotations ---
+        ---------------------------------
+        -----------------------
+        --- Extras Rotation ---
+        -----------------------
+        if actionList.Extra() then return true end
+        -----------------
+        --- Defensive ---
+        -----------------
+        if actionList.Defensive() then return true end
+        ------------------
+        --- Pre-Combat ---
+        ------------------
+        if actionList.PreCombat() then return true end
+        -----------------------------
+        --- In Combat - Rotations ---
+        -----------------------------
+        if inCombat and isValidUnit("target") and cd.global.remain() == 0 then
+            ------------------------------
+            --- In Combat - Interrupts ---
+            ------------------------------
+            if actionList.Interrupt() then return true end
+            ---------------------------
+            --- SimulationCraft APL ---
+            ---------------------------
+            if getOptionValue("APL Mode") == 1 then
+                -- Start Attack
+                -- actions=auto_attack
+                if not IsAutoRepeatSpell(GetSpellInfo(6603)) and getDistance(units.dyn5) < 5 then
+                    StartAttack(units.dyn5)
+                end
+                -- Cooldowns
+                -- call_action_list,name=CDs
+                if actionList.Cooldown() then return true end
+            end -- End SimC APL
+            ------------------------
+            --- Ask Mr Robot APL ---
+            ------------------------
+            if getOptionValue("APL Mode") == 2 then
 
-            end -- End Out of Combat Rotation
------------------------------
---- In Combat - Rotations --- 
------------------------------
-            if inCombat then
-
-            end -- End In Combat Rotation
-        end -- Pause
-    end -- End Timer
-end -- End runRotation 
+            end -- End AMR
+        end -- End In Combat Rotation
+    end -- Pause
+    return true
+end -- End runRotation
 local id = 000 -- Change to the spec id profile is for.
 if br.rotations[id] == nil then br.rotations[id] = {} end
 tinsert(br.rotations[id],{
