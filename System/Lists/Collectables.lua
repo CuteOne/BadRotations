95ea582f6118d41c8996f9eaf561220e107d3159
Pangloo <42098476+Pangloo@users.noreply.github.com>
2020-03-21 11:34:58 -0600

    WOW RESTART REQUIRED Tracker Out of Core

---
+++ System/Lists/Collectables.lua
@@ -0,0 +1,8 @@
+if br.lists == nil then br.lists = {} end
+br.lists.visions = {
+    [341337] = black, -- black
+    [341338] = blue, -- blue
+    [341339] = green, -- green
+    [341340] = purple, -- purple
+    [341341] = red, -- red
+}
---
+++ System/engines/Tracker.lua
@@ -0,0 +1,66 @@
+function br.objectTracker()
+	if br.timer:useTimer("Tracker Lag", 0.07) then
+		LibDraw.clearCanvas()
+		if isChecked("Chest Tracker") or isChecked("Potions Tracker") or isChecked("Custom Tracker") then
+			for i = 1, GetObjectCountBR() do
+				local object = GetObjectWithIndex(i)
+				local name = ObjectName(object)
+				local objectid = ObjectID(object)
+				if isChecked("Chest Tracker") then
+					if string.match(strupper(name),strupper("cache")) or string.match(strupper(name),strupper("chest")) then
+						local xOb, yOb, zOb = ObjectPosition(object)
+						local pX,pY,pZ = ObjectPosition("player")
+						if xOb ~= nil and GetDistanceBetweenPositions(pX,pY,pZ,xOb,yOb,zOb) < 300 then
+							--LibDraw.Circle(xOb,yOb,zOb, 2)
+							LibDraw.Text(name.." "..objectid,"GameFontNormal",xOb,yOb,zOb+3)
+							if isChecked("Draw Lines to Tracked Objects") then 
+								LibDraw.Line(pX,pY,pZ,xOb,yOb,zOb)
+							end
+							if isChecked("Auto Interact") and not br.player.inCombat and GetDistanceBetweenPositions(pX,pY,pZ,xOb,yOb,zOb) <= 7 then
+								ObjectInteract(object)
+							end
+						end
+					end
+				end
+
+
+				if isChecked("Potions Tracker") then
+					--[[ local badpot = {"Blank","Red","Black","Green","Blue","Purple"} ]]
+					for _,v in pairs(br.lists.visions) do
+						if v == objectid then
+							local xOb, yOb, zOb = ObjectPosition(object)
+							local pX,pY,pZ = ObjectPosition("player")
+							if xOb ~= nil and GetDistanceBetweenPositions(pX,pY,pZ,xOb,yOb,zOb) <200 then
+								--LibDraw.Circle(xOb,yOb,zOb, 1)
+								LibDraw.Text(name.." "..objectid,"GameFontNormal",xOb,yOb,zOb+3)
+								if isChecked("Draw Lines to Tracked Objects") then 
+									LibDraw.Line(pX,pY,pZ,xOb,yOb,zOb)
+								end
+								if isChecked("Auto Interact") and not br.player.inCombat and GetDistanceBetweenPositions(pX,pY,pZ,xOb,yOb,zOb) <= 7 then
+									ObjectInteract(object)
+								end
+							end
+						end
+					end
+				end
+
+				if isChecked("Custom Tracker") then
+					if getOptionValue("Custom Tracker") ~= "" and string.len(getOptionValue("Custom Tracker")) >= 3 and string.match(strupper(name),strupper(getOptionValue("Custom Tracker"))) then
+						local xOb, yOb, zOb = ObjectPosition(object)
+						local pX,pY,pZ = ObjectPosition("player")
+						if xOb ~= nil and GetDistanceBetweenPositions(pX,pY,pZ,xOb,yOb,zOb) <200 then
+							--LibDraw.Circle(xOb,yOb,zOb, 1)
+							LibDraw.Text(name.." "..objectid,"GameFontNormal",xOb,yOb,zOb+3)
+							if isChecked("Draw Lines to Tracked Objects") then 
+								LibDraw.Line(pX,pY,pZ,xOb,yOb,zOb)
+							end
+							if isChecked("Auto Interact") and not br.player.inCombat and GetDistanceBetweenPositions(pX,pY,pZ,xOb,yOb,zOb) <= 7 then
+								ObjectInteract(object)
+							end
+						end
+					end
+				end
+			end
+		end
+	end
+end
