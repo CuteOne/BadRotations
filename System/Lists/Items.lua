344fabe26dad8b714f9d3a604a637680cbada6dd
Pangloo <42098476+Pangloo@users.noreply.github.com>
2020-03-21 11:44:59 -0600

    deleting redundant list

--- System/Lists/Items.lua
+++ System/Lists/Items.lua
@@ -126,11 +126,3 @@ br.lists.items = {
     -- Legendary 8.3 cloak
     shroudOfResolve                 = 169223,
 }
-
-br.lists.visions = {
-    [341337] = black, -- black
-    [341338] = blue, -- blue
-    [341339] = green, -- green
-    [341340] = purple, -- purple
-    [341341] = red, -- red
-}
