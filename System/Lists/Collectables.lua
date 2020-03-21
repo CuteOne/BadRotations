1eb868ec326861edca1727c6292e37c9871d4b57
Pangloo <42098476+Pangloo@users.noreply.github.com>
2020-03-21 12:02:19 -0600

    Meant to push this too. Last one i promise

--- System/Lists/Collectables.lua
+++ System/Lists/Collectables.lua
@@ -1,8 +1,10 @@
 if br.lists == nil then br.lists = {} end
 br.lists.visions = {
-    [341337] = black, -- black
-    [341338] = blue, -- blue
-    [341339] = green, -- green
-    [341340] = purple, -- purple
-    [341341] = red, -- red
+    341337, -- black
+    341338, -- blue
+    341339, -- green
+    341340, -- purple
+    341341, -- red
+    136633,
+    299265
 }
