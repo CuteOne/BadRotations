b5b25552fca6d3ad737657039e8d2b2a532c974c
221357 <index@student.pwr.edu.pl>
2020-06-23 16:13:03 +0200

    Assa Exsang tweaks

--- Rotations/Rogue/Assassination/AssassinationFiskee.lua
+++ Rotations/Rogue/Assassination/AssassinationFiskee.lua
@@ -1056,10 +1056,6 @@ local function runRotation()
         if talent.masterAssassin and combo >= 4 and debuff.rupture.refresh("target") and (debuff.vendetta.exists("target") or debuff.toxicBlade.exists("target")) then
             if cast.rupture("target") then return true end
         end
-        -- # Refresh rupture when we have Vendetta and don't have BotE buff Subter/Exsang
-        if (talent.exsanguinate or talent.subterfuge) and combo >= 4 and debuff.rupture.refresh("target") and debuff.vendetta.exists("target") and not buff.seethingRage.exists() then
-            if cast.rupture("target") then return true end
-        end  
         -- # Refresh garrote when we have Vendetta or Toxic Blade on a target with Master Assassin
         if talent.masterAssassin and debuff.garrote.refresh("target") and (debuff.vendetta.exists("target") or debuff.toxicBlade.exists("target")) then
             if cast.garrote("target") then return true end
@@ -1070,7 +1066,7 @@ local function runRotation()
         end
         -- # Envenom at 4+ (5+ with DS) CP. Immediately on 2+ targets, with Vendetta, or with TB, or with MA; otherwise wait for some energy. Also wait if Exsg combo is coming up.
         -- actions.direct=envenom,if=combo_points>=4+talent.deeper_stratagem.enabled&(debuff.vendetta.up|debuff.toxic_blade.up|energy.deficit<=25+variable.energy_regen_combined|!variable.single_target)&(!talent.exsanguinate.enabled|cooldown.exsanguinate.remains>2)
-        if combo >= (4 + dSEnabled) and ((debuff.vendetta.exists("target") or not useCDs() or ttd("target") < getOptionValue("CDs TTD Limit")) or debuff.toxicBlade.exists("target") or buff.masterAssassin.exists() or energyDeficit <= (25 + energyRegenCombined) or enemies10 > 1) and (not talent.exsanguinate or cd.exsanguinate.remain() > 2 or mode.exsang == 2 or ttd("target") < 8) then
+        if combo >= (4 + dSEnabled) and ((debuff.vendetta.exists("target") or not useCDs() or ttd("target") < getOptionValue("CDs TTD Limit")) or debuff.toxicBlade.exists("target") or buff.masterAssassin.exists() or energyDeficit <= (25 + energyRegenCombined) or enemies10 > 1) and (not talent.exsanguinate or cd.exsanguinate.remain() > 2 or not debuff.garrote.exists("target") or mode.exsang == 2 or ttd("target") < 8) then
             if cast.envenom("target") then return true end
         end
         -- actions.direct+=/variable,name=use_filler,value=combo_points.deficit>1|energy.deficit<=25+variable.energy_regen_combined|!variable.single_target
@@ -1137,9 +1133,14 @@ local function runRotation()
     end
 
     local function actionList_Dot()
+        -- # Special Garrote and Rupture setup prior to Exsanguinate cast
+        -- actions.dot+=/garrote,if=talent.exsanguinate.enabled&!exsanguinated.garrote&dot.garrote.pmultiplier<=1&cooldown.exsanguinate.remains<2&spell_targets.fan_of_knives=1&raid_event.adds.in>6&dot.garrote.remains*0.5<target.time_to_die
+        if mode.exsang == 1 and talent.exsanguinate and (mode.vanish == 0 or cd.vanish.remain() > 4) and not debuff.garrote.exsang("target") and debuff.garrote.applied("target") <= 1 and cd.exsanguinate.remain() < 2 and enemies10 == 1 and debuff.garrote.remain("target")*0.5 < ttd("target") then
+            if cast.garrote("target") then return true end
+        end
         -- # Special Rupture setup for Exsg
         -- actions.dot=rupture,if=talent.exsanguinate.enabled&((combo_points>=cp_max_spend&cooldown.exsanguinate.remains<1)|(!ticking&(time>10|combo_points>=2)))
-        if mode.exsang == 1 and enemies10 < 3 and talent.exsanguinate and ((combo >= comboMax and cd.exsanguinate.remain() < 1) or (not debuff.rupture.exists("target") and (combatTime > 10 or combo >= 2))) and ttd("target") > 10 then
+        if mode.exsang == 1 and enemies10 < 3 and talent.exsanguinate and (not debuff.garrote.refresh("target") or debuff.vendetta.exists("target")) and ((combo >= comboMax and cd.exsanguinate.remain() < 1) or (not debuff.rupture.exists("target") and (combatTime > 10 or combo >= 2))) and ttd("target") > 10 then
             if cast.rupture("target") then return true end
         end
         -- actions.dot+=/pool_resource,for_next=1
@@ -1211,7 +1212,7 @@ local function runRotation()
             for i = 1, #enemyTable5 do
                 local thisUnit = enemyTable5[i].unit
                 local garroteRemain = debuff.garrote.remain(thisUnit)
-                if shallWeDot(thisUnit) and garroteRemain <= 5.4 and (enemyTable5[i].ttd - garroteRemain) > 2 then
+                if shallWeDot(thisUnit) and garroteRemain <= 5.4 and (enemyTable5[i].ttd - garroteRemain) > 2 and not vanishCheck then
                     if cast.pool.garrote() then return true end
                     if cast.garrote(thisUnit) then return true end
                 end
@@ -1258,7 +1259,9 @@ local function runRotation()
                         if cast.garrote(garroteTable[i].unit) then return true end
                     end
                 else
-                    if cast.garrote(garroteTable[1].unit) then return true end
+                    if not vanishCheck then
+                        if cast.garrote(garroteTable[1].unit) then return true end
+                    end 
                 end
             end
         end
