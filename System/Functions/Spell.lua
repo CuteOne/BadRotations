340d55aa4ca47827c67c8b25f356004e2f51cf72
Ben Humphrey <44178664+cxmplex@users.noreply.github.com>
2020-07-08 18:20:31 -0400

    Update Spell.lua

--- System/Functions/Spell.lua
+++ System/Functions/Spell.lua
@@ -1,4 +1,5 @@
 function castInterrupt(SpellID,Percent,Unit)
+	Percent = Math.min(Percent + math.random(-6.5, 6.5), 99) end
 	if Unit == nil then Unit = "target" end
 	if GetObjectExists(Unit) then
 		local castName, _, _, castStartTime, castEndTime, _, _, castInterruptable = UnitCastingInfo(Unit)
