1ba491f19e238d59113f5b929abca02102eb6baa
Pangloo <42098476+Pangloo@users.noreply.github.com>
2020-08-29 12:01:14 -0600

    BM stuff, maybe good, maybe not?

--- Rotations/Hunter/BeastMastery/PangloBowelMovement.lua
+++ Rotations/Hunter/BeastMastery/PangloBowelMovement.lua
@@ -53,6 +53,12 @@ local function createToggles()
         [2] = { mode = "Off", value = 2 , overlay = "Best Cleave Target Disabled", tip = "Best Cleave Target Disabled", highlight = 0, icon = br.player.spell.misdirection }
     };
     CreateButton("BestCleave",0,1)
+    BestialWrathModes = {
+        [1] = { mode = "ON", value = 1 , overlay = "Use Bestial Wrath always", tip = "BW ALWAYS", highlight = 1, icon = br.player.spell.bestialWrath },
+        [2] = { mode = "CDS", value = 2 , overlay = "Use Besital Wrath on CD", tip = "BW CD Mode", highlight = 1, icon = br.player.spell.bestialWrath },
+        [3] = { mode = "OFF", value = 2 , overlay = "Do not use Bestial Wrath", tip = "BW OFF", highlight = 0, icon = br.player.spell.bestialWrath }
+    };
+    CreateButton("BestialWrath",1,1)
 end
 
 local function createOptions()
@@ -403,7 +409,6 @@ local function runRotation()
                         elseif (cd.freezingTrap.remains() > gcdFixed or not isChecked("Ice Trap")) and cast.able.feignDeath() and isChecked("Feign Thing") then
                             if cast.feignDeath() then
                                 feignTime = GetTime()
-                                return true
                             end
                         end
                     end
@@ -502,9 +507,12 @@ local function runRotation()
     end
 
     local function ST()
-        if useCDs() and isChecked("Aspect of the Wild") and cd.aspectOfTheWild.remains() <= 2 then
+        if (useCDs() and isChecked("Aspect of the Wild") and cd.aspectOfTheWild.remains() <= 2) and 
+        ((not cast.able.bestialWrath() and (mode.bestialWrath == 1 or mode.bestialWrath == 2 and useCDs())) or mode.bestialWrath == 3)
+        and (not cast.able.killCommand() or buff.frenzy.stack("pet")< 3) then
             if cast.barbedShot("target") then return end
         end
+
         if not Barb1 and talent.killerCobra and buff.danceOfDeath.remains() < (gcdFixed + 2) and charges.barbedShot.frac() < 1.3 then
             if buff.bestialWrath.exists("player") then
                 if useCDs() then if cast.aspectOfTheWild() then return end end
@@ -515,19 +523,11 @@ local function runRotation()
             end
         end
 
-
-        if Barb1 or ((buff.frenzy.exists("pet") and buff.frenzy.remains("pet") <= 2) or charges.barbedShot.frac() >= 1.3 or not buff.frenzy.exists("pet"))
-        then
-            if cast.barbedShot("target") then return end
-        end
-
-        
-
-        if hunterTTD() and not buff.aspectOfTheWild.exists() and isChecked("Aspect of the Wild") and useCDs() and (charges.barbedShot.frac() <= 1.2 or not traits.primalInstincts.active) then
+        if hunterTTD() and not buff.aspectOfTheWild.exists() and isChecked("Aspect of the Wild") and useCDs() and ((charges.barbedShot.frac() <= 1.2 or not traits.primalInstincts.active) or buff.frenzy.stack("pet")< 3) then
             if cast.aspectOfTheWild() then return end
         end
 
-        if hunterTTD() and (getOptionValue("Bestial Wrath") == 2 or (getOptionValue("Bestial Wrath") == 1 and useCDs())) and (buff.bestialWrath.remains() < gcdFixed) then
+        if hunterTTD() and (mode.bestialWrath == 1 or (mode.bestialWrath == 2 and useCDs())) and (buff.bestialWrath.remains() < gcdFixed) then
             if cast.bestialWrath() then return end
         end
 
@@ -550,7 +550,7 @@ local function runRotation()
     end
 
     local function AOE()
-        if useCDs() and isChecked("Aspect of the Wild") and cd.aspectOfTheWild.remains() <= 2 then
+        if (useCDs() and isChecked("Aspect of the Wild") and cd.aspectOfTheWild.remains() <= 2) and ((not cast.able.bestialWrath() and (mode.bestialWrath == 1 or mode.bestialWrath == 2 and useCDs())) or mode.bestialWrath == 3) then
             if cast.barbedShot("target") then return end
         end
         if (((buff.frenzy.exists("pet") and buff.frenzy.remains("pet") <= 2) or charges.barbedShot.frac() >= 1.8 or not buff.frenzy.exists("pet"))
@@ -567,11 +567,11 @@ local function runRotation()
             if AoEBarbed() then return end
         end
 
-        if hunterTTD() and not buff.aspectOfTheWild.exists() and isChecked("Aspect of the Wild") and useCDs() then
+        if hunterTTD() and not buff.aspectOfTheWild.exists() and isChecked("Aspect of the Wild") and useCDs() and ((charges.barbedShot.frac() <= 1.2 or not traits.primalInstincts.active) or buff.frenzy.stack("pet")< 3)then
             if cast.aspectOfTheWild() then return end
         end
 
-        if hunterTTD() and (getOptionValue("Bestial Wrath") == 2 or (getOptionValue("Bestial Wrath") == 1 and useCDs())) and (buff.bestialWrath.remains() < gcdFixed) then
+        if hunterTTD() and (mode.bestialWrath == 1 or (mode.bestialWrath == 2 and useCDs())) and (buff.bestialWrath.remains() < gcdFixed) then
             if cast.bestialWrath() then return end
         end
 
@@ -635,7 +635,7 @@ local function runRotation()
                 StartAttack()
         end
     end
-    if botSpell ~= spell.feignDeath or ((feignTime ~= nil and (GetTime() - feignTime > 2)) and not debuff.grandDelusions.exists("player")) then
+    if feignTime == nil or (feignTime ~= nil and (GetTime() - feignTime > 1.2)) then
         if offGCD() then return end
         if not Barb1 then 
             if defensive() then return end
