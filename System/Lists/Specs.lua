fde7af40769b917ac39e4523ccb975e7cb476e05
Ashley (CuteOne) <ph34rt3hcute1@gmail.com>
2020-10-16 19:29:05 -0400

    Specs List - Updated for 1-10 specIDs

--- System/Lists/Specs.lua
+++ System/Lists/Specs.lua
@@ -5,61 +5,73 @@ br.lists.spec = {
 	DEATHKNIGHT = {
 		Blood = 250,
 		Frost = 251,
-		Unholy = 252
+		Unholy = 252,
+		Initial = 1455,
 	},
 	DEMONHUNTER = {
 		Havoc = 577,
-		Vengeance = 581
+		Vengeance = 581,
+		Initial = 1456,
 	},
 	DRUID = {
 		Balance = 102,
 		Feral = 103,
 		Guardian = 104,
-		Restoration = 105
+		Restoration = 105,
+		Initial = 1447,
 	},
 	HUNTER = {
 		BeastMastery = 253,
 		Marksmanship = 254,
-		Survival = 255
+		Survival = 255,
+		Initial = 1448,
 	},
 	MAGE = {
 		Arcane = 62,
 		Fire = 63,
-		Frost = 64
+		Frost = 64,
+		Initial = 1449,
 	},
 	MONK = {
 		Brewmaster = 268,
 		Windwalker = 269,
-		Mistweaver = 270
+		Mistweaver = 270,
+		Initial = 1450,
 	},
 	PALADIN = {
 		Holy = 65,
 		Protection = 66,
-		Retribution = 70
+		Retribution = 70,
+		Initial = 1451,
 	},
 	PRIEST = {
 		Discipline = 256,
 		Holy = 257,
-		Shadow = 258
+		Shadow = 258,
+		Initial = 1452,
 	},
 	ROGUE = {
 		Assassination = 259,
 		Outlaw = 260,
-		Subtlety = 261
+		Subtlety = 261,
+		Initial = 1453,
 	},
 	SHAMAN = {
 		Elemental = 262,
 		Enhancement = 263,
-		Restoration = 264
+		Restoration = 264,
+		Initial = 1444,
 	},
 	WARLOCK = {
 		Affliction = 265,
 		Demonology = 266,
-		Destruction = 267
+		Destruction = 267,
+		Initial = 1451,
 	},
 	WARRIOR = {
 		Arms = 71,
 		Fury = 72,
-		Protection = 73
+		Protection = 73,
+		Initial = 1446,
 	}
 }
