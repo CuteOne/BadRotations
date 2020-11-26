b45a813e0eff9525bfdcd74a7927c2bcf44b49d4
Hassliebe <thorben@hassliebe.net>
2020-11-26 02:37:57 +0100

    Initial Profile
    - redundant stuff ripped out
    - cleaned up
    - shared abilities added

--- System/Lists/Spells.lua
+++ System/Lists/Spells.lua
@@ -3569,7 +3569,6 @@ br.lists.spells = {
                 recklessness                = 1719,
                 siegebreaker                = 280772,
                 whirlwind                   = 190411,
-                ignorePain                  = 190456,
             },
             artifacts                       = {
 
@@ -3581,7 +3580,6 @@ br.lists.spells = {
                 recklessness                = 1719,
                 suddenDeath                 = 280776,
                 whirlwind                   = 85739,
-                ignorePain                  = 190456,
             },
             debuffs                         = {
                 siegebreaker                = 280773,
@@ -3622,7 +3620,6 @@ br.lists.spells = {
                 demoralizingShout           = 1160,
                 devastate                   = 20243,
                 focusedRage                 = 204488,
-                ignorePain                  = 190456,
                 impendingVictory            = 202168,
                 intercept                   = 198304,
                 lastStand                   = 12975,
@@ -3632,7 +3629,6 @@ br.lists.spells = {
                 shieldSlam                  = 23922,
                 shieldWall                  = 871,
                 shockwave                   = 46968,
-                spellReflection             = 23920,
                 thunderClap                 = 6343,
             },
             artifacts                       = {
@@ -3641,12 +3637,10 @@ br.lists.spells = {
             buffs                           = {
                 avatar                      = 107574,
                 defensiveStance             = 71,
-                ignorePain                  = 190456,
                 lastStand                   = 12975,
                 revenge                     = 5302,
                 shieldBlock                 = 132404,
                 shieldWall                  = 871,
-                spellReflection             = 23920,
                 vengeanceFocusedRage        = 202573,
                 vengeanceIgnorePain         = 202574,
                 vengeanceRevenge            = 202573,
@@ -3700,6 +3694,11 @@ br.lists.spells = {
                 stormBolt                   = 107570,
                 taunt                       = 355,
                 victoryRush                 = 34428,
+                ignorePain                  = 190456,
+                shatteringThrow             = 64382,
+                spellReflection             = 23920,
+                challengingShout            = 1161,
+                intervene                   = 3411,
                 whirlwind                   = 1680,
             },
             artifacts                       = {
@@ -3707,6 +3706,8 @@ br.lists.spells = {
             },
             buffs                           = {
                 battleShout                 = 6673,
+                ignorePain                  = 190456,
+                spellReflection             = 23920,
                 berserkerRage               = 18499,
                 fujiedasFury                = 207776,
                 victorious                  = 32216,
