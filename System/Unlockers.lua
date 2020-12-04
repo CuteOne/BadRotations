58cdba496d6adba395f9f980e915a2086c53cd12
Pangloo <42098476+Pangloo@users.noreply.github.com>
2020-12-04 14:45:59 -0600

    Very initial fix due to update

--- System/Unlockers.lua
+++ System/Unlockers.lua
@@ -264,7 +264,7 @@ function br:checkBrOutOfDate()
         local startTime = debugprofilestop()
         -- Request Current Version from GitHub
         if EasyWoWToolbox ~= nil then -- EWT
-            SendHTTPRequest('https://raw.githubusercontent.com/CuteOne/BadRotations/master/BadRotations.toc', nil, function(body) brcurrVersion =(string.match(body, "(%d+%p%d+%p%d+)")) end)
+            --SendHTTPRequest('https://raw.githubusercontent.com/CuteOne/BadRotations/master/BadRotations.toc', nil, function(body) brcurrVersion =(string.match(body, "(%d+%p%d+%p%d+)")) end)
             
             -- Check for commit updates from System/Updater.lua, which relies on EWT
             br.updater:CheckOutdated()
