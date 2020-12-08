be2e512e16407a75e1dcadce4fd66826e1a36c06
Kuukuu <echidnablade@gmail.com>
2020-12-08 00:22:55 -0800

    System - Level check on runeforging
    Maybe fix IsRuneforgeLegendary bug?  Who knows....

--- System/API/Runeforge.lua
+++ System/API/Runeforge.lua
@@ -32,16 +32,17 @@ br.api.runeforge = function(runeforge,k,v)
         end
         return false
     end
-    
-    for i = 1, #itemLeggoSlots do
-        if GetInventoryItemID("player",i) ~= nil then
-            item = ItemLocation:CreateFromEquipmentSlot(itemLeggoSlots[i])
-            isLeggo = C_LegendaryCrafting.IsRuneforgeLegendary(item)
-            if isLeggo then
-                slotPowers = C_LegendaryCrafting.GetRuneforgePowers(item,1)
-                runeforge[k].equiped = findPowerBySlot(1) or findPowerBySlot(2)
+    if UnitLevel("player") >= 50 then
+        for i = 1, #itemLeggoSlots do
+            if GetInventoryItemID("player",i) ~= nil then
+                item = ItemLocation:CreateFromEquipmentSlot(itemLeggoSlots[i])
+                isLeggo = C_LegendaryCrafting.IsRuneforgeLegendary(item)
+                if isLeggo then
+                    slotPowers = C_LegendaryCrafting.GetRuneforgePowers(item,1)
+                    runeforge[k].equiped = findPowerBySlot(1) or findPowerBySlot(2)
+                end
             end
+            if runeforge[k].equiped then break end
         end
-        if runeforge[k].equiped then break end
     end
 end
