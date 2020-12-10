89ea6449639ee4640a4de290100eb1d21cffc5ca
Kuukuu <echidnablade@gmail.com>
2020-12-09 20:11:25 -0800

    RSham - Version update I keep forgetting to do

--- Rotations/Shaman/Restoration/RestoKuu.lua
+++ Rotations/Shaman/Restoration/RestoKuu.lua
@@ -72,7 +72,7 @@ local function createOptions()
     local function rotationOptions()
         local section
     -- General Options
-        section = br.ui:createSection(br.ui.window.profile, "General - Version 1.01")
+        section = br.ui:createSection(br.ui.window.profile, "General - Version 1.1")
             br.ui:createCheckbox(section, "Disable Auto Ground Cast Circle Cancel")
             br.ui:createCheckbox(section,"OOC Healing","|cff15FF00Enables|cffFFFFFF/|cffD60000Disables |cffFFFFFFout of combat healing|cffFFBB00.")
         -- Dummy DPS Test
