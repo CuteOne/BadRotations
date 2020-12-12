bdd705fc08d8f8edf048fe1eb75d1e73ef3fa410
Ashley (CuteOne) <ph34rt3hcute1@gmail.com>
2020-12-12 14:21:57 -0500

    Runeforge API - Fix for IsRuneforgeLegendary check

--- System/API/Runeforge.lua
+++ System/API/Runeforge.lua
@@ -6,18 +6,18 @@ br.api.runeforge = function(runeforge,k,v)
     local item
     local isLeggo = false
     local itemLeggoSlots = {
-        [1] = 1,
-        [2] = 2,
-        [3] = 3,
-        [4] = 5,
-        [5] = 6,
-        [6] = 7,
-        [7] = 8,
-        [8] = 9,
-        [9] = 10,
-        [10] = 11,
-        [11] = 12,
-        [12] = 15,
+        [1] = 1, -- Head
+        [2] = 2, -- Neck
+        [3] = 3, -- Shoulder
+        [4] = 5, -- Chest
+        [5] = 6, -- Waist
+        [6] = 7, -- Legs
+        [7] = 8, -- Feet
+        [8] = 9, -- Wrists
+        [9] = 10, -- Hands
+        [10] = 11, -- Finger1
+        [11] = 12, -- Finger2
+        [12] = 15, -- Back
     }
     local slotPowers = {}
 
@@ -32,17 +32,18 @@ br.api.runeforge = function(runeforge,k,v)
         end
         return false
     end
-    if isChecked("Legendary Support") then
-        for i = 1, #itemLeggoSlots do
-            if GetInventoryItemID("player",i) ~= nil then
-                item = ItemLocation:CreateFromEquipmentSlot(itemLeggoSlots[i])
+    for i = 1, #itemLeggoSlots do
+        if GetInventoryItemID("player",i) ~= nil then
+            -- From: https://wow.gamepedia.com/ItemLocationMixin
+            item = ItemLocation:CreateFromEquipmentSlot(itemLeggoSlots[i])
+            if item:IsValid() then
                 isLeggo = C_LegendaryCrafting.IsRuneforgeLegendary(item)
                 if isLeggo then
                     slotPowers = C_LegendaryCrafting.GetRuneforgePowers(item,1)
                     runeforge[k].equiped = findPowerBySlot(1) or findPowerBySlot(2)
                 end
             end
-            if runeforge[k].equiped then break end
         end
+        if runeforge[k].equiped then break end
     end
 end
