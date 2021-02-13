633d5577f1a0e22ccddc07d787331f0b731c2c43
Kuukuu <echidnablade@gmail.com>
2021-02-12 16:43:45 -0800

    SlashCommands.lua update

--- System/SlashCommands.lua
+++ System/SlashCommands.lua
@@ -26,7 +26,7 @@ function br.SlashCommandHelp(cmd, msg)
 		end
 		return
 	end
-	table.insert(br.commandHelp,"|cffFFFFFF        /" .. cmd .. "|cffFFDD11 - " .. msg)
+	table.insert(br.commandHelp, "|cffFFFFFF        /" .. cmd .. "|cffFFDD11 - " .. msg)
 	-- br.commandHelp = br.commandHelp .. "|cffFFFFFF\n        /" .. cmd .. "|cffFFDD11 - " .. msg
 end
 
@@ -44,11 +44,12 @@ local function toggleUI()
 end
 
 local function toggle(name, index, check)
+	local toggleFound = false
 	if check == nil then
 		check = false
 	end
-	if br.toggleFound == nil then
-		br.toggleFound = false
+	if toggleFound == nil then
+		toggleFound = false
 	end
 	for k, _ in pairs(br.data.settings[br.selectedSpec].toggles) do
 		local toggle = string.upper(name) --name --(name:gsub("^%l", string.upper))
@@ -60,10 +61,10 @@ local function toggle(name, index, check)
 				break
 			end
 			if index == nil then
-				ToggleValue(toggle)
+				br.ToggleValue(toggle)
 				break
 			else
-				ToggleToValue(toggle, index)
+				br.ToggleToValue(toggle, index)
 				break
 			end
 		end
@@ -90,11 +91,11 @@ local function updateRate()
 end
 
 local function forewardDisengage() -- from Stinky Twitch
-	local s, d, e = GetSpellCooldown(781)
+	local s, d, e = br._G.GetSpellCooldown(781)
 	if s == 0 then
-		FaceDirection(mod(ObjectFacing("player") + math.pi, math.pi * 2), true)
-		CastSpellByID(781)
-		FaceDirection(mod(ObjectFacing("player") + math.pi, math.pi * 2), true)
+		br._G.FaceDirection(_G.mod(br._G.ObjectFacing("player") + math.pi, math.pi * 2), true)
+		br._G.CastSpellByID(781)
+		br._G.FaceDirection(_G.mod(br._G.ObjectFacing("player") + math.pi, math.pi * 2), true)
 	end
 end
 
@@ -112,7 +113,7 @@ function br:slashHelpList()
 	br.SlashCommandHelp("br queue remove spellId", "Removes the Spell from the Queue by Spell Id.")
 	br.SlashCommandHelp("br ui", "Shows a list of toggleable UI elements, IE: /br ui main")
 	br.SlashCommandHelp("br updaterate", "Displays Current Update Rate")
-	if select(2, UnitClass("player")) == "HUNTER" then
+	if select(2, br._G.UnitClass("player")) == "HUNTER" then
 		br.SlashCommandHelp("br disengage", "Assign to macro to Forward Disengage.")
 	end
 	br.SlashCommandHelp("br healing", "Adds/Removes Target from Healing List")
@@ -120,7 +121,7 @@ function br:slashHelpList()
 end
 
 br:slashHelpList()
-function handler(message, editbox)
+function br.handler(message, editbox)
 	local msg = string.lower(message)
 	local msg1 = getStringIndex(message, 1)
 	local msg2 = getStringIndex(message, 2)
@@ -129,14 +130,18 @@ function handler(message, editbox)
 	local msg5 = getStringIndex(message, 5)
 	local msg6 = getStringIndex(message, 6)
 	local msg7 = getStringIndex(message, 7)
+	local targetUnit
+	local specialCast
+	local minCount
+	local range
 	if msg == "" or msg == nil then
--- Main On/Off		
+		-- Main On/Off
 		toggleUI()
 	elseif msg == "help" then
--- Help		
+		-- Help
 		br.SlashCommandHelp("Print Help")
 	elseif msg1 == "blacklist" then
--- Blacklist		
+		-- Blacklist
 		if msg2 == "dump" then
 			br._G.print("|cffFF0000Blacklist:")
 			if #br.data.blackList == (0 or nil) then
@@ -155,14 +160,14 @@ function handler(message, editbox)
 				br.data.blackList = {}
 			end
 			if br.GetUnitExists("mouseover") then
-				local mouseoverName = UnitName("mouseover")
-				local mouseoverGUID = UnitGUID("mouseover")
+				local mouseoverName = br._G.UnitName("mouseover")
+				local mouseoverGUID = br._G.UnitGUID("mouseover")
 				-- Now we're trying to find that unit in the blackList table to remove
 				local found
 				for k, v in pairs(br.data.blackList) do
 					-- Now we're trying to find that unit in the Cache table to remove
-					if UnitGUID("mouseover") == v.guid then
-						tremove(br.data.blackList, k)
+					if br._G.UnitGUID("mouseover") == v.guid then
+						br._G.tremove(br.data.blackList, k)
 						br._G.print("|cffFFDD11" .. mouseoverName .. "|cffFF0000 Removed from Blacklist")
 						found = true
 					--blackList[k] = nil
@@ -170,14 +175,14 @@ function handler(message, editbox)
 				end
 				if not found then
 					br._G.print("|cffFFDD11" .. mouseoverName .. "|cffFF0000 Added to Blacklist")
-					tinsert(br.data.blackList, {guid = mouseoverGUID, name = mouseoverName})
+					br._G.tinsert(br.data.blackList, {guid = mouseoverGUID, name = mouseoverName})
 				end
 			end
 		else
 			br._G.print("Invalid Option for: |cFFFF0000" .. msg1 .. "|r try |cffFFDD11 /br help |r for available options.")
 		end
 	elseif msg1 == "pause" then
--- Pause
+		-- Pause
 		--[[if msg2 == "hold" then
 			br.ChatOverlay("Profile Paused")
 			return true
@@ -192,10 +197,15 @@ function handler(message, editbox)
 				br.data.settings[br.selectedSpec].toggles["Pause"] = 0
 			end
 		else
-			br._G.print("Invalid Option for: |cFFFF0000" ..	msg1 ..	"|r try " --[["|cffFFDD11 /br pause hold |r - Pauses while held (via macro) or ]] .. "|cffFFDD11 /br pause toggle |r - Switches Pause On/Off")
+			br._G.print(
+				"Invalid Option for: |cFFFF0000" ..
+					msg1 ..
+						"|r try " --[["|cffFFDD11 /br pause hold |r - Pauses while held (via macro) or ]] ..
+							"|cffFFDD11 /br pause toggle |r - Switches Pause On/Off"
+			)
 		end
 	elseif msg1 == "toggle" then
--- Toggles
+		-- Toggles
 		if msg2 ~= nil then
 			if toggle(msg2, msg3, true) then
 				toggle(msg2, msg3)
@@ -206,7 +216,7 @@ function handler(message, editbox)
 			br._G.print("No Toggle Specified: Try |cffFFDD11 /br help |r for list of toggles.")
 		end
 	elseif msg1 == "queue" then
--- Queue
+		-- Queue
 		if msg2 == "clear" then
 			if br.player.queue == nil then
 				br._G.print("Queue Already Cleared")
@@ -222,30 +232,51 @@ function handler(message, editbox)
 			if msg3 == nil then
 				br._G.print("No Spell Provided to add to Queue.")
 			elseif msg3 == "help" then
-				br._G.print("Queue Add Conditions\n"..
-					"Example: /br queue add 123456 player aoe 3 8\n\n"..
-					"123456 = Spell ID, this is required for queue.\n"..
-					"player = Target (optional), where to cast spell using standard macro targets or (nil, best, playerGround, targetGround, pettarget)\n"..
-					"aoe    = Cast Type (optional), if the spell you are queuing has special cast type, (nil, aoe, cone, rect, ground, dead)\n"..
-					"3 	   = Minimal Units to cast on (optional), specify the minimal number of units required to cast the spell or nil for none.\n"..
-					"8      = Spells effect range (optional), specify the damage effect range of the spell or nil for none.\n\n"..					
-					"This will cast spell 123456 on \"player\" as an AOE once there are at least 3 units within 8 yards of the player.\n"
+				br._G.print(
+					"Queue Add Conditions\n" ..
+						"Example: /br queue add 123456 player aoe 3 8\n\n" ..
+							"123456 = Spell ID, this is required for queue.\n" ..
+								"player = Target (optional), where to cast spell using standard macro targets or (nil, best, playerGround, targetGround, pettarget)\n" ..
+									"aoe    = Cast Type (optional), if the spell you are queuing has special cast type, (nil, aoe, cone, rect, ground, dead)\n" ..
+										"3 	   = Minimal Units to cast on (optional), specify the minimal number of units required to cast the spell or nil for none.\n" ..
+											"8      = Spells effect range (optional), specify the damage effect range of the spell or nil for none.\n\n" ..
+												'This will cast spell 123456 on "player" as an AOE once there are at least 3 units within 8 yards of the player.\n'
 				)
 			else
-				local spellName, _, _, _, _, _, spellId = GetSpellInfo(msg3)
-				local notOnCD = true 
-				if br ~= nil and br.player ~= nil and spellName ~= nil then notOnCD = br.getSpellCD(spellName) <= br.player.gcdMax end
-				if msg4 ~= nil then targetUnit = tostring(msg4)	end
-				if msg5 ~= nil then specialCast = tostring(msg5) end 
-				if msg6 ~= nil then minCount = tonumber(msg6) end 
-				if msg7 ~= nil then range = tonumber(msg7) end 
+				local spellName, _, _, _, _, _, spellId = br._G.GetSpellInfo(msg3)
+				local notOnCD = true
+				if br ~= nil and br.player ~= nil and spellName ~= nil then
+					notOnCD = br.getSpellCD(spellName) <= br.player.gcdMax
+				end
+				if msg4 ~= nil then
+					targetUnit = tostring(msg4)
+				end
+				if msg5 ~= nil then
+					specialCast = tostring(msg5)
+				end
+				if msg6 ~= nil then
+					minCount = tonumber(msg6)
+				end
+				if msg7 ~= nil then
+					range = tonumber(msg7)
+				end
 				if spellName == nil then
 					br._G.print("Invalid Spell ID: |cffFFDD11 Unable to add.")
-				elseif not notOnCD then 
+				elseif not notOnCD then
 					br._G.print("Spell |cFFFF0000" .. spellName .. "|r not added, cooldown greater than gcd.")
 				else
 					if #br.player.queue == 0 and notOnCD then
-						tinsert(br.player.queue, {id = spellId, name = spellName, target = targetUnit, castType = specialCast, minUnits = mincount, effectRng = range})
+						br._G.tinsert(
+							br.player.queue,
+							{
+								id = spellId,
+								name = spellName,
+								target = targetUnit,
+								castType = specialCast,
+								minUnits = minCount,
+								effectRng = range
+							}
+						)
 						br._G.print("Added |cFFFF0000" .. spellName .. "|r to the queue.")
 					elseif #br.player.queue ~= 0 then
 						for i = 1, #br.player.queue do
@@ -253,7 +284,17 @@ function handler(message, editbox)
 								br._G.print("|cFFFF0000" .. spellName .. "|r is already queued.")
 								break
 							elseif notOnCD then
-								tinsert(br.player.queue, {id = spellId, name = spellName, target = targetUnit, castType = specialCast, minUnits = mincount, effectRng = range})
+								br._G.tinsert(
+									br.player.queue,
+									{
+										id = spellId,
+										name = spellName,
+										target = targetUnit,
+										castType = specialCast,
+										minUnits = minCount,
+										effectRng = range
+									}
+								)
 								br._G.print("Added |cFFFF0000" .. spellName .. "|r to the queue.")
 								break
 							end
@@ -265,12 +306,12 @@ function handler(message, editbox)
 			if msg3 == nil then
 				br._G.print("No Spell Provided to remove from Queue.")
 			else
-				local spellName, _, _, _, _, _, spellId = GetSpellInfo(msg3)
+				local spellName, _, _, _, _, _, spellId = br._G.GetSpellInfo(msg3)
 				local removedSpell = false
 				if #br.player.queue ~= 0 then
 					for i = 1, #br.player.queue do
 						if spellId == br.player.queue[i].id then
-							tremove(br.player.queue, i)
+							br._G.tremove(br.player.queue, i)
 							br._G.print("Removed |cFFFF0000" .. spellName .. "|r from the queue.")
 							removedSpell = true
 							break
@@ -286,13 +327,17 @@ function handler(message, editbox)
 				end
 			end
 		elseif msg2 == nil then
-			br._G.print("Invalid Option for: |cFFFF0000" .. msg1 .. "|r try |cffFFDD11 /br queue clear |r - Clears the Queue list or |cffFFDD11 /br queue add (spell)|r - Adds specified spell to Queue list or |cffFFDD11 /br queue remove (spell) |r - Removes specifid from Queue list.")
+			br._G.print(
+				"Invalid Option for: |cFFFF0000" ..
+					msg1 ..
+						"|r try |cffFFDD11 /br queue clear |r - Clears the Queue list or |cffFFDD11 /br queue add (spell)|r - Adds specified spell to Queue list or |cffFFDD11 /br queue remove (spell) |r - Removes specifid from Queue list."
+			)
 		end
 	elseif msg == "updaterate" then
--- Show Update Rate
+		-- Show Update Rate
 		updateRate()
 	elseif msg1 == "healing" then
--- Special Heal List
+		-- Special Heal List
 		local unit
 		if br.GetUnitExists("mouseover") then
 			unit = "mouseover"
@@ -302,8 +347,8 @@ function handler(message, editbox)
 			unit = "target"
 		end
 		if unit then
-			local unitName = UnitName(unit)
-			local unitGUID = UnitGUID(unit)
+			local unitName = br._G.UnitName(unit)
+			local unitGUID = br._G.UnitGUID(unit)
 			if br.novaEngineTables.SpecialHealUnitList[unitGUID] ~= nil then
 				br.novaEngineTables.SpecialHealUnitList[unitGUID] = nil
 				br._G.print("|cffFFDD11" .. unitName .. "|cffFF0000 Removed from Special Heal List")
@@ -315,11 +360,11 @@ function handler(message, editbox)
 			br._G.print("No Target Found.  Please Select a Target Before Adding")
 		end
 	elseif msg1 == "debuff" then
--- Debuff List
+		-- Debuff List
 		if msg2 == nil then
 			br._G.print("No Spell Provided to Add")
 		else
-			local spellName, _, _, _, _, _, spellId = GetSpellInfo(msg3)
+			local spellName, _, _, _, _, _, spellId = br._G.GetSpellInfo(msg3)
 			if spellName == nil then
 				br._G.print("Invalid Spell ID: |cffFFDD11 Unable to add.")
 			else
@@ -333,14 +378,15 @@ function handler(message, editbox)
 			end
 		end
 	elseif msg == "disengage" then
--- Forward Disengage
+		-- Forward Disengage
 		if _G.IsLeftShiftKeyDown() then
-			br._G.br._G.CastSpellByName(GetSpellInfo(781))
+			br._G.br._G.CastSpellByName(br._G.GetSpellInfo(781))
 		else
 			forewardDisengage()
 		end
 	elseif msg1 == "ui" then
--- Other
+		-- Updater
+		-- Other
 		if msg2 == "main" then
 			-- Show/Hide Bot Options
 			br.ui:toggleWindow("config")
@@ -360,61 +406,59 @@ function handler(message, editbox)
 					br.mainButton:Show()
 				end
 			end
-		elseif msg2 == "icon" then 
-			if hiddenIcon == nil or hiddenIcon == false then 
-				BadRotationsButton:Hide()
-				hiddenIcon = true
+		elseif msg2 == "icon" then
+			if br.hiddenIcon == nil or br.hiddenIcon == false then
+				br.BadRotationsButton:Hide()
+				br.hiddenIcon = true
 			else
-				BadRotationsButton:Show()
-				hiddenIcon = false 
+				br.BadRotationsButton:Show()
+				br.hiddenIcon = false
 			end
 		elseif msg2 == nil then
 			-- Show UI Options
-			br._G.print("Please provide one of the following options with showUI\n" .. 
-				"|cFFFF0000 main |r - Shows/Hides main bot options\n" .. 
-				"|cFFFF0000 profile |r - Shows/Hides profile options\n" .. 
-				"|cFFFF0000 togglebar |r - Shows/Hides toggle bar\n" .. 
-				"|cFFFF0000 icon |r - Shows/Hides minimap button\n"
+			br._G.print(
+				"Please provide one of the following options with showUI\n" ..
+					"|cFFFF0000 main |r - Shows/Hides main bot options\n" ..
+						"|cFFFF0000 profile |r - Shows/Hides profile options\n" ..
+							"|cFFFF0000 togglebar |r - Shows/Hides toggle bar\n" .. "|cFFFF0000 icon |r - Shows/Hides minimap button\n"
 			)
 		end
-
--- Updater
 	elseif msg1 == "update" then
 		br.updater:Update()
 	else
 		br._G.print("Invalid Command: |cFFFF0000" .. msg .. "|r try |cffFFDD11 /br help")
 	end
 end
-SlashCmdList["BR"] = handler
+br._G.SlashCmdList["BR"] = br.handler
 
 -- macro used to gather caster/spell/buff on our actual target
 SLASH_dumpInfo1 = "/dumpinfo"
-function SlashCmdList.dumpInfo(msg, editbox)
+function br._G.SlashCmdList.dumpInfo(msg, editbox)
 	-- find unit in our engines
-	for k, v in pairs(br.enemy) do
-		if br.enemy[k].guid == UnitGUID("target") then
-			targetInfo = { }
-			targetInfo.name = UnitName("target")
+	for k, _ in pairs(br.enemy) do
+		if br.enemy[k].guid == br._G.UnitGUID("target") then
+			_G.targetInfo = {}
+			_G.targetInfo.name = br._G.UnitName("target")
 			local thisUnit = br.enemy[k].unit
-			targetInfo.unitID = thisUnit.id
-			local buff1 = UnitBuff("target",1)
-			local buff2 = UnitBuff("target",2)
-			local deBuff1 = UnitBuff("target",1)
-			local deBuff2 = UnitBuff("target",2)
+			_G.targetInfo.unitID = thisUnit.id
+			local buff1 = br._G.UnitBuff("target", 1)
+			local buff2 = br._G.UnitBuff("target", 2)
+			local deBuff1 = br._G.UnitBuff("target", 1)
+			local deBuff2 = br._G.UnitBuff("target", 2)
 			if buff1 then
-				targetInfo.buff1 = buff1
+				_G.targetInfo.buff1 = buff1
 			end
 			if buff2 then
-				targetInfo.buff2 = buff2
+				_G.targetInfo.buff2 = buff2
 			end
 			if deBuff1 then
-				targetInfo.deBuff1 = deBuff1
+				_G.targetInfo.deBuff1 = deBuff1
 			end
 			if deBuff2 then
-				targetInfo.deBuff2 = deBuff2
+				_G.targetInfo.deBuff2 = deBuff2
 			end
-			RunMacroText("/dump targetInfo")
-			targetInfo = { }
+			br._G.RunMacroText("/dump targetInfo")
+			_G.targetInfo = {}
 			break
 		end
 	end
