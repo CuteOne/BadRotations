219448533171f0dc718fd9c1e06d36eabd66206d
PsyonicBR <56609657+PsyonicBR@users.noreply.github.com>
2021-04-04 21:13:36 -0700

    Update Tracker.lua

--- System/engines/Tracker.lua
+++ System/engines/Tracker.lua
@@ -1,5 +1,5 @@
 local _, br = ...
-local LibDraw = LibStub("LibDraw-1.0")
+local LibDraw = LibStub("LibDraw-BR")
 
 local function trackObject(object, name, objectid, interact)
     local xOb, yOb, zOb = br._G.ObjectPosition(object)
