1dd3f3618b7dc0181ee3aa6a9cc3df75f1f2589b
Jakub Kulewicz <jakubkulewicz05@gmail.com>
2021-05-20 14:16:37 +0200

    Sub: SBS logic for shades

--- Rotations/Rogue/Subtlety/SubS0ul.lua
+++ Rotations/Rogue/Subtlety/SubS0ul.lua
@@ -751,7 +751,8 @@ local function runRotation()
                         )
                     end
                     for i = 1, #spikeList do
-                        if br.isSafeToAttack(spikeList[i]) and (not debuff.serratedBoneSpikeDot.exists(spikeList[i]) or charges.serratedBoneSpike.frac() >= 2.75) then
+                        if br.isSafeToAttack(spikeList[i]) and (unit.hp(spikeList[i]) < 90 or br.getUnitID(thisUnit) ~= 171557) and
+                         (not debuff.serratedBoneSpikeDot.exists(spikeList[i]) or charges.serratedBoneSpike.frac() >= 2.75) then
                             if cast.serratedBoneSpike(spikeList[i]) then
                                 return true
                             end
