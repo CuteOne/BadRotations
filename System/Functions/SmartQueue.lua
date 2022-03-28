0286fadaf953cfef3184a7b5d9d8d55d108737e7
ph34rt3hcute1 <ph34rt3hcute1@gmail.com>
2022-03-28 09:51:20 -0400

    update SmartQueue.lua

--- System/Functions/SmartQueue.lua
+++ System/Functions/SmartQueue.lua
@@ -57,7 +57,7 @@ local function GetKeyBindings()
 			end
 		end
 		local bonusBar = br._G.GetBonusBarOffset()
-		local slotID = (1 + (_G["NUM_ACTIONBAR_PAGES"] + bonusBar - 1) * _["NUM_ACTIONBAR_BUTTONS"])
+		local slotID = (1 + (_G["NUM_ACTIONBAR_PAGES"] + bonusBar - 1) * _G["NUM_ACTIONBAR_BUTTONS"])
 		if (bonusBar == 0 and slot <= 12) or (bonusBar > 0 and slot >= slotID and slot < (slotID + 12)) then
 			name = "ACTIONBUTTON" .. (((slot - 1)%12) + 1)
 		elseif slot <= 36 then
