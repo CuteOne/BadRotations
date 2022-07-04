62b4747d0279d31f78fb96632a5fbc6670c14290
Ashley (CuteOne) <ph34rt3hcute1@gmail.com>
2022-07-04 13:19:45 -0400

    Rogue - Update Initial Rogue Profile

--- Rotations/Rogue/Initial/InitialRogue.lua
+++ Rotations/Rogue/Initial/InitialRogue.lua
@@ -1,4 +1,4 @@
-local rotationName = "Overlord"
+local rotationName = "Initial"
 
 ---------------
 --- Toggles ---
@@ -16,6 +16,12 @@ local function createToggles()
         [2] = { mode = "Off", value = 2 , overlay = "Defensive Disabled", tip = "No Defensives will be used.", highlight = 0, icon = br.player.spell.crimsonVial}
     };
     br.ui:createToggle(DefensiveModes,"Defensive",2,0)
+    -- Interrupts Button
+    local InterruptModes = {
+        [1] = { mode = "On", value = 1 , overlay = "Interrupts Enabled", tip = "Use Interrupt Abilities", highlight = 1, icon = br.player.spell.kick},
+        [2] = { mode = "Off", value = 2 , overlay = "Interrupts Disabled", tip = "Do Not Use Interrupt Abilities", highlight = 0, icon = br.player.spell.kick}
+    };
+    br.ui:createToggle(InterruptModes,"Interrupt",3,0)
 end
 
 ---------------
@@ -30,23 +36,44 @@ local function createOptions()
         --- GENERAL OPTIONS ---
         -----------------------
         section = br.ui:createSection(br.ui.window.profile,  "General")
-        br.ui:createCheckbox(section, "Auto Sprint", "Will use sprint automatically.")
-        br.ui:createCheckbox(section, "Always Stealth", "Will use stealth at all times.")
-        br.ui:createCheckbox(section, "Use Eviscerate", "Will use Eviscerate.")
-        br.ui:createCheckbox(section, "Use Slice and Dice", "Will use Slice and Dice.")
-
+            -- Eviscerate Execute
+            br.ui:createDropdownWithout(section, "Eviscerate Execute",{"|cffFFFF00Enabled Notify","|cff00FF00Enabled","|cffFF0000Disabled"}, 2,"Options for using Eviscerate when the damage from it will kill the unit.")
+            -- Sprint
+            br.ui:createCheckbox(section, "Auto Sprint", "Will use sprint automatically.")
+            -- Stealth
+            br.ui:createCheckbox(section, "Always Stealth", "Will use stealth at all times.")
+            -- Stealth Breaker
+            br.ui:createDropdownWithout(section, "Stealth Breaker", {"|cff00FF00Ambush","|cffFFFF00Cheapshot","|cffFF0000Sinister Strike"}, 3, "|cffFFFFFFSet what to break Stealth with.")
         br.ui:checkSectionState(section)
         -------------------------
         --- DEFENSIVE OPTIONS ---
         -------------------------
         section = br.ui:createSection(br.ui.window.profile, "Defensive")
-        br.ui:createSpinner(section, "Crimson Vial", 70, 0, 100, 5, "|cffFFFFFFHealth Percent to Cast At")
-
+            -- Crimson Vial
+            br.ui:createSpinner(section, "Crimson Vial", 70, 0, 100, 5, "|cffFFFFFFHealth Percent to Cast At")
+        br.ui:checkSectionState(section)
+        -------------------------
+        --- INTERRUPT OPTIONS ---
+        -------------------------
+        section = br.ui:createSection(br.ui.window.profile, "Interrupt")
+            -- Kick
+            br.ui:createCheckbox(section, "Kick", "|cffFFFFFFUse Kick")
+            -- Interrupt Percentage
+            br.ui:createSpinnerWithout(section, "Interrupt At",  0,  0,  95,  5,  "|cffFFFFFFCast Percent to Cast At (0 is random)")
         br.ui:checkSectionState(section)
         ----------------------
         --- TOGGLE OPTIONS ---
         ----------------------
         section = br.ui:createSection(br.ui.window.profile,  "Toggle Keys")
+            -- Single/Multi Toggle
+            br.ui:createDropdownWithout(section, "Rotation Mode", br.dropOptions.Toggle,  6)
+            -- Defensive Key Toggle
+            br.ui:createDropdownWithout(section, "Defensive Mode", br.dropOptions.Toggle,  6)
+            -- Interrupts Key Toggle
+            br.ui:createDropdownWithout(section, "Interrupt Mode", br.dropOptions.Toggle,  6)
+            -- Pause Toggle
+            br.ui:createDropdown(section, "Pause Mode", br.dropOptions.Toggle,  6)
+        br.ui:checkSectionState(section)
 
     end
     optionTable = {{
@@ -65,6 +92,7 @@ local cast
 local cd
 local combo
 local debuff
+local enemies
 local energy
 local has
 local mode
@@ -77,12 +105,55 @@ local use
 -- General Locals
 local haltProfile
 local profileStop
+local eviscerateDamage
+local finishHim
 -- Profile Specific Locals
 local actionList = {}
+
+-----------------
+--- Functions ---
+-----------------
+-- Eviscerate Finish
+local function eviscerateFinish(thisUnit)
+    local GetSpellDescription = br._G["GetSpellDescription"]
+    local desc = GetSpellDescription(spell.eviscerate)
+    local damage = 0
+    local finishHim = false
+    if ui.value("Eviscerate Execute") ~= 3 and combo > 0 and not unit.isDummy(thisUnit) then
+        local comboStart = desc:find(" "..combo.." ",1,true)
+        if comboStart ~= nil then
+            comboStart = comboStart + 2
+            local damageList = desc:sub(comboStart,desc:len())
+            comboStart = damageList:find(": ",1,true)+2
+            damageList = damageList:sub(comboStart,desc:len())
+            local comboEnd = damageList:find(" ",1,true)-1
+            damageList = damageList:sub(1,comboEnd)
+            damage = damageList:gsub(",","")
+        end
+        eviscerateDamage = tonumber(damage)
+        finishHim = tonumber(damage) >= unit.health(thisUnit) and unit.health(thisUnit) > 0
+    end
+    return finishHim
+end
 
 --------------------
 --- Action Lists ---
 --------------------
+-- Action List - Extras
+actionList.Extras = function()
+    -- Instant Poison
+    if cast.able.instantPoison() and not buff.instantPoison.exists() then
+        if cast.instantPoison() then ui.debug("Casting Instant Poison") return true end
+    end
+    -- Stealth
+    if ui.checked("Always Stealth") and cast.able.stealth() and not unit.casting() and cast.timeSinceLast.stealth() > unit.gcd(true) then
+        if cast.stealth() then ui.debug("Casting Stealth") return true end
+    end
+    -- Sprint
+    if ui.checked("Auto Sprint") and cast.able.sprint() then
+        if cast.sprint() then ui.debug("Casting Sprint") return true end
+    end
+end
 -- Action List - Defensive
 actionList.Defensive = function()
     --Crimson Vial
@@ -93,9 +164,51 @@ actionList.Defensive = function()
     end
 end -- End Action List - Defensive
 
+-- Action List - Interrupt
+actionList.Interrupt = function()
+    if ui.useInterrupt() then
+        -- Kick
+        if ui.checked("Kick") then
+            for i=1, #enemies.yards5f do
+                local thisUnit = enemies.yards5f[i]
+                if cast.able.kick(thisUnit) and unit.interruptable(thisUnit,ui.value("Interrupt At")) then
+                    if cast.kick(thisUnit) then ui.debug("Casting Kick on "..unit.name(thisUnit)) return true end
+                end
+            end
+        end
+    end
+end -- End Action List - Interrupt
+
 -- Action List - Pre-Combat
 actionList.PreCombat = function()
-
+    if unit.valid("target") then
+        --Stealth
+        if cast.able.stealth() and unit.level() >= 3 and unit.distance("target") <= 20 and not buff.stealth.exists() and cast.timeSinceLast.stealth() > unit.gcd(true) then
+            if cast.stealth() then ui.debug("Casting Stealth [Pre-Combat]") return true end
+        end
+        if buff.stealth.exists() or unit.level() < 3 then
+            --Ambush
+            if ui.value("Stealth Breaker") == 1 and cast.able.ambush("target") and cast.timeSinceLast.ambush() > unit.gcd(true) then
+                if cast.ambush("target") then ui.debug("Casting Ambush [Pre-Combat]") return true end
+            end
+            -- Cheap Shot
+            if (ui.value("Stealth Breaker") == 2 or (ui.value("Stealth Breaker") == 1 and unit.level() < 7))
+                and cast.able.cheapShot("target") and cast.timeSinceLast.cheapShot() > unit.gcd(true)
+            then
+                if cast.cheapShot("target") then ui.debug("Casting Cheap Shot [Pre-Combat]") return true end
+            end
+            -- Sinister Strike
+            if ui.value("Stealth Breaker") == 3 and cast.able.sinisterStrike("target")
+                and (combo < 5 or unit.level() < 2) and cast.timeSinceLast.sinisterStrike() > unit.gcd(true)
+            then
+                if cast.sinisterStrike("target") then ui.debug("Casting Sinister Strike [Pre-Combat]") return true end
+            end
+        end
+        -- Start Attack
+        if cast.able.autoAttack("target") and energy < 45 then
+            if cast.autoAttack("target") then ui.debug("Casting Auto Attack [Pre-Combat]") return true end
+        end
+    end
 end -- End Action List - PreCombat
 
 ----------------
@@ -111,6 +224,7 @@ local function runRotation()
     cd                                            = br.player.cd
     combo                                         = br.player.power.comboPoints.amount()
     debuff                                        = br.player.debuff
+    enemies                                       = br.player.enemies
     energy                                        = br.player.power.energy.amount()
     has                                           = br.player.has
     mode                                          = br.player.ui.mode
@@ -128,6 +242,11 @@ local function runRotation()
     units.get(5) -- Makes a variable called, units.dyn5
     units.get(40) -- Makes a variable called, units.dyn40
     units.get(40,true)
+    -- Enemies
+    enemies.get(5,"player",false,true) -- makes enemies.yards5f
+
+    if eviscerateDamage == nil then eviscerateDamage = 0 end
+    finishHim = eviscerateFinish(units.dyn5)
 
     -- Pause Timer
     if br.pauseTime == nil then br.pauseTime = GetTime() end
@@ -145,22 +264,7 @@ local function runRotation()
         ---------------------------------
         --- Out Of Combat - Rotations ---
         ---------------------------------
-        --Instant Poison
-        if spell.known.instantPoison() and not buff.instantPoison.exists() then
-            if cast.instantPoison() then ui.debug("Casting Instant Poison") return true end
-        end
-        --Auto Sprint
-        if unit.level() >= 6 and ui.checked("Auto Sprint") then
-            if br.player.moving and not unit.inCombat() then
-                if cast.sprint() then ui.debug("Casting Sprint") return true end
-            end
-        end
-        --Always Stealth
-        if unit.level() >= 3 and ui.checked("Always Stealth") then
-            if not unit.inCombat() and not buff.stealth.exists("player") then
-                if cast.stealth () then ui.debug("Casting Stealth") return true end
-            end
-        end
+        if actionList.Extras() then return true end
         -----------------
         --- Defensive ---
         -----------------
@@ -169,56 +273,47 @@ local function runRotation()
         --- Pre-Combat ---
         ------------------
         if actionList.PreCombat() then return true end
-            --Stealth
-            if unit.level() >= 3 and not unit.inCombat() and unit.valid("target") and cast.able.stealth() and br.getDistance("player", "target") <= 20 and not buff.stealth.exists("player") then
-                if cast.stealth() then ui.debug("Casting Stealth") return true end
-            end
         -----------------------------
         --- In Combat - Rotations ---
         -----------------------------
         -- Check for combat
-        if unit.valid("target") and cd.global.remain() == 0 then
-            if unit.exists(units.dyn40) and unit.distance(units.dyn40) < 40 then
-                ------------------------------
-                --- In Combat - Interrupts ---
-                ------------------------------
-                -- Start Attack
-                -- actions=auto_attack
-                if not IsAutoRepeatSpell(GetSpellInfo(6603)) and unit.exists(units.dyn5) and unit.distance(units.dyn5) < 5 then
-                    br._G.StartAttack(units.dyn5)
+        if unit.inCombat() and not profileStop and unit.exists(units.dyn5) then
+            if ui.value("Eviscerate Execute") == 1 then
+                ui.chatOverlay("Eviscerate Damage: "..eviscerateDamage..", Unit HP: "..unit.health(units.dyn5))
+            end
+            ------------------------------
+            --- In Combat - Interrupts ---
+            ------------------------------
+            if actionList.Interrupt() then return true end
+            -- Start Attack
+            if cast.able.autoAttack(units.dyn5) then
+                if cast.autoAttack(units.dyn5) then ui.debug("Casting Auto Attack") return true end
+            end
+            -- Eviscerate Finish
+            if cast.able.eviscerate() and (combo >= 5 or finishHim) then
+                if finishHim then
+                    if cast.eviscerate() then ui.debug("Casting Eviscerate [Finish]") return true end
+                else
+                    if cast.eviscerate() then ui.debug("Casting Eviscerate") return true end
                 end
-                --Ambush
-                if buff.stealth.exists() and unit.exists("target") and unit.distance("target") < 5 then
-                    if br._G.CastSpellByID(8676) then ui.debug("Casting Ambush") return true end
-                end
-                --Eviscerate
-                if unit.level() >= 2 and ui.checked("Use Eviscerate") then
-                    if combo > 4 or unit.hp("target") < 20 then
-                        if spell.known.eviscerate() and cast.able.eviscerate and unit.exists(units.dyn5) and unit.distance(units.dyn5) < 5 then
-                            if cast.eviscerate("target") then ui.debug("Casting Eviscerate") return true end
-                        end
-                    end
-                end
-                --Slice and Dice
-                if unit.level() >= 9 and ui.checked("Use Slice and Dice") then
-                    if combo > 4 or unit.hp("target") < 20 then
-                        if spell.known.sliceAndDice() and cast.able.eviscerate and unit.exists(units.dyn5) and unit.distance(units.dyn5) < 5 then
-                            if cast.sliceAndDice("target") then ui.debug("Casting Slice and Dice") return true end
-                        end
-                    end
-                end
-                -- Sinister Strike
-                if spell.known.sinisterStrike() and cast.able.sinisterStrike and unit.exists(units.dyn5) and unit.distance(units.dyn5) < 5 then
-                    if cast.sinisterStrike() then ui.debug("Casting Sinister Strike") return true end
-                end
-                --Kick Interrupt
-                if br.canInterrupt() then
-                    if spell.known.kick() and cast.able.kick() and unit.exists(units.dyn5) and unit.distance(units.dyn5) < 5 then
-                        if cast.kick() then ui.debug("Casting Kick") return true end
-                    end
-                end
-            end -- End In Combat Rotation
-        end
+            end
+            --Ambush
+            if cast.able.ambush() and buff.stealth.exists() then
+                if cast.ambush() then ui.debug("Casting Ambush") return true end
+            end
+            --Slice and Dice
+            if cast.able.sliceAndDice() and unit.level() >= 9 and combo > 4 and not buff.sliceAndDice.exists() then
+                if cast.sliceAndDice() then ui.debug("Casting Slice and Dice") return true end
+            end
+            --Eviscerate
+            if cast.able.eviscerate() and (combo > 4 or unit.hp(units.dyn5) < 20) and unit.level() >= 2 then
+                if cast.eviscerate() then ui.debug("Casting Eviscerate") return true end
+            end
+            -- Sinister Strike
+            if cast.able.sinisterStrike() then
+                if cast.sinisterStrike() then ui.debug("Casting Sinister Strike") return true end
+            end
+        end -- End In Combat Rotation
     end -- Pause
 end -- End runRotation
 local id = 1453 -- Change to the spec id profile is for.
