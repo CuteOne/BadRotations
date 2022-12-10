da68ee88acd82743ab6f38a87b3d87008d8dc8f6
Kuukuu <echidnablade@gmail.com>
2022-12-10 00:51:28 -0800

    Devastation Evoker - add info for auto readme update

--- Rotations/Evoker/Devastation/DevastateKuu.lua
+++ Rotations/Evoker/Devastation/DevastateKuu.lua
@@ -1,3 +1,17 @@
+-------------------------------------------------------
+-- Author = Kuukuu
+-- Patch = 10.0
+--    Patch should be the latest patch you've updated the rotation for (i.e., 9.2.5)
+-- Coverage = 25%
+--    Coverage should be your estimated percent coverage for class mechanics (i.e., 100%)
+-- Status = Sporadic
+--    Status should be one of: Full, Limited, Sporadic, Inactive, Unknown
+-- Readiness = Development
+--    Readiness should be one of: Raid, NoRaid, Basic, Development, Untested
+-------------------------------------------------------
+-- Required: Fill above fields to populate README.md --
+-------------------------------------------------------
+
 local rotationName = "Devastated Kuu" -- Change to name of profile listed in options drop down
 
 ---------------
