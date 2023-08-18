01705a3c911ce129ffff2e295e98b0d47ba213f9
Ashley (CuteOne) <ph34rt3hcute1@gmail.com>
2023-08-18 13:30:31 -0400

    LDoc - Covenants were a pain before and still are.

--- System/API/Covenant.lua
+++ System/API/Covenant.lua
@@ -2,15 +2,17 @@
 -- This table provides information about covenants *Shadowlands*.
 -- Covenant data is stored in br.player.covenant and can be utilized by `local covenant = br.player.covenant` in your profile.
 -- @module br.player.covenant
-local _, br = ...
-if br.api == nil then br.api = {} end
 
---- @type covenant
+--- Checks if you are a part of the specified covenant or not.
+-- @type covenant
 -- @field kyrian.active A boolean indicating if Kyrian is the active covenant.
 -- @field venthyr.active A boolean indicating if Venthyr is the active covenant.
 -- @field nightFae.active A boolean indicating if Night Fae is the active covenant.
 -- @field necrolord.active A boolean indicating if Necrolord is the active covenant.
 -- @field none.active A boolean indicating if there is no active covenant.
+
+local _, br = ...
+if br.api == nil then br.api = {} end
 
 br.api.covenant = function(covenant)
     local activeID = br._G.C_Covenants.GetActiveCovenantID()
