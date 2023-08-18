9a5a1b2688358b20731ff231f090b91b46cd1dfc
Ashley (CuteOne) <ph34rt3hcute1@gmail.com>
2023-08-18 11:18:19 -0400

    LDoc - Modified post processing script

--- .github/scripts/ldoc_post_processing.py
+++ .github/scripts/ldoc_post_processing.py
@@ -1,32 +1,37 @@
 import os
-import re
 from bs4 import BeautifulSoup
 
-# Directory containing the generated LDoc HTML files
-DOCS_DIR = './docs/'
+# Define the directory where the LDoc-generated HTML files are located
+DOCS_DIR = './docs'
 
-# Iterate over all HTML files in the directory
+# Process each HTML file in the directory
 for filename in os.listdir(DOCS_DIR):
     if filename.endswith('.html'):
         filepath = os.path.join(DOCS_DIR, filename)
-        print(f"Processing {filepath}...")  # Debug print
+        print(f"Processing {filepath}...")
 
         with open(filepath, 'r', encoding='utf-8') as file:
             soup = BeautifulSoup(file, 'html.parser')
 
-            # Find all h2 tags (module names)
-            for h2_tag in soup.find_all('h2'):
-                module_name = h2_tag.text.strip()
-                print(f"Found module: {module_name}")  # Debug print
+            # Extract the module name from the h1 tag
+            h1_tag = soup.find('h1')
+            if h1_tag and "Module" in h1_tag.text:
+                module_name = h1_tag.text.split()[-1].split('.')[-1]
+                print(f"Extracted module name: {module_name}")
 
-                # For each subsequent dt tag (function or type name), prepend the module name if not already present
-                for dt_tag in h2_tag.find_all_next('dt'):
-                    if not dt_tag.text.startswith(module_name + '.'):
-                        dt_tag.string = module_name + '.' + dt_tag.text
-                        print(f"Updated to: {dt_tag.string}")  # Debug print
+                # Find all function names and prepend the module name if not already present
+                for dt in soup.find_all('dt'):
+                    function_name = dt.string
+                    if function_name and not function_name.startswith(module_name + '.'):
+                        print(f"Updating function name: {function_name} to {module_name}.{function_name}")
+                        dt.string = module_name + '.' + function_name
 
-        # Write the modified HTML back to the file
-        with open(filepath, 'w', encoding='utf-8') as file:
-            file.write(str(soup))
+                # Save the changes back to the file
+                with open(filepath, 'w', encoding='utf-8') as file:
+                    file.write(str(soup))
+            else:
+                print(f"No module name found in {filepath}")
+
+        print(f"Processed {filepath}.")
 
 print("Processing complete.")
