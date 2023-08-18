f8a6c955c8cb04c4b1e3249796b4cf83db251388
Ashley (CuteOne) <ph34rt3hcute1@gmail.com>
2023-08-18 13:35:52 -0400

    LDoc - This surely will work, don't know otherwise

--- System/API/Covenant.lua
+++ System/API/Covenant.lua
@@ -1,15 +1,13 @@
 ---
 -- This table provides information about covenants *Shadowlands*.
 -- Covenant data is stored in br.player.covenant and can be utilized by `local covenant = br.player.covenant` in your profile.
+-- br.player.covenant has checks to determine if you are a part of the specified covenant or not.
+-- br.player.covenant.kyrian.active - Indicates if Kyrian is the active covenant.
+-- br.player.covenant.venthyr.active - Indicates if Venthyr is the active covenant.
+-- br.player.covenant.nightFae.active - Indicates if Night Fae is the active covenant.
+-- br.player.covenant.necrolord.active - Indicates if Necrolord is the active covenant.
+-- br.player.covenant.none.active - Indicates if there are no active covenants.
 -- @module br.player.covenant
-
---- Checks if you are a part of the specified covenant or not.
--- @type covenant
--- @field kyrian.active A boolean indicating if Kyrian is the active covenant.
--- @field venthyr.active A boolean indicating if Venthyr is the active covenant.
--- @field nightFae.active A boolean indicating if Night Fae is the active covenant.
--- @field necrolord.active A boolean indicating if Necrolord is the active covenant.
--- @field none.active A boolean indicating if there is no active covenant.
 
 local _, br = ...
 if br.api == nil then br.api = {} end
