e909bb866efa6023acc41299c51ae4fbf411e03c
Ashley (CuteOne) <ph34rt3hcute1@gmail.com>
2024-04-28 15:36:16 -0400

    API - Documentation Update

--- System/API/Power.lua
+++ System/API/Power.lua
@@ -1,9 +1,10 @@
-local _, br = ...
-if br.api == nil then br.api = {} end
+---
+-- These functions helpful functions for commonly used features in all profiles.
+-- Power functions are stored in br.player.power and can be utilized by `local power = br.player.power` in your profile.
 -- power is the table located at br.player.power
 -- powerType is the name of the power passed from the builder which cycles all the collected power from the power list
 -- powerIndex is the powerID passed from the builder which cycles all the collected power from the power list
--- Power List options - spell in examples signifies one of these
+-- Power List - powerIndex = powerType
     -- 0 = mana
     -- 1 = rage
     -- 2 = focus
@@ -22,15 +23,24 @@ if br.api == nil then br.api = {} end
     -- 17 = fury
     -- 18 = pain
     -- 19 = essence
+-- @module br.player.power
+local _, br = ...
+if br.api == nil then br.api = {} end
+
 br.api.power = function(power,powerType,powerIndex)
     local isDKRunes = select(2,br._G.UnitClass("player")) == "DEATHKNIGHT" and powerIndex == 5
     local isDestruction = br._G.GetSpecializationInfo(br._G.GetSpecialization()) == 267 and powerIndex == 7
 
-    -- br.player.power.spell.deficit() - Returns amount of specified power until max is reached
+    --- Gets the amount of specified power until max is reached
+    -- @function power.powerType.deficit
+    -- @return number
     power[powerType].deficit = function()
         return br.getPowerMax("player",powerIndex) - br.getPower("player",powerIndex)
     end
-    -- br.player.power.spell.frac() - Used by DKs to return fractional counts of their available runes
+
+    --- Gets the fractional counts of their available runes - Used by DKs
+    -- @function power.powerType.frac
+    -- @return number
     power[powerType].frac = function()
         -- Death Knights
         if isDKRunes then
@@ -49,11 +59,17 @@ br.api.power = function(power,powerType,powerIndex)
         end
         return br.getPower("player",powerIndex)
     end
-    -- br.player.power.spell.max() - Returns maximum amount of the specified power
+
+    --- Gets the maximum aount of the specified power
+    -- @function power.spell.max
+    -- @return number
     power[powerType].max = function()
         return br.getPowerMax("player",powerIndex)
     end
-    -- br.player.power.spell.percent() -- Returns current amount of specified power as a percentage
+
+    --- Gets the current amount of specified power as a percentage
+    -- @function power.spell.percent
+    -- @return number
     power[powerType].percent = function()
         if br.getPowerMax("player",powerIndex) == 0 then
             return 0
@@ -61,11 +77,17 @@ br.api.power = function(power,powerType,powerIndex)
             return ((br.getPower("player",powerIndex) / br.getPowerMax("player",powerIndex)) * 100)
         end
     end
-    -- br.player.power.spell.regen() -- Returns the current rate of regeneration for the specified power
+
+    --- Gets the current rate of regeneration for the specified power
+    -- @function power.spell.regen
+    -- @return number
     power[powerType].regen = function()
         return br.getRegen("player")
     end
-    -- br.player.power.spell.ttm() -- Returns the time in seconds until the specified power is maxed
+
+    --- Gets the time in seconds until the specified power is maxed
+    -- @function power.spell.ttm
+    -- @return number
     power[powerType].ttm = function(amount)
         if isDKRunes then
             if amount == nil then amount = 6 end
@@ -75,29 +97,32 @@ br.api.power = function(power,powerType,powerIndex)
         end
     end
 
-    --- Function to calculate or fetch power based on `isDKRunes`.
-    -- This function serves a dual purpose. When `isDKRunes` is true,
-    -- it counts the total number of 'runes' by executing a loop 6 times
-    -- and adding the result of `br._G.GetRuneCount(i)` to `runeCount` in each iteration.
-    -- If `isDKRunes` is false, it simply retrieves the power of the player using `br.getPower()`.
-    -- @global
-    -- @name power[powerType]
-    -- @class function
-    -- @return runeCount if `isDKRunes` is true, otherwise it returns the result of `br.getPower("player",powerIndex)`
-    -- power[powerType] = function()
-    --     if isDKRunes then
-    --         local runeCount = 0
-    --         for i = 1, 6 do
-    --             runeCount = runeCount + br._G.GetRuneCount(i)
-    --         end
-    --         return runeCount
-    --     else
-    --         return br.getPower("player",powerIndex)
-    --     end
-    -- end
-    -- Define the metatable
+    -- --- Function to calculate or fetch power based on `isDKRunes`.
+    -- -- This function serves a dual purpose. When `isDKRunes` is true,
+    -- -- it counts the total number of 'runes' by executing a loop 6 times
+    -- -- and adding the result of `br._G.GetRuneCount(i)` to `runeCount` in each iteration.
+    -- -- If `isDKRunes` is false, it simply retrieves the power of the player using `br.getPower()`.
+    -- -- @global
+    -- -- @name power[powerType]
+    -- -- @class function
+    -- -- @return runeCount if `isDKRunes` is true, otherwise it returns the result of `br.getPower("player",powerIndex)`
+    -- -- power[powerType] = function()
+    -- --     if isDKRunes then
+    -- --         local runeCount = 0
+    -- --         for i = 1, 6 do
+    -- --             runeCount = runeCount + br._G.GetRuneCount(i)
+    -- --         end
+    -- --         return runeCount
+    -- --     else
+    -- --         return br.getPower("player",powerIndex)
+    -- --     end
+    -- -- end
+
+    -- -- Define the metatable
     local powerMetaTable = {
-        -- Define the __call metamethod
+        --- Gets the current amount of the specified power.
+        -- @function power.powerType
+        -- @return number
         __call = function(tbl, ...)
             if isDKRunes then
                 local runeCount = 0
