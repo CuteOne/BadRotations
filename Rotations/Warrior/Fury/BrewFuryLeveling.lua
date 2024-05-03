c42c7f412e7a151b21385734487ce483d2f4a955
BrewingCoder <scott@gscottsingleton.com>
2024-05-02 20:34:08 -0400

    Fury Options Update

--- Rotations/Warrior/Fury/BrewFuryLeveling.lua
+++ Rotations/Warrior/Fury/BrewFuryLeveling.lua
@@ -5,7 +5,7 @@
 -- Status = Limited
 -- Readiness = PVE Leveling
 -------------------------------------------------------
-local rotationName = "BrewFuryLeveling" -- Change to name of profile listed in options drop down
+local rotationName = "BrewFuryLeveling" 
 
 local colors = {
     blue    = "|cff4285F4",
@@ -21,13 +21,27 @@ local colors = {
     orange  = "|cffff8000"
 }
 
+local text = {
+    options={
+        onlyUseConsumablesInRaids = "Only use consumables in raids",
 
+    },
+    cooldowns={
+        cooldowns = colors.purple .. "Cooldowns",
+        useCooldowns = colors.purple .. "Use cooldowns",
+        numberOfMobs = colors.purple .. "Number of mobs",
+        enragedRegeneration = colors.purple .. "Enraged Regeneration",
+    },
+    defensive={
+        defensive = colors.orange .. "Defensive",
+        useSpellReflection = colors.orange .. "Use Spell Reflection if can't interrupt",
+        usePummel = colors.orange .. "Use Pummel as interrupt",
+        beserkerRage = colors.orange .. "Use Beserker Rage",
+        
+    }
+}
 
----------------
---- Toggles ---
----------------
-local function createToggles() -- Define custom toggles, these are the buttons from the toggle bar
-    -- Rotation Button
+local function createToggles() 
     local RotationModes = {
         [1] = { mode = "Auto", value = 1 , overlay = "Automatic Rotation", tip = "Swaps between Single and Multiple based on number of #enemies.yards8 in range.", highlight = 1, icon = br.player.spell.whirlwind },
         [2] = { mode = "Mult", value = 2 , overlay = "Multiple Target Rotation", tip = "Multiple target rotation used.", highlight = 0, icon = br.player.spell.bladestorm },
@@ -35,20 +49,17 @@ local function createToggles() -- Define custom toggles, these are the buttons f
         [4] = { mode = "Off", value = 4 , overlay = "DPS Rotation Disabled", tip = "Disable DPS Rotation", highlight = 0, icon = br.player.spell.enragedRegeneration}
     };
     br.ui:createToggle(RotationModes,"Rotation",1,0)
-    -- Cooldown Button
     local CooldownModes = {
         [1] = { mode = "Auto", value = 1 , overlay = "Cooldowns Automated", tip = "Automatic Cooldowns - Boss Detection.", highlight = 1, icon = br.player.spell.recklessness },
         [2] = { mode = "On", value = 2 , overlay = "Cooldowns Enabled", tip = "Cooldowns used regardless of target.", highlight = 0, icon = br.player.spell.recklessness },
         [3] = { mode = "Off", value = 3 , overlay = "Cooldowns Disabled", tip = "No Cooldowns will be used.", highlight = 0, icon = br.player.spell.recklessness }
     };
     br.ui:createToggle(CooldownModes,"Cooldown",2,0)
-    -- Defensive Button
     local DefensiveModes = {
         [1] = { mode = "On", value = 1 , overlay = "Defensive Enabled", tip = "Includes Defensive Cooldowns.", highlight = 1, icon = br.player.spell.enragedRegeneration },
         [2] = { mode = "Off", value = 2 , overlay = "Defensive Disabled", tip = "No Defensives will be used.", highlight = 0, icon = br.player.spell.enragedRegeneration }
     };
     br.ui:createToggle(DefensiveModes,"Defensive",3,0)
-    -- Interrupt Button
     local InterruptModes = {
         [1] = { mode = "On", value = 1 , overlay = "Interrupts Enabled", tip = "Includes Basic Interrupts.", highlight = 1, icon = br.player.spell.pummel },
         [2] = { mode = "Off", value = 2 , overlay = "Interrupts Disabled", tip = "No Interrupts will be used.", highlight = 0, icon = br.player.spell.pummel }
@@ -59,11 +70,19 @@ local function createToggles() -- Define custom toggles, these are the buttons f
         [2] = { mode = "OFF", value = 0 , overlay = "Debugs Off", tip = "Debug Messages Off", highlight = 0, icon =200733 },
     }
     br.ui:createToggle(DebugModes,"Debugs",5,0)
+    local HeroicLeap = {
+        [1] = { mode = "ON", value = 1 , overlay = "Heroic Leap On", tip = "Heroic Leap On", highlight = 1, icon=br.player.spell.heroicLeap },
+        [2] = { mode = "OFF", value = 0 , overlay = "Heroic Leap Off", tip = "Heroic Leap Off", highlight = 0, icon=br.player.spell.heroicLeap },
+    }
+    br.ui:createToggle(HeroicLeap,"HeroicLeap",4,1)
+    local Charge = {
+        [1] = { mode = "ON", value = 1 , overlay = "Charge On", tip = "Charge On", highlight = 1, icon=br.player.spell.charge },
+        [2] = { mode = "OFF", value = 0 , overlay = "Charge Off", tip = "Charge Off", highlight = 0, icon=br.player.spell.charge },
+    }
+    br.ui:createToggle(Charge,"Charge",3,1)
 end
 
----------------
---- OPTIONS ---
----------------
+
 local function createOptions()
     local optionTable
 
@@ -73,40 +92,26 @@ local function createOptions()
         --- GENERAL OPTIONS --- -- Define General Options
         -----------------------
         section = br.ui:createSection(br.ui.window.profile,  "General")
+        br.ui:checkSectionState(section)
 
+        section = br.ui:createSection(br.ui.window.profile,  text.cooldowns.cooldowns)
+            br.ui:createCheckbox(section,  text.cooldowns.useCooldowns)
+            br.ui:createSpinner(section,  text.cooldowns.numberOfMobs, 2,  2,  10,  1,  "|cffFFBB00Number of enemies to use cooldowns on.")
+            br.ui:createCheckbox(section,  text.cooldowns.enragedRegeneration)
         br.ui:checkSectionState(section)
-        ------------------------
-        --- COOLDOWN OPTIONS --- -- Define Cooldown Options
-        ------------------------
-        section = br.ui:createSection(br.ui.window.profile,  "Cooldowns")
 
+        section = br.ui:createSection(br.ui.window.profile, text.defensive.defensive)
+            br.ui:createCheckbox(section,  text.defensive.useSpellReflection)
+            br.ui:createCheckbox(section,  text.defensive.usePummel)
+            br.ui:createCheckbox(section,  text.defensive.beserkerRage)
+            br.ui:createSpinner(section,  colors.orange .. "Interrupt At",  0,  0,  95,  5,  "|cffFFBB00Cast Percentage to use at.")
         br.ui:checkSectionState(section)
-        -------------------------
-        --- DEFENSIVE OPTIONS --- -- Define Defensive Options
-        -------------------------
-        section = br.ui:createSection(br.ui.window.profile, "Defensive")
 
-        br.ui:checkSectionState(section)
-        -------------------------
-        --- INTERRUPT OPTIONS --- -- Define Interrupt Options
-        -------------------------
-        section = br.ui:createSection(br.ui.window.profile, "Interrupts")
-            -- Interrupt Percentage
-            br.ui:createSpinner(section,  "Interrupt At",  0,  0,  95,  5,  "|cffFFBB00Cast Percentage to use at.")
-        br.ui:checkSectionState(section)
-        ----------------------
-        --- TOGGLE OPTIONS --- -- Degine Toggle Options
-        ----------------------
         section = br.ui:createSection(br.ui.window.profile,  "Toggle Keys")
-            -- Single/Multi Toggle
             br.ui:createDropdown(section,  "Rotation Mode", br.dropOptions.Toggle,  4)
-            --Cooldown Key Toggle
             br.ui:createDropdown(section,  "Cooldown Mode", br.dropOptions.Toggle,  3)
-            --Defensive Key Toggle
             br.ui:createDropdown(section,  "Defensive Mode", br.dropOptions.Toggle,  6)
-            -- Interrupts Key Toggle
             br.ui:createDropdown(section,  "Interrupt Mode", br.dropOptions.Toggle,  6)
-            -- Pause Toggle
             br.ui:createDropdown(section,  "Pause Mode", br.dropOptions.Toggle,  6)
         br.ui:checkSectionState(section)
     end
@@ -171,6 +176,12 @@ end -- End Action List - Extra
 
 -- Action List - Defensive
 actionList.Defensive = function()
+    -- for i=1, #enemies.yards5 do
+    --     local name,_,_,startTime,endTime,_,_,notInterruptable,spellId = br._G.UnitCastingInfo(enemies.yards5[i])
+    --     if name ~= nil then
+    --         if notInterruptable or (not notInterruptable and )
+    --     end 
+    -- end
 
 end -- End Action List - Defensive
 
@@ -209,8 +220,8 @@ actionList.PreCombat = function()
                 end
         end
         if unit.valid("target") then -- Abilities below this only used when target is valid
-            -- Start Attack
-            if unit.distance("target") >=8 and unit.distance("target") <= 40 
+            
+            if ui.mode.HeroicLeap==1 and unit.distance("target") >=8 and unit.distance("target") <= 40 
                 and cast.able.heroicLeap("target") 
                 and not cast.last.heroicLeap()
                 and not cast.last.charge()
@@ -223,7 +234,8 @@ actionList.PreCombat = function()
                     debugMessage("Failed to Heroic Leap")
                 end
             end
-            if unit.distance("target") >=8 and unit.distance("target") <= 25 
+
+            if ui.mode.Charge==1 and unit.distance("target") >=8 and unit.distance("target") <= 25 
                 and cast.able.charge("target") 
                 and not cast.last.heroicLeap()
                 and not cast.last.charge()
@@ -236,21 +248,7 @@ actionList.PreCombat = function()
                         debugMessage("Failed to Charge")
                     end
             end
-            if not unit.moving("player") and unit.distance("target") >=8 and unit.distance("target") <= 30 
-                and cast.able.heroicThrow("target") 
-                and not cast.last.heroicLeap()
-                and not cast.last.charge()
-                and not cast.last.heroicThrow()
-                then 
-                    if cast.heroicThrow("target") 
-                    then 
-                        debugMessage("Casting Heroic Throw") 
-                        return true 
-                    else
-                        debugMessage("Failed to Heroic Throw")
-                    end
-            end
-
+            
             if unit.distance("target") <= 5 then
                 if cast.able.autoAttack("target") then
                     if cast.autoAttack("target") then debugMessage("Casting Auto Attack [Pre-Combat]") return true end
@@ -284,7 +282,9 @@ local function runRotation() -- This is the main profile loop, any below this po
 
 
     if var.lastCast == nil then var.lastCast=ui.time() end
-    ui.mode.Debug = br.data.settings[br.selectedSpec].toggles["Debugs"]
+    ui.mode.Debug      = br.data.settings[br.selectedSpec].toggles["Debugs"]
+    ui.mode.Charge     = br.data.settings[br.selectedSpec].toggles["Charge"]
+    ui.mode.HeroicLeap = br.data.settings[br.selectedSpec].toggles["HeroicLeap"]
 
     -- Explanations on the Units and Enemies functions can be found in System/API/Units.lua and System/API/Enemies.lua
     -------------
@@ -340,12 +340,20 @@ local function runRotation() -- This is the main profile loop, any below this po
             --- Main ---
             ------------
 
-            if buff.victorious.exists() and cast.able.victoryRush("target") and unit.gcd()<= 0 then
+            if not talent.impendingVictory and buff.victorious.exists() and cast.able.victoryRush("target") and unit.gcd()<= 0 then
                 if cast.victoryRush("target") then 
                     debugMessage("Casting Victory Rush") 
                     return true 
                 else
                     debugMessage("Failed to Victory Rush")
+                end
+            end
+            if talent.impendingVictory and cast.able.impendingVictory("target") and unit.gcd()<= 0 then
+                if cast.impendingVictory("target") then 
+                    debugMessage("Casting Impending Victory") 
+                    return true 
+                else
+                    debugMessage("Failed to Impending Victory")
                 end
             end
             -- if cast.able.victoryRush("target") and unit.gcd()<= 0 then
